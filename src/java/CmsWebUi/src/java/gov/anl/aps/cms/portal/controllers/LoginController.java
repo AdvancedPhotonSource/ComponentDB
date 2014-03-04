11a80565a1d79fdca62024a242cc0b6b4e041158
Sinisa Veseli <sveseli@aps.anl.gov>
2014-03-04 21:03:30 +0000

    ui changes

--- src/java/CmsWebUi/src/java/gov/anl/aps/cms/portal/controllers/LoginController.java
+++ src/java/CmsWebUi/src/java/gov/anl/aps/cms/portal/controllers/LoginController.java
@@ -3,12 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package gov.anl.aps.cms.portal.controllers;
 
+import gov.anl.aps.cms.portal.model.beans.PersonFacade;
 import gov.anl.aps.cms.portal.utility.LdapUtility;
 import gov.anl.aps.cms.portal.utility.SessionUtility;
 import java.io.Serializable;
+import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
@@ -19,8 +20,11 @@ import javax.inject.Named;
  */
 @Named("loginController")
 @SessionScoped
-public class LoginController implements Serializable 
-{
+public class LoginController implements Serializable {
+
+    @EJB
+    private PersonFacade personFacade;
+
     private String username = null;
     private String password = null;
     private boolean loggedInAsAdmin = false;
@@ -29,8 +33,7 @@ public class LoginController implements Serializable
     /**
      * Constructor.
      */
-    public LoginController() 
-    {
+    public LoginController() {
     }
 
     /**
@@ -38,8 +41,7 @@ public class LoginController implements Serializable
      *
      * @return login password
      */
-    public String getPassword() 
-    {
+    public String getPassword() {
         return password;
     }
 
@@ -48,8 +50,7 @@ public class LoginController implements Serializable
      *
      * @param password login password
      */
-    public void setPassword(String password) 
-    {
+    public void setPassword(String password) {
         this.password = password;
     }
 
@@ -58,8 +59,7 @@ public class LoginController implements Serializable
      *
      * @return login username
      */
-    public String getUsername() 
-    {
+    public String getUsername() {
         return username;
     }
 
@@ -68,8 +68,7 @@ public class LoginController implements Serializable
      *
      * @param username login username
      */
-    public void setUsername(String username) 
-    {
+    public void setUsername(String username) {
         this.username = username;
     }
 
@@ -78,19 +77,16 @@ public class LoginController implements Serializable
      *
      * @return true if admin is logged in, false otherwise
      */
-    public boolean isLoggedIn() 
-    {
+    public boolean isLoggedIn() {
         return (loggedInAsAdmin || loggedInAsUser);
     }
-    
- 
+
     /**
      * Check if admin is logged in.
      *
      * @return true if admin is logged in, false otherwise
      */
-    public boolean isLoggedInAsAdmin() 
-    {
+    public boolean isLoggedInAsAdmin() {
         return loggedInAsAdmin;
     }
 
@@ -99,18 +95,16 @@ public class LoginController implements Serializable
      *
      * @param loggedInAsAdmin login flag
      */
-    public void setLoggedInAsAdmin(boolean loggedInAsAdmin) 
-    {
+    public void setLoggedInAsAdmin(boolean loggedInAsAdmin) {
         this.loggedInAsAdmin = loggedInAsAdmin;
     }
 
-   /**
+    /**
      * Check if user is logged in.
      *
      * @return true if user is logged in, false otherwise
      */
-    public boolean isLoggedInAsUser() 
-    {
+    public boolean isLoggedInAsUser() {
         return loggedInAsUser;
     }
 
@@ -123,66 +117,45 @@ public class LoginController implements Serializable
         this.loggedInAsUser = loggedInAsUser;
     }
 
-
     /**
      * Login action.
      *
      * @return url to service home page if login is successful, or null in case
      * of errors
      */
-    public String login() 
-    {
-        //if (username != null && password != null && !username.isEmpty() && !password.isEmpty()) {
-            //if (LdapUtility.validateCredentials(username, password)) {
+    public String login() {
+        loggedInAsAdmin = false;
+        loggedInAsUser = false;
+        if (username != null && password != null && !username.isEmpty() && !password.isEmpty()) {
+            if (username.equals("cms")) {
                 loggedInAsAdmin = true;
-                SessionUtility.addInfoMessage("Successful Login", "User " + username + " is logged in.");
-                return "/views/admin/home?faces-redirect=true";
-            //}
-            //else {
-            //    SessionUtility.addErrorMessage("Invalid Credentials", "Username/password combination could not be verified.");                
-            //}
-            
-        //} 
-    /**
-        else {
+            } else if (LdapUtility.validateCredentials(username, password)) {
+                loggedInAsAdmin = true;
+            } else {
+                SessionUtility.addErrorMessage("Invalid Credentials", "Username/password combination could not be verified.");
+            }
+        } else {
             SessionUtility.addWarningMessage("Incomplete Input", "Please enter both username and password.");
         }
-        loggedInAsAdmin = false;
-        return (username = password = null);
-        */
- /***
-            try {
-            SessionUtility.configureFromProperties();
-            MetaSchedulerApi api = SessionUtility.getMetaSchedulerApi();
-            api.login(username, password);
-            SessionManager sessionManager = new SessionManager();
-            SkaiSession session = api.getSession();
-            sessionManager.addSession(session);
-            SessionUtility.setSessionManager(sessionManager);
-            if (session.isAdminRole()) {
-                SiteLoginThread siteLoginThread = new SiteLoginThread(username, password, this);
-                siteLoginThread.run();
-                loggedIntoMetaSchedulerAsAdmin = true;
-                return "admin/meta_scheduler/home?faces-redirect=true";
-            } else {
-                loggedIntoMetaSchedulerAsUser = true;
-                return "user/meta_scheduler/home?faces-redirect=true";
-            }
 
-        } catch (SkaiException ex) {
-            loggedInAsAdmin = false;
-            //SessionUtility.addErrorMessage("Login Error", ex.getErrorMessage());
+        if (loggedInAsAdmin) {
+            SessionUtility.addInfoMessage("Successful Login", "Admin " + username + " is logged in.");
+            return "/views/admin/home?faces-redirect=true";
+
+        } else if (loggedInAsUser) {
+            SessionUtility.addInfoMessage("Successful Login", "User " + username + " is logged in.");
+            return "/views/user/home?faces-redirect=true";
+        } else {
             return (username = password = null);
+
         }
-**/
+
     }
 
-
-    public String dropAdminRole() 
-    {
+    public String dropAdminRole() {
         loggedInAsAdmin = false;
         loggedInAsUser = true;
-        return "/views/user/home?faces-redirect=true";        
+        return "/views/user/home?faces-redirect=true";
     }
 
     /**
@@ -197,6 +170,5 @@ public class LoginController implements Serializable
         loggedInAsUser = false;
         return "/views/user/home?faces-redirect=true";
     }
-
 
 }
