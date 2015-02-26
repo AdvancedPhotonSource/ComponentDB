60de18d3e70918da0aaa6776679d2737a88f96a1
Sinisa Veseli <sveseli@aps.anl.gov>
2015-02-26 18:56:56 +0000

    create directory before writing files

--- sbin/cdb_prepare_dev_config.sh
+++ sbin/cdb_prepare_dev_config.sh
@@ -25,18 +25,24 @@ CDB_WEB_SERVICE_LOG_FILE=${CDB_INSTALL_DIR}/var/log/cdb.log
 CDB_DB_PASSWORD_FILE=$CDB_ROOT_DIR/etc/cdb.db.passwd
 
 echo "Preparing development configuration"
+mkdir -p $CDB_ETC_DIR
 
 echo "Modifying glassfish-web config file"
 portalSrcDir=$CDB_ROOT_DIR/src/java/CdbWebPortal
 configFile=$portalSrcDir/web/WEB-INF/glassfish-web.xml
 cmd="cat $configFile.template | sed 's?CDB_DATA_DIR?$CDB_DATA_DIR?g' > $configFile"
-eval $cmd
+eval $cmd || exit 1
 
 echo "Configuring glassfish db access"
+if [ ! -f $CDB_DB_PASSWORD_FILE ]; then
+    echo "File $CDB_DB_PASSWORD_FILE does not exist."
+    exit 1
+fi
+
 CDB_DB_PASSWORD=`cat $CDB_DB_PASSWORD_FILE`
 configFile=$portalSrcDir/setup/glassfish-resources.xml
 cmd="cat $configFile.template | sed 's?CBD_DB_PASSWORD?$CDB_DB_PASSWORD?g' > $configFile"
-eval $cmd
+eval $cmd || exit 1
 
 echo "Generating web service config file"
 cmd="cat $CDB_ROOT_DIR/etc/cdb.conf.template \
