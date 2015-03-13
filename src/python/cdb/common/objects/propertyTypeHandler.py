c9b60150cc96503ac4668919a29e254aedc4044e
Sinisa Veseli <sveseli@aps.anl.gov>
2015-03-13 14:26:19 +0000

    add property related objects

---
+++ src/python/cdb/common/objects/propertyTypeHandler.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from cdbObject import CdbObject
+
+class PropertyTypeHandler(CdbObject):
+
+    DEFAULT_KEY_LIST = [ 'id', 'name', 'description' ]
+
+    def __init__(self, dict):
+        CdbObject.__init__(self, dict)
+
