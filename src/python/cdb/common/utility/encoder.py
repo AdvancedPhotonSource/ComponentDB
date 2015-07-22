a325e7b41b710538d85119d76adf8a011072085d
Sinisa Veseli <sveseli@aps.anl.gov>
2015-07-22 13:50:26 +0000

    add value utility; modify encoder to take None values

--- src/python/cdb/common/utility/encoder.py
+++ src/python/cdb/common/utility/encoder.py
@@ -8,10 +8,14 @@ class Encoder:
     def encode(cls, data):
         # Encode twice, in order to avoid issues like '+' being
         # interpreted as space after decoding
+        if data is None:
+            return None
         encodedData = base64.b64encode(base64.encodestring('%s' % data))
         return encodedData
 
     @classmethod
     def decode(cls, encodedData):
+        if encodedData is None:
+            return None
         data = base64.decodestring(base64.b64decode('%s' % encodedData))
         return data
