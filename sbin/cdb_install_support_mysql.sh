8841214c6928731dd8da817823416137e0e51db4
Sinisa Veseli <sveseli@aps.anl.gov>
2015-09-24 21:43:14 +0000

    updated mysql deployment scripts

--- sbin/cdb_install_support_mysql.sh
+++ sbin/cdb_install_support_mysql.sh
@@ -6,19 +6,15 @@ MY_DIR=`dirname $0` && cd $MY_DIR && MY_DIR=`pwd`
 if [ -z "${CDB_ROOT_DIR}" ]; then
     CDB_ROOT_DIR=$MY_DIR/..
 fi
-CDB_SUPPORT_DIR=$CDB_ROOT_DIR/../support
-CDB_DATA_DIR=$CDB_ROOT_DIR/../data
+CDB_SHORT_HOSTNAME=`hostname -s`
+CDB_INSTALL_DIR=${CDB_INSTALL_DIR:=$CDB_ROOT_DIR/..}
+CDB_SUPPORT_DIR=${CDB_SUPPORT_DIR:=$CDB_INSTALL_DIR/support-$CDB_SHORT_HOSTNAME}
 
 execute() {
     echo "Executing: $@"
     eval "$@"
 }
 
-if [ ! -d $CDB_SUPPORT_DIR ]; then
-    echo "Creating new CDB support directory $CDB_SUPPORT_DIR."
-    cd `dirname $CDB_SUPPORT_DIR`
-    execute svn co $CDB_SVN_URL/support support
-fi
 cd $CDB_SUPPORT_DIR
 execute $CDB_SUPPORT_DIR/bin/build_mysql.sh
 
