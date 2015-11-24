0a4496d944bbb03c6e406a11b83e658e339aa143
Dariusz Piotr Jarosz <djarosz@aps.anl.gov>
2015-11-24 18:50:28 +0000

    Add the property type handler traveler instance/ traveler template along with property types who utalize these handlers

--- db/sql/cdb_dev/populate_property_type.sql
+++ db/sql/cdb_dev/populate_property_type.sql
@@ -79,6 +79,8 @@ INSERT INTO `property_type` VALUES
 (77,'Distance From Source Point','',10,NULL,'','meters'),
 (78,'Dimension','',3,NULL,'',''),
 (79,'Verified','',10,11,'',''),
-(80,'Critical Component','',10,10,'','');
+(80,'Critical Component','',10,10,'',''),
+(81,'Traveler Template','Allows integration of traveler templates from traveler application.',5,13,'',''),
+(82,'Traveler Instance','Allows integration of traveler instances from traveler application.',5,14,'','');
 /*!40000 ALTER TABLE `property_type` ENABLE KEYS */;
 UNLOCK TABLES;
