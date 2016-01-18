6081a5c48985c0352bc7368d64bef49fba4f15f7
Dariusz Piotr Jarosz <djarosz@aps.anl.gov>
2016-01-18 19:46:56 +0000

    Add functionality to add/update a location on a component instance.

--- src/python/cdb/common/db/impl/componentInstanceHandler.py
+++ src/python/cdb/common/db/impl/componentInstanceHandler.py
@@ -8,14 +8,19 @@ from cdb.common.exceptions.objectAlreadyExists import ObjectAlreadyExists
 from cdb.common.exceptions.objectNotFound import ObjectNotFound
 from cdb.common.db.entities.componentInstance import ComponentInstance
 from cdb.common.db.entities.componentInstanceProperty import ComponentInstanceProperty
+from cdb.common.db.impl.componentInstanceLocationHistoryHandler import ComponentInstanceLocationHistoryHandler
 from cdb.common.db.impl.cdbDbEntityHandler import CdbDbEntityHandler
-from entityInfoHandler import EntityInfoHandler
+from cdb.common.db.impl.locationHandler import LocationHandler
+from cdb.common.db.impl.entityInfoHandler import EntityInfoHandler
+
 
 class ComponentInstanceHandler(CdbDbEntityHandler):
 
     def __init__(self):
         CdbDbEntityHandler.__init__(self)
         self.entityInfoHandler = EntityInfoHandler()
+        self.componentInstanceLocationHistoryHandler = ComponentInstanceLocationHistoryHandler()
+        self.locationHandler = LocationHandler()
 
     def findComponentInstanceById(self, session, id):
         try:
@@ -36,4 +41,24 @@ class ComponentInstanceHandler(CdbDbEntityHandler):
         session.add(dbComponentInstanceProperty)
         session.flush()
         return dbComponentInstanceProperty
+
+    def updateComponentInstanceLocation(self, session, componentInstanceId, locationId, locationDetails, enteredByUserId):
+        self.logger.debug('Updating location for component instance id %s' % componentInstanceId)
+        dbComponentInstance = self.getComponentInstanceById(session, componentInstanceId)
+
+        if dbComponentInstance.location:
+            self.componentInstanceLocationHistoryHandler.addComponentInstanceLocationHistory(session, dbComponentInstance)
+
+        dbLocation = self.locationHandler.getLocationById(session, locationId)
+        dbComponentInstance.location = dbLocation
+        dbComponentInstance.location_details = locationDetails
+
+        # Update entity information
+        dbEntityInfo = dbComponentInstance.entityInfo
+        dbComponentInstance.entityInfo = self.entityInfoHandler.updateEntityInfo(session, dbEntityInfo, enteredByUserId)
+
+        session.add(dbComponentInstance)
+        session.flush()
+        return dbComponentInstance
+
 
