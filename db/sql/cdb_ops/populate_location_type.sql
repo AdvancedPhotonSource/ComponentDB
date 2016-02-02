1bc852329f4055db464069c82f0b0064257c830b
Dariusz Piotr Jarosz <djarosz@aps.anl.gov>
2016-02-02 19:29:59 +0000

    Add basic supporting entities to cdb_ops

--- db/sql/cdb_ops/populate_location_type.sql
+++ db/sql/cdb_ops/populate_location_type.sql
@@ -1,4 +1,12 @@
 LOCK TABLES `location_type` WRITE;
 /*!40000 ALTER TABLE `location_type` DISABLE KEYS */;
+INSERT INTO `location_type` VALUES
+(1,'Building',NULL),
+(2,'Area',NULL),
+(3,'Room',NULL),
+(4,'Table',NULL),
+(5,'Cabinet',NULL),
+(6,'Rack',NULL),
+(7,'Shelf',NULL);
 /*!40000 ALTER TABLE `location_type` ENABLE KEYS */;
 UNLOCK TABLES;
