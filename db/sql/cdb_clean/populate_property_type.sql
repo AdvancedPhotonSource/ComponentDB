99acdf6c086e641371c6d9c5d0837d2e566595ad
Dariusz Piotr Jarosz <djarosz@aps.anl.gov>
2016-02-10 14:44:59 +0000

    Make corrections to the default clean database property type script

--- db/sql/cdb_clean/populate_property_type.sql
+++ db/sql/cdb_clean/populate_property_type.sql
@@ -1,6 +1,6 @@
 LOCK TABLES `property_type` WRITE;
 /*!40000 ALTER TABLE `property_type` DISABLE KEYS */;
-INSERT INTO 'property_type' VALUES
+INSERT INTO `property_type` VALUES
 (1, 'Image', 'Allows a user to add images to a domain entity', 1, 2, NULL, NULL);
 /*!40000 ALTER TABLE `property_type` ENABLE KEYS */;
 UNLOCK TABLES;
