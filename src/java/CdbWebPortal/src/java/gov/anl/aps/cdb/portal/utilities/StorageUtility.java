51de49fb01f96c3b17fcc365c58a835eef0cf767
Dariusz Piotr Jarosz <djarosz@aps.anl.gov>
2016-03-30 18:23:44 +0000

    Add ability to perform file system checking when generating a path for a viewable attachment. Use stock images for missing gallery previews. Handles viewable documents.

--- src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/utilities/StorageUtility.java
+++ src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/utilities/StorageUtility.java
@@ -10,6 +10,8 @@
 package gov.anl.aps.cdb.portal.utilities;
 
 import gov.anl.aps.cdb.common.constants.CdbProperty;
+import gov.anl.aps.cdb.common.constants.CdbPropertyValue;
+import java.io.File;
 
 /**
  * Utility class for manipulating file system storage.
@@ -23,29 +25,29 @@ public class StorageUtility {
     private static final String PropertyValueImagesDirectory = "/propertyValue/images";
     private static final String PropertyValueDocumentsDirectory = "/propertyValue/documents";
     private static final String LogAttachmentsDirectory = "/log/attachments";
-    
+
     public static String getFullFilePath(String rootPath, String fileName) {
         if (fileName != null) {
             return rootPath + "/" + fileName;
         }
         return null;
     }
-    
+
     //
     // Log attachments
     //
     public static String getLogAttachmentsDirectory() {
         return LogAttachmentsDirectory;
     }
-    
+
     public static String getLogAttachmentPath(String attachmentName) {
         return getFullFilePath(getLogAttachmentsDirectory(), attachmentName);
     }
-    
+
     public static String getApplicationLogAttachmentPath(String attachmentName) {
         return SessionUtility.getContextRoot() + getLogAttachmentPath(attachmentName);
     }
-    
+
     public static String getFileSystemLogAttachmentsDirectory() {
         return STORAGE_DIRECTORY + getLogAttachmentsDirectory();
     }
@@ -64,7 +66,7 @@ public class StorageUtility {
     public static String getPropertyValueDocumentPath(String documentName) {
         return getFullFilePath(getPropertyValueDocumentsDirectory(), documentName);
     }
-    
+
     public static String getApplicationPropertyValueDocumentPath(String documentName) {
         return SessionUtility.getContextRoot() + getPropertyValueDocumentPath(documentName);
     }
@@ -76,7 +78,7 @@ public class StorageUtility {
     public static String getFileSystemPropertyValueDocumentPath(String documentName) {
         return getFullFilePath(getFileSystemPropertyValueDocumentsDirectory(), documentName);
     }
-    
+
     //
     // Property value images
     //
@@ -84,14 +86,53 @@ public class StorageUtility {
         return PropertyValueImagesDirectory;
     }
     
-    public static String getPropertyValueImagePath(String imageName) {
-        return getFullFilePath(getPropertyValueImagesDirectory(), imageName);
+    public static String getPropertyValueImagePath(String imageName, String type) {
+        return getPropertyValueImagePath(imageName, type, true);
     }
-    
+
+    public static String getPropertyValueImagePath(String imageName, String type, Boolean verifyFileExists) {
+        String imagePath;
+
+        if (imageName.startsWith(CdbPropertyValue.IMAGE_PREFIX)) {
+            imagePath = getFullFilePath(getPropertyValueImagesDirectory(), imageName + type);
+        } else {
+            imagePath = getFullFilePath(getPropertyValueDocumentsDirectory(), imageName + type);
+        }
+
+        if (verifyFileExists) {
+            String fsPath = STORAGE_DIRECTORY + imagePath;
+            Boolean fileExists = (new File(fsPath)).isFile();
+            if (fileExists) {
+                return imagePath;
+            } else {
+                String format = GalleryUtility.getImageFormat(imageName);
+                String dataType;
+
+                if (format.equalsIgnoreCase("pdf")) {
+                    dataType = "pdf";
+                } else {
+                    if (GalleryUtility.viewableFormat(format)) {
+                        dataType = "image";
+                    } else {
+                        return imagePath; 
+                    }
+                }
+
+                return "/resources/images/" + dataType + "-gallery.png" + type;
+            }
+        } else {
+            return imagePath;
+        }
+    }
+
     public static String getApplicationPropertyValueImagePath(String imageName) {
-        return SessionUtility.getContextRoot() + getPropertyValueImagePath(imageName);
+        if (imageName.startsWith(CdbPropertyValue.IMAGE_PREFIX)) {
+            return SessionUtility.getContextRoot() + getPropertyValueImagePath(imageName, CdbPropertyValue.ORIGINAL_IMAGE_EXTENSION, false);
+        } else {
+            return SessionUtility.getContextRoot() + getPropertyValueImagePath(imageName, "", false);
+        }
     }
-    
+
     public static String getFileSystemPropertyValueImagesDirectory() {
         return STORAGE_DIRECTORY + getPropertyValueImagesDirectory();
     }
@@ -99,5 +140,5 @@ public class StorageUtility {
     public static String getFileSystemPropertyValueImagePath(String imageName) {
         return getFullFilePath(getFileSystemPropertyValueImagesDirectory(), imageName);
     }
-    
+
 }
