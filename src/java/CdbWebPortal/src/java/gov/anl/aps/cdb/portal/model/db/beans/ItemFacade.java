0149edd4f1f8fac7ab4d75e8afa4eaa37327c137
Dariusz Piotr Jarosz <djarosz@aps.anl.gov>
2016-06-08 16:44:53 +0000

    Restore functionality to entiy. Add a unique attributes query used for insert precheck.

--- src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/model/db/beans/ItemFacade.java
+++ src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/model/db/beans/ItemFacade.java
@@ -5,8 +5,8 @@
  */
 package gov.anl.aps.cdb.portal.model.db.beans;
 
+import gov.anl.aps.cdb.portal.model.db.entities.Domain;
 import gov.anl.aps.cdb.portal.model.db.entities.Item;
-import gov.anl.aps.cdb.portal.model.db.entities.ItemElement;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
@@ -40,6 +40,65 @@ public class ItemFacade extends CdbEntityFacade<Item> {
         return null;
     }
     
+    public List<Item> findByName(String name) {
+        try{
+            return (List<Item>) em.createNamedQuery("Item.findByName")
+                    .setParameter("name", name)
+                    .getResultList();
+        } catch (NoResultException ex) {
+            
+        }
+        return null;
+    }
+    
+    public Item findByUniqueAttributes(Item derivedFromItem, Domain domain, 
+            String name, String itemIdentifier1, String itemIdentifier2) {
+        String QueryString = "SELECT i FROM Item i WHERE "; 
+        if (derivedFromItem == null) {
+           QueryString += "i.derivedFromItem is null ";  
+        } else {
+            QueryString += "i.derivedFromItem.id = " + derivedFromItem.getId() + " "; 
+        }
+        
+        QueryString += " AND i.domain.id = " + domain.getId() + " "; 
+        
+        if(name == null || name.isEmpty()) {
+           QueryString += "AND i.name is null ";  
+        } else {
+           QueryString += "AND i.name = '" + name + "' ";   
+        }
+        
+        if(itemIdentifier1 == null || itemIdentifier1.isEmpty()) {
+           QueryString += "AND i.itemIdentifier1 is null ";  
+        } else {
+           QueryString += "AND i.itemIdentifier1 = '" + itemIdentifier1 + "' ";   
+        }
+        
+        if(itemIdentifier2 == null || itemIdentifier2.isEmpty()) {
+           QueryString += "AND i.itemIdentifier2 is null ";  
+        } else {
+           QueryString += "AND i.itemIdentifier2 = '" + itemIdentifier2 + "' ";   
+        }
+        
+        try {
+            return (Item) em.createQuery(QueryString).getSingleResult(); 
+        } catch (NoResultException ex) {
+        }       
+        
+        return null; 
+    }
+    
+    public List<Item> findByDerivedFromItemId(int derivedFromItemId) {
+        try{
+            return (List<Item>) em.createNamedQuery("Item.findByDerivedFromItemId")
+                    .setParameter("id", derivedFromItemId)
+                    .getResultList();
+        } catch (NoResultException ex) {
+            
+        }
+        return null;
+    }
+    
     public List<Item> findByDomainAndEntityTypeOrderByQrId(String domainName, String entityTypeName) {
         try{
             return (List<Item>) em.createNamedQuery("Item.findByDomainNameOderByQrId")
@@ -50,6 +109,10 @@ public class ItemFacade extends CdbEntityFacade<Item> {
             
         }
         return null;
+    }
+    
+    public List<Item> findAllWithName() {
+        return (List<Item>) em.createNamedQuery("Item.findAllWithName").getResultList(); 
     }
 
     public ItemFacade() {
