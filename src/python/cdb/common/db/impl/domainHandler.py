3868f99385486e6ba932ce21dcffa581af154dcc
Dariusz Piotr Jarosz <djarosz@aps.anl.gov>
2016-06-17 13:12:29 +0000

    Add suport for adding allowed entity types for domain handlers.

--- src/python/cdb/common/db/impl/domainHandler.py
+++ src/python/cdb/common/db/impl/domainHandler.py
@@ -9,12 +9,14 @@ from cdb.common.exceptions.invalidArgument import InvalidArgument
 from cdb.common.db.entities.domain import Domain
 from cdb.common.db.entities.domainHandler import DomainHandler as DomainHandlerEntity
 from cdb.common.db.impl.cdbDbEntityHandler import CdbDbEntityHandler
-
+from cdb.common.db.entities.allowedDomainHandlerEntityType import AllowedDomainHandlerEntityType
+from cdb.common.db.impl.entityTypeHandler import EntityTypeHandler
 
 class DomainHandler(CdbDbEntityHandler):
 
     def __init__(self):
         CdbDbEntityHandler.__init__(self)
+        self.entityTypeHandler = EntityTypeHandler()
 
     def findDomainHandlerByName(self, session, name):
         return self._findDbObjByName(session, DomainHandlerEntity, name)
@@ -39,3 +41,19 @@ class DomainHandler(CdbDbEntityHandler):
         session.flush()
         self.logger.debug('Inserted domain id %s' % dbDomain.id)
         return dbDomain
+
+    def addAllowedEntityType(self, session, domainHandlerName, entityTypeName):
+        domainHandler = self.findDomainHandlerByName(session, domainHandlerName)
+        entityType = self.entityTypeHandler.findEntityTypeByName(session, entityTypeName)
+
+        dbAllowedDomainHandlerEntityType = AllowedDomainHandlerEntityType()
+
+        dbAllowedDomainHandlerEntityType.domainHandler = domainHandler
+        dbAllowedDomainHandlerEntityType.entityType = entityType
+
+
+        session.add(dbAllowedDomainHandlerEntityType)
+        session.flush()
+        self.logger.debug('Inserted allowed entity type %s for domain handler %s' % (entityTypeName, domainHandlerName))
+
+        return dbAllowedDomainHandlerEntityType
