99872dcd2915a87c229d446120fba8557f2e5410
Dariusz Piotr Jarosz <djarosz@aps.anl.gov>
2016-07-27 16:17:46 +0000

    Add UI functionality for the switching of group setting views. Available groups for the user. Icons with meaning for different groups. Sorting of groups in a logical order. Generated Label for the current view being displayed.

--- src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/controllers/SettingController.java
+++ src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/controllers/SettingController.java
@@ -3,7 +3,9 @@
  */
 package gov.anl.aps.cdb.portal.controllers;
 
+import gov.anl.aps.cdb.common.constants.CdbRole;
 import gov.anl.aps.cdb.portal.model.db.beans.SettingTypeFacade;
+import gov.anl.aps.cdb.portal.model.db.beans.UserGroupFacade;
 import gov.anl.aps.cdb.portal.model.db.entities.SettingEntity;
 import gov.anl.aps.cdb.portal.model.db.entities.SettingType;
 import gov.anl.aps.cdb.portal.model.db.entities.UserGroup;
@@ -11,6 +13,7 @@ import gov.anl.aps.cdb.portal.model.db.entities.UserInfo;
 import gov.anl.aps.cdb.portal.utilities.SessionUtility;
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
@@ -23,7 +26,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.apache.log4j.Logger;
 
 /**
- * Login controller.
+ * Setting controller.
  */
 @Named("settingController")
 @SessionScoped
@@ -35,15 +38,18 @@ public class SettingController implements Serializable {
     @EJB
     private SettingTypeFacade settingTypeFacade;
 
+    @EJB
+    private UserGroupFacade userGroupFacade;
+
     private static final Logger logger = Logger.getLogger(SettingController.class.getName());
 
     private UserInfoController userInfoController = null;
     private UserGroupController userGroupController = null;
-    
-    private UserGroup userGroupForSettingsView; 
 
+    private UserGroup userGroupForSettingsView = null;
     private SettingEntity currentSettingEntity = null;
     private Date settingsTimestamp = null;
+    private List<UserGroup> availableUserGroupsForSelection = null;
 
     public SettingController() {
         settingsTimestamp = new Date();
@@ -66,7 +72,7 @@ public class SettingController implements Serializable {
         } else if (getCurrentSettingEntity() != null) {
             if (getCurrentSettingEntity().areSettingsModifiedAfterDate(lastSettingLoadTime)) {
                 return true;
-            }           
+            }
         }
 
         return areSettingListsDifferentAfterDate(lastSettingLoadTime);
@@ -88,7 +94,7 @@ public class SettingController implements Serializable {
             group.setUserGroupSettingList(sessionUserGroup.getUserGroupSettingList());
             userGroupController.setCurrent(group);
             userGroupController.update();
-            return; 
+            return;
         }
         //TODO add permission checks for group setting mod. 
 
@@ -107,16 +113,16 @@ public class SettingController implements Serializable {
     }
 
     public UserGroup getUserGroupForSettingsView() {
-        return userGroupForSettingsView; 
+        return userGroupForSettingsView;
     }
 
     public void setUserGroupForSettingsView(UserGroup userGroupSettingsView) {
-        UserGroup lastUserGroupSettingView = getUserGroupForSettingsView(); 
+        UserGroup lastUserGroupSettingView = getUserGroupForSettingsView();
         if ((lastUserGroupSettingView != null && userGroupSettingsView != null
                 && !Objects.equals(userGroupSettingsView.getId(), lastUserGroupSettingView.getId()))
-                || (userGroupSettingsView == null && lastUserGroupSettingView != null) 
+                || (userGroupSettingsView == null && lastUserGroupSettingView != null)
                 || (userGroupSettingsView != null && lastUserGroupSettingView == null)) {
-            settingsTimestamp = null; 
+            settingsTimestamp = null;
             populateSessionSettingEntityFromSettingTypeDefaults(userGroupSettingsView);
             this.userGroupForSettingsView = userGroupSettingsView;
             try {
@@ -132,9 +138,16 @@ public class SettingController implements Serializable {
         ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
         ec.redirect(((HttpServletRequest) ec.getRequest()).getRequestURI());
     }
-    
+
+    private void resetSessionVariables() {
+        settingsTimestamp = null;
+        currentSettingEntity = null;
+        userGroupForSettingsView = null;
+        availableUserGroupsForSelection = null;
+    }
+
     public void loadSessionUser(UserInfo sessionUser) {
-        settingsTimestamp = null; 
+        resetSessionVariables();
         populateSessionSettingEntityFromSettingTypeDefaults(sessionUser);
     }
 
@@ -146,5 +159,110 @@ public class SettingController implements Serializable {
                 settingEntity.populateDefaultSettingList(settingTypeList);
             }
         }
+    }
+
+    private UserInfo getSessionUser() {
+        return (UserInfo) SessionUtility.getUser();
+    }
+
+    public String getDefaultSelectionLabel() {
+        UserInfo sessionUser = getSessionUser();
+        if (sessionUser != null) {
+            return "User(" + sessionUser.getUsername() + ")";
+        }
+        return "Default";
+    }
+
+    public String getViewAsLabel() {
+        SettingEntity settingEntity = getCurrentSettingEntity();
+        if (settingEntity instanceof UserGroup) {
+            return "Group(" + ((UserGroup) settingEntity).getName() + ")";
+        }
+
+        return getDefaultSelectionLabel();
+    }
+
+    /**
+     * Fetch user groups with settings and all user groups that current user has
+     * permission to edit.
+     *
+     * @return
+     */
+    public List<UserGroup> getAvailableUserGroupsForSelection() {
+        if (availableUserGroupsForSelection == null) {
+            if (SessionUtility.getUser() != null) {
+                availableUserGroupsForSelection = sortUserGroupsForUser(userGroupFacade.findAll());
+            } else {
+                availableUserGroupsForSelection = userGroupFacade.findUserGroupsWithSettings();
+            }
+        }
+        return availableUserGroupsForSelection;
+    }
+
+    /**
+     * Sort groups for user based on groups with settings and groups with
+     * permission to edit for the highest priority.
+     *
+     * @param userGroupList
+     * @return
+     */
+    private List<UserGroup> sortUserGroupsForUser(List<UserGroup> userGroupList) {
+        List<UserGroup> newUserGroupList = new ArrayList<>();
+
+        // Remove all without settings and permissions. 
+        List<UserGroup> toRemoveUserGroupList = new ArrayList<>();
+        for (UserGroup userGroup : userGroupList) {
+            if (userGroup.getUserGroupSettingList() == null
+                    || userGroup.getUserGroupSettingList().isEmpty()
+                    && !isSessionUserHaveGroupWritePermission(userGroup)) {
+                toRemoveUserGroupList.add(userGroup);
+            }
+        }
+
+        // Remove the un needed groups. 
+        for (UserGroup userGroup : toRemoveUserGroupList) {
+            userGroupList.remove(userGroup);
+        }
+
+        // Add groups user is member of. 
+        for (UserGroup userGroup : userGroupList) {
+            if (isSessionUserMemeberOfGroup(userGroup)) {
+                newUserGroupList.add(userGroup);
+            }
+        }
+
+        // Add groups user has permission for 
+        for (UserGroup userGroup : userGroupList) {
+            if (isSessionUserHaveGroupWritePermission(userGroup)
+                    && !newUserGroupList.contains(userGroup)) {
+                newUserGroupList.add(userGroup);
+            }
+        }
+
+        // Add remaining groups. 
+        for (UserGroup userGroup : userGroupList) {
+            if (!newUserGroupList.contains(userGroup)) {
+                newUserGroupList.add(userGroup);
+            }
+        }
+
+        return newUserGroupList;
+    }
+
+    public boolean isSessionUserHaveGroupWritePermission(UserGroup userGroup) {
+        UserInfo sessionUser = getSessionUser();
+        if (sessionUser != null) {
+            // TODO user the group settings writeable role that oculd be assigned to users. 
+            return SessionUtility.getRole().equals(CdbRole.ADMIN);
+        }
+        return false;
+    }
+
+    public boolean isSessionUserMemeberOfGroup(UserGroup userGroup) {
+        UserInfo sessionUser = getSessionUser();
+        if (sessionUser != null) {
+            return userGroup.getUserInfoList().contains(sessionUser);
+        }
+        return false;
     }
 }
