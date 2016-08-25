bdcd3567628c7163c0de089a2adf8ed6f16a49ff
Dariusz Piotr Jarosz <djarosz@aps.anl.gov>
2016-08-25 13:47:48 +0000

    No converter name is required when only one is defined for an entity.

--- src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/controllers/UserInfoController.java
+++ src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/controllers/UserInfoController.java
@@ -471,7 +471,7 @@ public class UserInfoController extends CdbEntityController<UserInfo, UserInfoFa
     /**
      * Converter class for user info objects.
      */
-    @FacesConverter(value = "userInfoConverter", forClass = UserInfo.class)
+    @FacesConverter(forClass = UserInfo.class)
     public static class UserInfoControllerConverter implements Converter {
 
         @Override
