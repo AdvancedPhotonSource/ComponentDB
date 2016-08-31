af678b438ec42dd3953511a6ed305c3426caed0d
Dariusz Piotr Jarosz <djarosz@aps.anl.gov>
2016-08-31 17:33:00 +0000

    Add a regular expression for checking validity of emails.

--- src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/common/utilities/StringUtility.java
+++ src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/common/utilities/StringUtility.java
@@ -10,11 +10,18 @@
 package gov.anl.aps.cdb.common.utilities;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 /**
  * String utility class.
  */
 public class StringUtility {
+    
+    private static final Pattern emailPattern = Pattern.compile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$");
+   
+    public static boolean isEmailAddressValid(String emailAddress){
+        return emailPattern.matcher(emailAddress).matches();
+    }
 
     /**
      * Verify that two char sequences are the same.
