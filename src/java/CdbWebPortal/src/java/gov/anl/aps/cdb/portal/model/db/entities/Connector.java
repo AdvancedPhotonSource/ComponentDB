6c2d6b06ca9f8a5623858ba71324572299f40d2e
Dariusz Piotr Jarosz <djarosz@aps.anl.gov>
2016-08-31 17:34:56 +0000

    Fix the connector bug. table name was missing.

--- src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/model/db/entities/Connector.java
+++ src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/model/db/entities/Connector.java
@@ -20,6 +20,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -30,6 +31,7 @@ import javax.xml.bind.annotation.XmlTransient;
  * @author djarosz
  */
 @Entity
+@Table(name = "connector")
 @XmlRootElement
 @NamedQueries({
     @NamedQuery(name = "Connector.findAll", query = "SELECT c FROM Connector c"),
