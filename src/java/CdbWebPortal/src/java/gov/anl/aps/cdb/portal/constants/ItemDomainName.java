23aba53040e1807eabac94c40cedad0a0a5a7637
Dariusz Piotr Jarosz <djarosz@aps.anl.gov>
2016-09-13 18:30:46 +0000

    Add constants for domain names & item element relationship type names/.

---
+++ src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/constants/ItemDomainName.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gov.anl.aps.cdb.portal.constants;
+
+public enum ItemDomainName {
+        inventory("Inventory"),
+        catalog("Catalog"),
+        location("Location");
+
+        private String value;
+
+        private ItemDomainName(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+    };
