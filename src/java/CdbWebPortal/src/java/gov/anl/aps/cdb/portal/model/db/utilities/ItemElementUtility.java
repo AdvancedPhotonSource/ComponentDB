0cb9704868732393ca23607dc167530f00ba3639
Dariusz Piotr Jarosz <djarosz@aps.anl.gov>
2016-09-13 18:36:09 +0000

    Add improvements for cration of item hierarchy. Allow using item elements or items, support treenode types as domain name.

--- src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/model/db/utilities/ItemElementUtility.java
+++ src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/model/db/utilities/ItemElementUtility.java
@@ -13,7 +13,6 @@ import gov.anl.aps.cdb.common.exceptions.CdbException;
 import gov.anl.aps.cdb.common.exceptions.InvalidObjectState;
 import gov.anl.aps.cdb.portal.model.db.entities.Item;
 import gov.anl.aps.cdb.portal.model.db.entities.ItemElement;
-import gov.anl.aps.cdb.portal.utilities.ConfigurationUtility;
 import java.util.ArrayList;
 import java.util.List;
 import org.primefaces.model.DefaultTreeNode;
@@ -23,9 +22,29 @@ import org.primefaces.model.TreeNode;
  * DB utility class for item elements.
  */
 public class ItemElementUtility {
+    
+    private final static String ITEM_ELEMENT_NODE_TYPE = "itemElement"; 
+    
+    public static TreeNode createItemElementRoot (Item parentItem) throws CdbException {
+        return createTreeRoot(parentItem, false);
+    }
+    
+    public static TreeNode createItemRoot (Item parentItem) throws CdbException {
+        return createTreeRoot(parentItem, true);
+    }
 
-    public static TreeNode createItemElementRoot(Item parentItem) throws CdbException {
-        TreeNode itemElementRoot = new DefaultTreeNode(new ItemElement(), null);
+    private static TreeNode createTreeRoot(Item parentItem, boolean useChildItem) throws CdbException {
+        Object newTreeNodeDataObject; 
+        String nodeType; 
+        if (useChildItem) {
+            newTreeNodeDataObject = parentItem; 
+            nodeType = parentItem.getDomain().getName();
+        } else {
+            newTreeNodeDataObject = parentItem.getSelfElement(); 
+            nodeType = ITEM_ELEMENT_NODE_TYPE;
+        }
+        
+        TreeNode treeRoot = new DefaultTreeNode(nodeType, newTreeNodeDataObject, null);
         if (parentItem == null) {
             throw new CdbException("Cannot create item element tree view: parent item is not set.");
         }
@@ -35,11 +54,11 @@ public class ItemElementUtility {
         // element node, and removed from the branch list after population is done
         // If an object is encountered twice in the tree branch, this itemates an error.
         List<Item> itemTreeBranch = new ArrayList<>();
-        populateItemNode(itemElementRoot, parentItem, itemTreeBranch);
-        return itemElementRoot;
+        populateItemNode(treeRoot, parentItem, itemTreeBranch, useChildItem);
+        return treeRoot;
     }
 
-    private static void populateItemNode(TreeNode itemElementNode, Item item, List<Item> itemTreeBranch) throws InvalidObjectState {
+    private static void populateItemNode(TreeNode itemElementNode, Item item, List<Item> itemTreeBranch, boolean useChildItem) throws InvalidObjectState {
         List<ItemElement> itemElementList = item.getItemElementDisplayList();
         if (itemElementList == null) {
             return;
@@ -48,14 +67,27 @@ public class ItemElementUtility {
         for (ItemElement itemElement : itemElementList) {
             Item childItem = itemElement.getContainedItem();
            
-
-            TreeNode childItemElementNode = new DefaultTreeNode(itemElement, itemElementNode);
+            TreeNode childItemElementNode = null;
+            
+            if (useChildItem) {
+                if (childItem != null) {
+                    String nodeType = childItem.getDomain().getName(); 
+                    childItemElementNode = new DefaultTreeNode(nodeType, childItem, itemElementNode);
+                } else {
+                    // no child item is linked to item element. 
+                    continue;
+                }
+            } 
+            
+            if (childItemElementNode == null) {
+                childItemElementNode = new DefaultTreeNode(ITEM_ELEMENT_NODE_TYPE, itemElement, itemElementNode);
+            }
             if (childItem != null) {
                 if (itemTreeBranch.contains(childItem)) {
                     throw new InvalidObjectState("Cannot create item element tree view: circular child/parent relationship found with items "
                             + childItem.getName() + " and " + childItem.getName() + ".");
                 }
-                populateItemNode(childItemElementNode, childItem, itemTreeBranch);
+                populateItemNode(childItemElementNode, childItem, itemTreeBranch, useChildItem);
             }
 
         }
