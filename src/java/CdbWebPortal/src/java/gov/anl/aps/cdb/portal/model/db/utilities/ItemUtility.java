c6ab86a39da79879f02a04b90049d188fc8f01d5
Dariusz Piotr Jarosz <djarosz@aps.anl.gov>
2016-09-16 17:19:45 +0000

    Add common function for creating a list of items based on its elements. Add a common funtion to get a list of items based on relationship to the itme.

--- src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/model/db/utilities/ItemUtility.java
+++ src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/model/db/utilities/ItemUtility.java
@@ -10,6 +10,8 @@
 package gov.anl.aps.cdb.portal.model.db.utilities;
 
 import gov.anl.aps.cdb.portal.model.db.entities.Item;
+import gov.anl.aps.cdb.portal.model.db.entities.ItemElement;
+import gov.anl.aps.cdb.portal.model.db.entities.ItemElementRelationship;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -31,5 +33,63 @@ public class ItemUtility {
         }
         return filteredItemList;
     }
+    
+    /**
+     * Use item elements list to generate a list of items. 
+     * 
+     * @param parentItem
+     * @return 
+     */
+    public static List<Item> getItemListFromElementsList(Item parentItem) {
+        List<Item> itemList = new ArrayList<>(); 
+        List<ItemElement> itemElementList = parentItem.getItemElementDisplayList(); 
+        if (itemElementList != null) {
+            for (ItemElement itemElement : itemElementList) {
+                Item item = itemElement.getContainedItem(); 
+                if (item != null) {
+                    itemList.add(item);
+                }
+            }
+        }                
+        
+        return itemList; 
+    }
+    
+    /** 
+     * Generate a list of items related to the item provided. 
+     * 
+     * 
+     * @param item item provided
+     * @param relationshipTypeName relationship name to look for
+     * @param itemFirstInRelationship item provided is first item element or second item element. 
+     * @return list of items found. 
+     */
+    public static List<Item> getItemsRelatedToItem(Item item, String relationshipTypeName, boolean itemFirstInRelationship) {
+        ItemElement selfElement = item.getSelfElement(); 
+        List<Item> itemList = new ArrayList<>(); 
+        
+        List<ItemElementRelationship> itemElementRelationshipList; 
+        if (itemFirstInRelationship) {
+            itemElementRelationshipList = selfElement.getItemElementRelationshipList(); 
+        } else {
+            itemElementRelationshipList = selfElement.getItemElementRelationshipList1(); 
+        }
+        
+        if (itemElementRelationshipList != null) {
+            for (ItemElementRelationship ier : itemElementRelationshipList) {
+                if (ier.getRelationshipType().getName().equals(relationshipTypeName)) {
+                    ItemElement itemElement; 
+                    if (itemFirstInRelationship) {
+                        itemElement = ier.getSecondItemElement();
+                    } else {
+                        itemElement = ier.getFirstItemElement(); 
+                    }
+                    itemList.add(itemElement.getParentItem()); 
+                }
+            }
+        }
+        
+        return itemList; 
+    }   
     
 }
