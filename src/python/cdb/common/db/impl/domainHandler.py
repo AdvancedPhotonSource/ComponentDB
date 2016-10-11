bb3dbf49d78a05c6f9c289a8515d792b37c64197
Dariusz Piotr Jarosz <djarosz@aps.anl.gov>
2016-10-11 18:37:44 +0000

    Remove domain handler references. Update to domain handler to domain for allowed entity type.

--- src/python/cdb/common/db/impl/domainHandler.py
+++ src/python/cdb/common/db/impl/domainHandler.py
@@ -1,15 +1,8 @@
 #!/usr/bin/env python
 
-from sqlalchemy import and_
-from sqlalchemy.orm.exc import NoResultFound
-
-from cdb.common.exceptions.objectAlreadyExists import ObjectAlreadyExists
-from cdb.common.exceptions.objectNotFound import ObjectNotFound
-from cdb.common.exceptions.invalidArgument import InvalidArgument
 from cdb.common.db.entities.domain import Domain
-from cdb.common.db.entities.domainHandler import DomainHandler as DomainHandlerEntity
 from cdb.common.db.impl.cdbDbEntityHandler import CdbDbEntityHandler
-from cdb.common.db.entities.allowedDomainHandlerEntityType import AllowedDomainHandlerEntityType
+from cdb.common.db.entities.allowedEntityTypeDomain import AllowedEntityTypeDomain
 from cdb.common.db.impl.entityTypeHandler import EntityTypeHandler
 
 class DomainHandler(CdbDbEntityHandler):
@@ -18,42 +11,34 @@ class DomainHandler(CdbDbEntityHandler):
         CdbDbEntityHandler.__init__(self)
         self.entityTypeHandler = EntityTypeHandler()
 
-    def findDomainHandlerByName(self, session, name):
-        return self._findDbObjByName(session, DomainHandlerEntity, name)
-
     def findDomainByName(self, session, name):
         return self._findDbObjByName(session, Domain, name)
 
-    def addDomainHandler(self, session, domainHandlerName, description):
-        return self._addSimpleNameDescriptionTable(session, DomainHandlerEntity, domainHandlerName, description)
-
-    def addDomain(self, session, domainName, description, domainHandlerName):
+    def addDomain(self, session, domainName, description):
         self._prepareAddUniqueNameObj(session, Domain, domainName)
 
         # Create Domain Handler
         dbDomain = Domain(name=domainName)
         if description:
             dbDomain.description = description
-        if domainHandlerName:
-            dbDomain.domainHandler = self.findDomainHandlerByName(session, domainHandlerName)
 
         session.add(dbDomain)
         session.flush()
         self.logger.debug('Inserted domain id %s' % dbDomain.id)
         return dbDomain
 
-    def addAllowedEntityType(self, session, domainHandlerName, entityTypeName):
-        domainHandler = self.findDomainHandlerByName(session, domainHandlerName)
+    def addAllowedEntityType(self, session, domainName, entityTypeName):
+        domain = self.findDomainByName(session, domainName)
         entityType = self.entityTypeHandler.findEntityTypeByName(session, entityTypeName)
 
-        dbAllowedDomainHandlerEntityType = AllowedDomainHandlerEntityType()
+        dbAllowedEntityTypeDomain = AllowedEntityTypeDomain()
 
-        dbAllowedDomainHandlerEntityType.domainHandler = domainHandler
-        dbAllowedDomainHandlerEntityType.entityType = entityType
+        dbAllowedEntityTypeDomain.domain = domain
+        dbAllowedEntityTypeDomain.entityType = entityType
 
 
-        session.add(dbAllowedDomainHandlerEntityType)
+        session.add(dbAllowedEntityTypeDomain)
         session.flush()
-        self.logger.debug('Inserted allowed entity type %s for domain handler %s' % (entityTypeName, domainHandlerName))
+        self.logger.debug('Inserted allowed entity type %s for domain handler %s' % (entityTypeName, domainName))
 
-        return dbAllowedDomainHandlerEntityType
+        return dbAllowedEntityTypeDomain
