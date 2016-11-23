3270c6fd7e35dd78842cef5da271624f90fb3265
Dariusz Jarosz <dariuszjarosz@me.com>
2016-11-23 08:15:48 -0600

    Utilize the new functionality of plugin manager to appropriately update configuration per deployment.

--- tools/developer_tools/cdb_plugins/deploy_plugin.py
+++ tools/developer_tools/cdb_plugins/deploy_plugin.py
@@ -3,9 +3,21 @@
 Copyright (c) UChicago Argonne, LLC. All rights reserved.
 See LICENSE file.
 """
+#
+# Adds the plugin to distribution and configures the plugin as necessary.
+#
+# Usage:
+# $0 [[CDB_DB_NAME]]
+#
 
 from utilities.plugin_manager import PluginManager
+import sys
 
-plugin_manager = PluginManager()
+cdb_db_name = sys.argv[1]
+if cdb_db_name == None:
+    cdb_db_name = 'cdb'
+
+
+plugin_manager = PluginManager(cdb_db_name)
 
 plugin_manager.prompt_deploy_plugin()
--- tools/developer_tools/cdb_plugins/update_plugin_generated_files.py
+++ tools/developer_tools/cdb_plugins/update_plugin_generated_files.py
@@ -3,11 +3,29 @@
 Copyright (c) UChicago Argonne, LLC. All rights reserved.
 See LICENSE file.
 """
+#
+# Update required auto-generated files for plugins.
+#
+# Usage:
+#
+# $0 [[CDB_DB_NAME]] [[UPDATE_CONFIGURATION (0/1)]]
+#
 
 from utilities.plugin_manager import PluginManager
+import sys
+
+cdb_db_name = 'cdb'
+if sys.argv.__len__() == 2:
+    cdb_db_name = sys.argv[1]
 
 # Storage directory is irrelevant for updating deployed plugins.
-plugin_manager = PluginManager(use_default_storage_directory=True)
+plugin_manager = PluginManager(cdb_db_name, use_default_storage_directory=True)
 
-plugin_manager.update_auto_generated_files()
+update_configuration = False
+if sys.argv.__len__() == 3:
+    input_update_configuration = sys.argv[2]
+    if input_update_configuration is not None:
+        if input_update_configuration == 1:
+            update_configuration = True
 
+plugin_manager.update_auto_generated_files(update_configuration)
