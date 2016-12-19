48a1f74e71c6c3b0924820bbb3f495483466694a
Dariusz Jarosz <dariuszjarosz@me.com>
2016-12-19 09:47:57 -0600

    Add logic for adding log entry (with attachment) and permissions verify before saving attachment.

--- src/python/cdb/cdb_web_service/impl/itemControllerImpl.py
+++ src/python/cdb/cdb_web_service/impl/itemControllerImpl.py
@@ -11,26 +11,46 @@ See LICENSE file.
 #
 
 #######################################################################
-
+from cdb.common.db.api.logDbApi import LogDbApi
 from cdb.common.objects.cdbObjectManager import CdbObjectManager
 from cdb.common.db.api.itemDbApi import ItemDbApi
+from cdb.common.utility.storageUtility import StorageUtility
+
 
 class ItemControllerImpl(CdbObjectManager):
     def __init__(self):
         CdbObjectManager.__init__(self)
         self.itemDbApi = ItemDbApi()
+        self.logDbApi = LogDbApi()
+        self.storageUtility = StorageUtility.getInstance()
 
     def getItemById(self, itemId):
         return self.itemDbApi.getItemById(itemId)
 
-    def addLogEntryForItemWithQrId(self, qrId, logEntryText, enteredByUserId):
+    def addLogEntryForItemWithQrId(self, qrId, logEntryText, enteredByUserId, attachmentName, cherryPyData):
         item = self.itemDbApi.getItemByQrId(qrId)
 
         itemId = item.data['id']
         selfElement = self.itemDbApi.getSelfElementByItemId(itemId)
         selfElementId = selfElement.data['id']
 
+        # Raises exception if user does not have sufficient privilages
+        self.itemDbApi.verifyPermissionsForWriteToItemElement(enteredByUserId, selfElementId)
+
+        # Check if attachment needs to be stored
+        storedAttachmentName = None
+        if attachmentName is not None and len(attachmentName) > 0:
+            storedAttachmentName = self.storageUtility.storeLogAttachment(cherryPyData, attachmentName)
+
         itemElementLog = self.itemDbApi.addItemElementLog(selfElementId, logEntryText, enteredByUserId)
-        return itemElementLog.data['log']
+        logEntry = itemElementLog.data['log']
 
+        if storedAttachmentName is not None:
+            logId = logEntry.data['id']
+            logAttachment = self.logDbApi.addLogAttachment(logId, storedAttachmentName, attachmentName, None)
+            del(logAttachment.data['log'])
+            logAttachmentJsonRep = logAttachment.getFullJsonRep()
+            # Refresh log entry
+            logEntry.data['logAttachmentAdded'] = logAttachmentJsonRep
 
+        return logEntry
