97a9468704bc3c2db6adaf8dce7399fd7eef5535
Dariusz Jarosz <dariuszjarosz@me.com>
2016-12-19 09:48:52 -0600

    Remove the logic for adding log entry from the service method.

--- src/python/cdb/cdb_web_service/service/itemSessionController.py
+++ src/python/cdb/cdb_web_service/service/itemSessionController.py
@@ -10,10 +10,8 @@ import shutil
 import cherrypy
 from cdb.common.exceptions.invalidRequest import InvalidRequest
 from cdb.cdb_web_service.impl.itemControllerImpl import ItemControllerImpl
-from cdb.cdb_web_service.impl.logControllerImpl import LogControllerImpl
 from cdb.common.service.cdbSessionController import CdbSessionController
 from cdb.common.utility.encoder import Encoder
-from cdb.common.utility.storageUtility import StorageUtility
 
 
 class ItemSessionController(CdbSessionController):
@@ -21,8 +19,6 @@ class ItemSessionController(CdbSessionController):
     def __init__(self):
         CdbSessionController.__init__(self)
         self.itemControllerImpl = ItemControllerImpl()
-        self.logControllerImpl = LogControllerImpl()
-        self.storageUtility = StorageUtility.getInstance()
 
     @cherrypy.expose
     @CdbSessionController.require(CdbSessionController.isLoggedIn())
@@ -33,26 +29,13 @@ class ItemSessionController(CdbSessionController):
         if not logEntry:
             raise InvalidRequest("Log entry must be provided")
 
-        # Check if attachment needs to be stored
-        storedAttachmentName = None
-        if attachmentName is not None and len(attachmentName) > 0:
-            attachmentName = Encoder.decode(attachmentName)
-            storedAttachmentName = self.storageUtility.storeLogAttachment(cherrypy.request.body, attachmentName)
-
-        logEntry = Encoder.decode(logEntry)
-
         sessionUser = self.getSessionUser()
         enteredByUserId = sessionUser.get('id')
+        attachmentName = Encoder.decode(attachmentName)
+        cherrypyData = cherrypy.request.body
+        logEntry = Encoder.decode(logEntry)
 
-        logAdded = self.itemControllerImpl.addLogEntryForItemWithQrId(qrId, logEntry, enteredByUserId)
-
-        if storedAttachmentName is not None:
-            logId = logAdded.data['id']
-            logAttachment = self.logControllerImpl.addLogAttachment(logId, storedAttachmentName, attachmentName, None)
-            del(logAttachment.data['log'])
-            logAttachmentJsonRep = logAttachment.getFullJsonRep()
-            # Refresh log entry
-            logAdded.data['logAttachmentAdded'] = logAttachmentJsonRep
+        logAdded = self.itemControllerImpl.addLogEntryForItemWithQrId(qrId, logEntry, enteredByUserId, attachmentName, cherrypyData)
 
         response = logAdded.getFullJsonRep()
         self.logger.debug('Returning log info for item with qrid %s: %s' % (qrId, response))
