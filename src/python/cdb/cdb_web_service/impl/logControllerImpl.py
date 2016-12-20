1164309228227ab9b597426c625d3618b5345719
Dariusz Jarosz <dariuszjarosz@me.com>
2016-12-20 07:52:41 -0600

    Add functionality for adding log attachments from the log controller

--- src/python/cdb/cdb_web_service/impl/logControllerImpl.py
+++ src/python/cdb/cdb_web_service/impl/logControllerImpl.py
@@ -14,14 +14,18 @@ See LICENSE file.
 
 from cdb.common.objects.cdbObjectManager import CdbObjectManager
 from cdb.common.db.api.logDbApi import LogDbApi
+from cdb.common.utility.storageUtility import StorageUtility
+
 
 class LogControllerImpl(CdbObjectManager):
     def __init__(self):
         CdbObjectManager.__init__(self)
         self.logDbApi = LogDbApi()
+        self.storageUtility = StorageUtility.getInstance()
 
-    def addLogAttachment(self, logId, attachmentName, attachmentTag, attachmentDescription):
-        return self.logDbApi.addLogAttachment(logId, attachmentName, attachmentTag, attachmentDescription)
+    def addLogAttachment(self, logId, attachmentName, attachmentDescription, cherryPyData):
+        storedAttachmentName = self.storageUtility.storeLogAttachment(cherryPyData, attachmentName)
+        return self.logDbApi.addLogAttachment(logId, storedAttachmentName, attachmentName, attachmentDescription)
 
     def getLogById(self, id):
         return self.logDbApi.getLogById(id)
