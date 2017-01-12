f14d2abaa1c7878a90a9820e0c847b7063c5f732
Dariusz Jarosz <dariuszjarosz@me.com>
2017-01-12 08:49:32 -0600

    Add paris link plugin.

---
+++ tools/developer_tools/cdb_plugins/plugins/parisLink/java/ParisLinkPropertyTypeHandler.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) UChicago Argonne, LLC. All rights reserved.
+ * See LICENSE file.
+ */
+package gov.anl.aps.cdb.portal.plugins.support.parisLink;
+
+import gov.anl.aps.cdb.portal.constants.DisplayType;
+import gov.anl.aps.cdb.portal.model.db.entities.PropertyValue;
+import gov.anl.aps.cdb.portal.model.db.entities.PropertyValueHistory;
+import gov.anl.aps.cdb.portal.model.jsf.handlers.AbstractPropertyTypeHandler;
+
+/**
+ * PARIS link property type handler.
+ */
+public class ParisLinkPropertyTypeHandler extends AbstractPropertyTypeHandler {
+
+    public static final String HANDLER_NAME = "PARIS Link";
+
+    private static final String ParisUrl = ParisLinkPluginManager.getParisUrlString(); 
+
+    public ParisLinkPropertyTypeHandler() {
+        super(HANDLER_NAME, DisplayType.HTTP_LINK);
+    }
+
+    public static String formatParisLink(String poId) {     
+        if (poId == null) {
+            return null;
+        }
+
+        String url = ParisUrl.replace("PO_ID", poId);
+        return url;
+    }
+
+    @Override
+    public void setTargetValue(PropertyValue propertyValue) {
+        String targetLink = formatParisLink(propertyValue.getValue());
+        propertyValue.setTargetValue(targetLink);
+    }
+
+    @Override
+    public void setTargetValue(PropertyValueHistory propertyValueHistory) {
+        String targetLink = formatParisLink(propertyValueHistory.getValue());
+        propertyValueHistory.setTargetValue(targetLink);
+    }
+}
