e86ce5ab61e5bc9fcb289003b10dc42b6dd6b021
Dariusz Jarosz <dariuszjarosz@me.com>
2017-01-27 10:25:53 -0600

    Add functionality to rest api to fetch logs by item id, fetch catalog items, fetch item properties. get items derived from item.

--- src/python/cdb/cdb_web_service/service/itemController.py
+++ src/python/cdb/cdb_web_service/service/itemController.py
@@ -36,3 +36,42 @@ class ItemController(CdbController):
         response = self.listToJson(logs)
         self.logger.debug('Returning log entries for item with qrid: %s' % qrId)
         return response
+
+    @cherrypy.expose
+    @CdbController.execute
+    def getItemLogsById(self, itemId):
+        if not itemId:
+            raise InvalidRequest("Invalid item id provided")
+
+        logs = self.itemControllerImpl.getLogEntriesForItemWithId(itemId)
+        response = self.listToJson(logs)
+        self.logger.debug('Returning log entries for item with id: %s' % itemId)
+        return response
+
+    @cherrypy.expose
+    @CdbController.execute
+    def getCatalogItems(self):
+        catalogItems = self.itemControllerImpl.getCatalogItems()
+        response = self.listToJson(catalogItems)
+        return response
+
+    @cherrypy.expose
+    @CdbController.execute
+    def getItemsDerivedFromItem(self, derivedFromItemId):
+        if not derivedFromItemId:
+            raise InvalidRequest("Invalid derived from item id provided")
+
+        catalogItems = self.itemControllerImpl.getItemsDerivedFromItemId(derivedFromItemId)
+        response = self.listToJson(catalogItems)
+        return response
+
+    @cherrypy.expose
+    @CdbController.execute
+    def getPropertiesForItemByItemId(self, itemId):
+        if not itemId:
+            raise InvalidRequest("Invalid item id provided")
+
+        properties = self.itemControllerImpl.getPropertiesForItemId(itemId)
+        response = self.listToJson(properties)
+        return response
+
