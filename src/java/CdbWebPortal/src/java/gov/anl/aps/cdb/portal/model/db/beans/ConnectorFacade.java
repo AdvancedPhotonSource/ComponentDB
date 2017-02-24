3c5b4e6168d1aca2076eb369399d89d4bb3bd4fa
Dariusz Jarosz <dariuszjarosz@me.com>
2017-02-24 15:06:40 -0600

    Add a stored procedure to get available connectors for a particular inventory item.

--- src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/model/db/beans/ConnectorFacade.java
+++ src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/model/db/beans/ConnectorFacade.java
@@ -5,9 +5,14 @@
 package gov.anl.aps.cdb.portal.model.db.beans;
 
 import gov.anl.aps.cdb.portal.model.db.entities.Connector;
+import gov.anl.aps.cdb.portal.model.db.entities.ConnectorType;
+import gov.anl.aps.cdb.portal.model.db.entities.Item;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import javax.persistence.StoredProcedureQuery;
 
 /**
  *
@@ -26,6 +31,26 @@ public class ConnectorFacade extends CdbEntityFacade<Connector> {
 
     public ConnectorFacade() {
         super(Connector.class);
+    }
+    
+    public List<Connector> getAvailableConnectorsForInventoryItem(Item item, ConnectorType connectorType, Boolean is_male) {
+        try {
+           StoredProcedureQuery query = em.createNamedStoredProcedureQuery("connector.available_connectors_for_inventory_item"); 
+           query.setParameter("item_id", item.getId()); 
+           
+           Integer connectorTypeId = null; 
+           
+           if (connectorType != null){
+               connectorTypeId = connectorType.getId(); 
+           }
+           
+           query.setParameter("connector_type_id", connectorTypeId);
+           query.setParameter("is_male", is_male);
+           
+           return query.getResultList(); 
+        } catch (NoResultException ex) {
+        }
+        return null;
     }
     
 }
