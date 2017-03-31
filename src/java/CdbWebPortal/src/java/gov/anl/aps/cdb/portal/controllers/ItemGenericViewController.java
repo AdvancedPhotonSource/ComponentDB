ec3d4c715dbfd06b59a48aba88017fd8cc9c253d
Dariusz Jarosz <dariuszjarosz@me.com>
2017-03-31 11:08:13 -0500

    Resolve a unsupported exception.

--- src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/controllers/ItemGenericViewController.java
+++ src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/controllers/ItemGenericViewController.java
@@ -158,7 +158,7 @@ public class ItemGenericViewController extends ItemController<Item, ItemFacade> 
      */
     @Override
     public String getDefaultDomainName() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return null; 
     }
 
     @Override
