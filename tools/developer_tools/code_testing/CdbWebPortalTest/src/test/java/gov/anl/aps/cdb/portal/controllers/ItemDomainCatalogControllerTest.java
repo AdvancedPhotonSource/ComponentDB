5d2efca88065a87550c01f3070b36cb347ac937e
Dariusz Jarosz <dariuszjarosz@me.com>
2017-04-07 14:40:31 -0500

    Fix the unit test issue that caused a failure.

--- tools/developer_tools/code_testing/CdbWebPortalTest/src/test/java/gov/anl/aps/cdb/portal/controllers/ItemDomainCatalogControllerTest.java
+++ tools/developer_tools/code_testing/CdbWebPortalTest/src/test/java/gov/anl/aps/cdb/portal/controllers/ItemDomainCatalogControllerTest.java
@@ -33,8 +33,8 @@ public class ItemDomainCatalogControllerTest extends CdbDBTest {
     }
 
     @Test
-    public void checkItemFacadeWasSet() {
-        assertNotNull(itemDomainCatalogController.getItem(1));
+    public void checkItemFacadeWasSet() {                
+        assertNotNull(itemDomainCatalogController.getEntityDbFacade().findAll());
     }
     
     @Test
