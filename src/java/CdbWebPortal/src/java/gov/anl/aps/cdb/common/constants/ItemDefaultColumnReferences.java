bf4f231a5c766087cd0eba335743d5088394c206
Dariusz Jarosz <dariuszjarosz@me.com>
2017-07-13 09:45:19 -0500

    Add an enum that provides a mechanism for reliable item column selection from view to controller.

---
+++ src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/common/constants/ItemDefaultColumnReferences.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) UChicago Argonne, LLC. All rights reserved.
+ * See LICENSE file.
+ */
+package gov.anl.aps.cdb.common.constants;
+
+public enum ItemDefaultColumnReferences {
+    name("name"),
+    itemIdentifier1("itemIdentifier1"),
+    itemIdentifier2("itemIdentifier2"),
+    itemProject("itemProject"),
+    description("description"),
+    qrId("qrId"),
+    derivedFromItem("derivedFromItem"),
+    ownerUser("ownerUser"),
+    ownerGroup("ownerGroup"),
+    groupWriteable("groupWriteable");
+
+    private String value;
+
+    private ItemDefaultColumnReferences(String value) {
+        this.value = value;
+    }
+
+    public final String getValue() {
+        return value;
+    }
+}
