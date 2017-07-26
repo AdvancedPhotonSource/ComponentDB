caab154f5f4731ebe5c4e054ca184f8f40ec1b62
Dariusz Jarosz <dariuszjarosz@me.com>
2017-07-26 12:59:22 -0400

    Resolve issues with installing routes

--- support/bin/install_python_packages.sh
+++ support/bin/install_python_packages.sh
@@ -15,7 +15,7 @@ $binDir/install_python_ldap.sh || exit 1
 $binDir/install_pip.sh || exit 1
 $binDir/install_twine.sh || exit 1
 $binDir/build_cherrypy.sh || exit 1
-$binDir/build_routes.sh || exit 1
+$binDir/install_routes.sh || exit 1
 $binDir/build_sqlalchemy.sh || exit 1
 $binDir/build_mysql_python.sh || exit 1
 $binDir/build_suds.sh || exit 1
