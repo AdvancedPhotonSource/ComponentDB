918a49eebf1ac2dd957dee1dca7c83d8c9b6051a
Dariusz Jarosz <dariuszjarosz@me.com>
2017-07-26 13:04:21 -0400

    Update clean populate to work with current db version

--- db/sql/clean/populate_property_type.sql
+++ db/sql/clean/populate_property_type.sql
@@ -6,12 +6,12 @@
 LOCK TABLES `property_type` WRITE;
 /*!40000 ALTER TABLE `property_type` DISABLE KEYS */;
 INSERT INTO `property_type` VALUES
-(1,'Image',NULL,1,2,NULL,NULL,0,0,0,1,1),
-(2,'Document (Upload)','',1,1,'','',0,0,0,1,1),
-(3,'Traveler Template (Electronic)','Allows integration of traveler templates from traveler application.',2,7,'','',0,0,0,1,1),
-(4,'Traveler Instance (Electronic)','Allows integration of traveler instances from traveler application.',2,8,'','',0,0,0,1,1),
-(5,'Item Element Row Color','',4,NULL,'','',0,0,0,1,1),
-(6,'Spare Parts Configuration','',3,NULL,'','',0,0,1,1,1),
-(7,'Spare Part Indication','',3,NULL,'','',0,0,1,1,1);
+(1,'Image',NULL,NULL,1,2,NULL,NULL,0,0,0,1,1),
+(2,'Document (Upload)','',NULL,1,1,'','',0,0,0,1,1),
+(3,'Traveler Template (Electronic)','Allows integration of traveler templates from traveler application.',NULL,2,7,'','',0,0,0,1,1),
+(4,'Traveler Instance (Electronic)','Allows integration of traveler instances from traveler application.',NULL,2,8,'','',0,0,0,1,1),
+(5,'Item Element Row Color','',NULL,4,NULL,'','',0,0,0,1,1),
+(6,'Spare Parts Configuration','',NULL,3,NULL,'','',0,0,1,1,1),
+(7,'Spare Part Indication','',NULL,3,NULL,'','',0,0,1,1,1);
 /*!40000 ALTER TABLE `property_type` ENABLE KEYS */;
 UNLOCK TABLES;
