3153f8d0f74bdc78c865949512274fe3eace3d3d
Dariusz Jarosz <dariuszjarosz@me.com>
2017-08-29 08:06:09 -0500

    Resolve the issue with the double download when application is deployed onto server.

--- tools/developer_tools/cdb_plugins/plugins/dm/java/DmPropertyTypeHandler.java
+++ tools/developer_tools/cdb_plugins/plugins/dm/java/DmPropertyTypeHandler.java
@@ -9,9 +9,12 @@ import gov.anl.aps.cdb.portal.model.db.entities.PropertyValue;
 import gov.anl.aps.cdb.portal.model.jsf.handlers.AbstractPropertyTypeHandler;
 import gov.anl.aps.dm.api.ExperimentDsApi;
 import gov.anl.aps.dm.common.exceptions.ConfigurationError;
+import gov.anl.aps.dm.common.exceptions.DmException;
+import gov.anl.aps.dm.common.exceptions.ObjectNotFound;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.util.logging.Level;
 import org.apache.log4j.Logger;
 import org.primefaces.model.DefaultStreamedContent;
 import org.primefaces.model.StreamedContent;
@@ -37,16 +40,12 @@ public class DmPropertyTypeHandler extends AbstractPropertyTypeHandler {
     private ExperimentDsApi client;
 
     public DmPropertyTypeHandler() {
-        super(HANDLER_NAME, DisplayType.FILE_DOWNLOAD);
-        try {
-            client = new ExperimentDsApi(SERVICE_URL, SERVICE_USER, SERVICE_PASS);
-        } catch (ConfigurationError ex) {
-            logger.error(ex);
-        }
+        super(HANDLER_NAME, DisplayType.FILE_DOWNLOAD);        
     }
     
     public StreamedContent fileDownloadActionCommand(PropertyValue propertyValue) {               
         try {
+            client = new ExperimentDsApi(SERVICE_URL, SERVICE_USER, SERVICE_PASS);
             ByteArrayOutputStream responseOutputStream = new ByteArrayOutputStream();            
             String experimentName = propertyValue.getPropertyMetadataValueForKey(EXPERIMENT_NAME_KEY);
             String experimentFilePath = propertyValue.getPropertyMetadataValueForKey(EXPERIMENT_FILE_PATH_KEY);            
@@ -55,9 +54,14 @@ public class DmPropertyTypeHandler extends AbstractPropertyTypeHandler {
             InputStream pipedInputStream = new ByteArrayInputStream(responseOutputStream.toByteArray());
             logger.debug("Serving file download for: " + fileName);
             return new DefaultStreamedContent(pipedInputStream, MIME_CONENT_TYPE, fileName);
-        } catch (Exception ex) {
+        } catch (ConfigurationError ex) {
             logger.error("ERROR: " + ex.getMessage());
-        }        
+        } catch (ObjectNotFound ex) {
+            logger.error("ERROR: " + ex.getMessage());
+        } catch (DmException ex) {
+            logger.error("ERROR: " + ex.getMessage());
+        }            
+                
         return null; 
     }
 
