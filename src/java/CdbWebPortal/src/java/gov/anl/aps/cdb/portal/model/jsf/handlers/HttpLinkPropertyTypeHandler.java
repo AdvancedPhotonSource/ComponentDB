7324f3e1410b3baaca49a9c958678aef360465d0
Dariusz Jarosz <dariuszjarosz@me.com>
2017-10-26 08:40:46 -0500

    Allow user to format thier url with a wiki style syntax.

--- src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/model/jsf/handlers/HttpLinkPropertyTypeHandler.java
+++ src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/model/jsf/handlers/HttpLinkPropertyTypeHandler.java
@@ -14,6 +14,8 @@ import gov.anl.aps.cdb.portal.model.db.entities.PropertyValueHistory;
 public class HttpLinkPropertyTypeHandler extends AbstractPropertyTypeHandler {
 
     public static final String HANDLER_NAME = "HTTP Link";
+    
+    private String[] lastProcessedValue = null; 
 
     public HttpLinkPropertyTypeHandler() {
         super(HANDLER_NAME, DisplayType.HTTP_LINK);
@@ -21,13 +23,63 @@ public class HttpLinkPropertyTypeHandler extends AbstractPropertyTypeHandler {
     
     @Override
     public void setDisplayValue(PropertyValue propertyValue) {
-        String linkValue = shortenHttpLinkDisplayValueIfNeeded(propertyValue.getValue());
+        String linkValue = getDisplayValueForLink(propertyValue.getValue()); 
         propertyValue.setDisplayValue(linkValue);
     }
 
     @Override
     public void setDisplayValue(PropertyValueHistory propertyValueHistory) {
-        String linkValue = shortenHttpLinkDisplayValueIfNeeded(propertyValueHistory.getValue());
+        String linkValue = getDisplayValueForLink(propertyValueHistory.getValue());
         propertyValueHistory.setDisplayValue(linkValue);
+    }
+
+    @Override
+    public void setTargetValue(PropertyValue propertyValue) {
+        propertyValue.setTargetValue(getTargetValueForLink(propertyValue.getValue()));
+    }
+    
+    @Override
+    public void setTargetValue(PropertyValueHistory propertyValueHistory) {
+        propertyValueHistory.setTargetValue(getTargetValueForLink(propertyValueHistory.getValue()));
+    }
+    
+    private String getDisplayValueForLink(String value) {
+        return getAppropriateValueForLink(value, 1);
+    }
+    
+    private String getTargetValueForLink(String value) {
+        return getAppropriateValueForLink(value, 0);
+    }
+    
+    /**
+     * Checks for wiki link format otherwise returns just a standard formatted link. 
+     * 
+     * @param value
+     * @param index - 0 for link, 1 for display 
+     * @return 
+     */
+    private String getAppropriateValueForLink(String value, int index) {
+        if (lastProcessedValue == null || !lastProcessedValue[0].equals(value)) {
+            lastProcessedValue = null; 
+        }
+        
+        if (lastProcessedValue == null) {
+            lastProcessedValue = new String[3]; 
+            lastProcessedValue[0] = value; 
+
+            if (value.contains(" | ") && value.startsWith("[") && value.endsWith("]")) {
+                value = value.substring(1, value.length() -1); 
+                
+                String[] stringSplit = value.split(" \\| "); 
+                
+                lastProcessedValue[1] = stringSplit[0];
+                lastProcessedValue[2] = stringSplit[1]; 
+            } else {
+                lastProcessedValue[1] = value; 
+                lastProcessedValue[2] = shortenHttpLinkDisplayValueIfNeeded(value);
+            }     
+        }
+        
+        return lastProcessedValue[index+1];  
     }
 }
