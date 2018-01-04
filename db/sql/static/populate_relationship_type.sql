19f9e481cb9a0fb42b60297346663f5eb8921e67
Dariusz Jarosz <dariuszjarosz@me.com>
2018-01-04 09:37:36 -0600

    Add relationsihip type tables to static tables. Closes #62

---
+++ db/sql/static/populate_relationship_type.sql
@@ -0,0 +1,6 @@
+LOCK TABLES `relationship_type` WRITE;
+/*!40000 ALTER TABLE `relationship_type` DISABLE KEYS */;
+INSERT INTO `relationship_type` VALUES
+(1,'Location','Location Relationship',1);
+/*!40000 ALTER TABLE `relationship_type` ENABLE KEYS */;
+UNLOCK TABLES;
