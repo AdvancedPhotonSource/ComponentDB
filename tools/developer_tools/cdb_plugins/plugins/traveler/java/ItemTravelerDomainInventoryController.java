4f3f5f55fe21d73849fe3df6d4c5b0c5d099a208
Dariusz Jarosz <dariuszjarosz@me.com>
2018-01-19 14:02:44 -0600

    Add functionality for storing if traveler instance column should be displayed.

--- tools/developer_tools/cdb_plugins/plugins/traveler/java/ItemTravelerDomainInventoryController.java
+++ tools/developer_tools/cdb_plugins/plugins/traveler/java/ItemTravelerDomainInventoryController.java
@@ -6,7 +6,11 @@ package gov.anl.aps.cdb.portal.plugins.support.traveler;
 
 import gov.anl.aps.cdb.portal.controllers.ItemController;
 import gov.anl.aps.cdb.portal.controllers.ItemDomainInventoryController;
+import gov.anl.aps.cdb.portal.model.db.entities.Item;
+import gov.anl.aps.cdb.portal.plugins.support.traveler.objects.Form;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
@@ -20,6 +24,8 @@ public class ItemTravelerDomainInventoryController extends ItemTravelerControlle
     
     public final static String controllerNamed = "itemTravelerDomainInventoryController";
     
+    private boolean isDisplayMultiEditTravelerInstance = false; 
+    
     private ItemDomainInventoryController itemDomainInventoryController = null; 
 
     @Override
@@ -30,9 +36,17 @@ public class ItemTravelerDomainInventoryController extends ItemTravelerControlle
                 
         return itemDomainInventoryController; 
     }
+
+    public boolean isIsDisplayMultiEditTravelerInstance() {
+        return isDisplayMultiEditTravelerInstance;
+    }
+
+    public void setIsDisplayMultiEditTravelerInstance(boolean isDisplayMultiEditTravelerInstance) {
+        this.isDisplayMultiEditTravelerInstance = isDisplayMultiEditTravelerInstance;
+    }
     
     public boolean getIsCollapsedTravelerInstances() {                
         return !(getTravelersForCurrent().size() > 0); 
-    }
+    }        
     
 }
