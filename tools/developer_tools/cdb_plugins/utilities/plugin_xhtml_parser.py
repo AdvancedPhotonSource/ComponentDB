06d22f2f8e30d44f2aeaacec5b0b827d2fd29f18
Dariusz Jarosz <dariuszjarosz@me.com>
2018-01-22 13:04:41 -0600

    Update the naming of plugins and move the configuration tabs to the application and not plugin. Update plugin framework to create new files for new functionality. closes #49 closes #55 closes #44 closes #47

--- tools/developer_tools/cdb_plugins/utilities/plugin_xhtml_parser.py
+++ tools/developer_tools/cdb_plugins/utilities/plugin_xhtml_parser.py
@@ -10,19 +10,37 @@ ABOUT_PAGE_PLUGIN_DIALOGS_XHTML_FILENAME = 'aboutPagePluginDialogs.xhtml'
 ABOUT_PAGE_PLUGIN_LINKS_XHTML_FILENAME = 'aboutPagePluginLinks.xhtml'
 ITEM_DOMAIN_CATALOG_LIST_ACTION_BUTTONS_XHTML_FILENAME = 'itemDomainCatalogListActionButtons.xhtml'
 ITEM_DOMAIN_CATALOG_LIST_DIALOGS_XHTML_FILENAME = 'itemDomainCatalogListDialogs.xhtml'
+ITEM_DOMAIN_CATALOG_DETAIL_VIEW_SECTIONS = 'catalogDetailsViewSections.xhtml'
+ITEM_DOMAIN_INVENTORY_DETAIL_VIEW_SECTIONS = 'inventoryDetailsViewSections.xhtml'
 PROPERTY_INFO_ACTION_DIALOGS_XHTML_FILENAME = 'propertyInfoActionDialogs.xhtml'
 PROPERTY_EDIT_LIST_SUPPORT_FILENAME = 'propertyEditListSupport.xhtml'
 PROPERTY_EDIT_OBJECT_SUPPORT_FILENAME = 'propertyEditObjectSupport.xhtml'
 PROPERTY_EDIT_OBJECT_VALUES_FILENAME = 'propertyEditObjectValues.xhtml'
+MULTI_EDIT_CATALOG_CONFIGURATION_FILENAME = 'catalogMultiEditConfigurations.xhtml'
+MULTI_EDIT_INVENTORY_CONFIGURATION_FILENAME = 'inventoryMultiEditConfigurations.xhtml'
+MULTI_EDIT_CATALOG_LIST_OBJECT_FILENAME = 'catalogMultiEditEditableListObjects.xhtml'
+MULTI_EDIT_INVENTORY_LIST_OBJECT_FILENAME = 'inventoryMultiEditEditableListObjects.xhtml'
+MULTI_EDIT_CATALOG_LIST_SUPPORT_FILENAME = 'catalogMultiEditEditableListSupports.xhtml'
+MULTI_EDIT_INVENTORY_LIST_SUPPORT_FILENAME = 'inventoryMultiEditEditableListSupports.xhtml'
 
 PLUGIN_ABOUT_DIALOGS_PATH = 'about/dialogs.xhtml'
 PLUGIN_ABOUT_LINKS_PATH = 'about/links.xhtml'
 PLUGIN_ITEM_DOMAIN_CATALOG_LIST_DIALOGS_PATH = 'item/domainCatalogListDialogs.xhtml'
 PLUGIN_ITEM_DOMAIN_CATALOG_LIST_ACTION_BUTTONS_PATH = 'item/domainCatalogListButtons.xhtml'
+PLUGIN_ITEM_DETAIL_VIEW_SECTION_BASE_PATH = 'itemDetailsViewSections'
+PLUGIN_ITEM_DOMAIN_CATALOG_VIEW_SECTION_PATH = '%s/catalogSections.xhtml' % PLUGIN_ITEM_DETAIL_VIEW_SECTION_BASE_PATH
+PLUGIN_ITEM_DOMAIN_INVENTORY_VIEW_SECTION_PATH = '%s/inventorySections.xhtml' % PLUGIN_ITEM_DETAIL_VIEW_SECTION_BASE_PATH
 PLUGIN_PROPERTY_INFO_ACTION_DIALOGS_PATH = 'propertyValue/infoActionDialogs.xhtml'
 PLUGIN_PROPERTY_EDIT_LIST_SUPPORT_PATH = 'propertyValue/editListSupport.xhtml'
 PLUGIN_PROPERTY_EDIT_OBJECT_SUPPORT_PATH = 'propertyValue/editObjectSupport.xhtml'
 PLUGIN_PROPERTY_EDIT_OBJECT_VALUES_PATH = 'propertyValue/editObjectValues.xhtml'
+PLUGIN_MULTI_EDIT_BASE_PATH = 'itemMultiEdit'
+PLUGIN_MULTI_EDIT_CATALOG_CONFIGURATION_PATH = '%s/catalogConfiguration.xhtml' % PLUGIN_MULTI_EDIT_BASE_PATH
+PLUGIN_MULTI_EDIT_INVENTORY_CONFIGURATION_PATH = '%s/inventoryConfiguration.xhtml' % PLUGIN_MULTI_EDIT_BASE_PATH
+PLUGIN_MULTI_EDIT_CATALOG_LIST_OBJECT_PATH = '%s/catalogEditableListObject.xhtml' % PLUGIN_MULTI_EDIT_BASE_PATH
+PLUGIN_MULTI_EDIT_INVENTORY_LIST_OBJECT_PATH = '%s/inventoryEditableListObject.xhtml' % PLUGIN_MULTI_EDIT_BASE_PATH
+PLUGIN_MULTI_EDIT_CATALOG_LIST_SUPPORT_PATH = '%s/catalogEditableListSupport.xhtml' % PLUGIN_MULTI_EDIT_BASE_PATH
+PLUGIN_MULTI_EDIT_INVENTORY_LIST_SUPPORT_PATH = '%s/inventoryEditableListSupport.xhtml' % PLUGIN_MULTI_EDIT_BASE_PATH
 
 UI_INCLUDE_SYNTAX = '   <ui:include src="%s"/>\n'
 
@@ -41,10 +59,19 @@ class PluginXhtmlParser():
         about_page_links = ''
         item_domain_catalog_list_dialogs = ''
         item_domain_catalog_list_action_buttons = ''
+        item_domain_catalog_view_sections = ''
+        item_domain_inventory_view_sections = ''
         property_info_action_dialogs = ''
         property_edit_list_support = ''
         property_edit_object_support = ''
         property_edit_object_values = ''
+
+        multi_edit_catalog_configurations = ''
+        multi_edit_inventory_configurations = ''
+        multi_edit_catalog_list_objects = ''
+        multi_edit_inventory_list_objects = ''
+        multi_edit_catalog_list_supports = ''
+        multi_edit_inventory_list_supports = ''
 
         for cdb_plugin in installed_plugins:
             plugin_name = cdb_plugin.plugin_name
@@ -55,10 +82,21 @@ class PluginXhtmlParser():
             item_domain_catalog_list_dialogs += self.generate_ui_inlcude(PLUGIN_ITEM_DOMAIN_CATALOG_LIST_DIALOGS_PATH, plugin_name)
             item_domain_catalog_list_action_buttons += self.generate_ui_inlcude(PLUGIN_ITEM_DOMAIN_CATALOG_LIST_ACTION_BUTTONS_PATH, plugin_name)
 
+            item_domain_catalog_view_sections += self.generate_ui_inlcude(PLUGIN_ITEM_DOMAIN_CATALOG_VIEW_SECTION_PATH, plugin_name)
+            item_domain_inventory_view_sections += self.generate_ui_inlcude(PLUGIN_ITEM_DOMAIN_INVENTORY_VIEW_SECTION_PATH, plugin_name)
+
             property_info_action_dialogs += self.generate_ui_inlcude(PLUGIN_PROPERTY_INFO_ACTION_DIALOGS_PATH, plugin_name)
             property_edit_list_support += self.generate_ui_inlcude(PLUGIN_PROPERTY_EDIT_LIST_SUPPORT_PATH, plugin_name)
             property_edit_object_support += self.generate_ui_inlcude(PLUGIN_PROPERTY_EDIT_OBJECT_SUPPORT_PATH, plugin_name)
             property_edit_object_values += self.generate_ui_inlcude(PLUGIN_PROPERTY_EDIT_OBJECT_VALUES_PATH, plugin_name)
+
+            multi_edit_catalog_configurations += self.generate_ui_inlcude(PLUGIN_MULTI_EDIT_CATALOG_CONFIGURATION_PATH, plugin_name)
+            multi_edit_inventory_configurations += self.generate_ui_inlcude(PLUGIN_MULTI_EDIT_INVENTORY_CONFIGURATION_PATH, plugin_name)
+            multi_edit_catalog_list_objects += self.generate_ui_inlcude(PLUGIN_MULTI_EDIT_CATALOG_LIST_OBJECT_PATH, plugin_name)
+            multi_edit_inventory_list_objects += self.generate_ui_inlcude(PLUGIN_MULTI_EDIT_INVENTORY_LIST_OBJECT_PATH, plugin_name)
+            multi_edit_catalog_list_supports += self.generate_ui_inlcude(PLUGIN_MULTI_EDIT_CATALOG_LIST_SUPPORT_PATH, plugin_name)
+            multi_edit_inventory_list_supports += self.generate_ui_inlcude(PLUGIN_MULTI_EDIT_INVENTORY_LIST_SUPPORT_PATH, plugin_name)
+
 
         xhtml_template_file_path = "%s/%s" % (PLUGIN_TEMPLATE_PATH, PLUGIN_XHTML_TEMPLATE_FILENAME)
         dir = os.path.dirname(__file__)
@@ -76,6 +114,10 @@ class PluginXhtmlParser():
             XHTML_TEMPLATE_UI_INCLUDE_KEY, item_domain_catalog_list_action_buttons)
         result[ITEM_DOMAIN_CATALOG_LIST_DIALOGS_XHTML_FILENAME] = xhtml_template_file_content.replace(
             XHTML_TEMPLATE_UI_INCLUDE_KEY, item_domain_catalog_list_dialogs)
+        result[ITEM_DOMAIN_CATALOG_DETAIL_VIEW_SECTIONS] = xhtml_template_file_content.replace(
+            XHTML_TEMPLATE_UI_INCLUDE_KEY, item_domain_catalog_view_sections)
+        result[ITEM_DOMAIN_INVENTORY_DETAIL_VIEW_SECTIONS] = xhtml_template_file_content.replace(
+            XHTML_TEMPLATE_UI_INCLUDE_KEY, item_domain_inventory_view_sections)
         result[PROPERTY_INFO_ACTION_DIALOGS_XHTML_FILENAME] = xhtml_template_file_content.replace(
             XHTML_TEMPLATE_UI_INCLUDE_KEY, property_info_action_dialogs)
         result[PROPERTY_EDIT_LIST_SUPPORT_FILENAME] = xhtml_template_file_content.replace(
@@ -84,6 +126,18 @@ class PluginXhtmlParser():
             XHTML_TEMPLATE_UI_INCLUDE_KEY, property_edit_object_support)
         result[PROPERTY_EDIT_OBJECT_VALUES_FILENAME] = xhtml_template_file_content.replace(
             XHTML_TEMPLATE_UI_INCLUDE_KEY, property_edit_object_values)
+        result[MULTI_EDIT_CATALOG_CONFIGURATION_FILENAME] = xhtml_template_file_content.replace(
+            XHTML_TEMPLATE_UI_INCLUDE_KEY, multi_edit_catalog_configurations)
+        result[MULTI_EDIT_INVENTORY_CONFIGURATION_FILENAME] = xhtml_template_file_content.replace(
+            XHTML_TEMPLATE_UI_INCLUDE_KEY, multi_edit_inventory_configurations)
+        result[MULTI_EDIT_CATALOG_LIST_OBJECT_FILENAME] = xhtml_template_file_content.replace(
+            XHTML_TEMPLATE_UI_INCLUDE_KEY, multi_edit_catalog_list_objects)
+        result[MULTI_EDIT_INVENTORY_LIST_OBJECT_FILENAME] = xhtml_template_file_content.replace(
+            XHTML_TEMPLATE_UI_INCLUDE_KEY, multi_edit_inventory_list_objects)
+        result[MULTI_EDIT_CATALOG_LIST_SUPPORT_FILENAME] = xhtml_template_file_content.replace(
+            XHTML_TEMPLATE_UI_INCLUDE_KEY, multi_edit_catalog_list_supports)
+        result[MULTI_EDIT_INVENTORY_LIST_SUPPORT_FILENAME] = xhtml_template_file_content.replace(
+            XHTML_TEMPLATE_UI_INCLUDE_KEY, multi_edit_inventory_list_supports)
 
         return result
 
