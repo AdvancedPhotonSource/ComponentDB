8c9b8c518e6150371fe15b96e99d51c5dcf5f57a
Dariusz Jarosz <dariuszjarosz@me.com>
2018-03-19 13:59:18 -0500

    Add functionality to fetch if an item is a template.

--- src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/model/db/entities/ItemDomainMachineDesign.java
+++ src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/model/db/entities/ItemDomainMachineDesign.java
@@ -4,7 +4,9 @@
  */
 package gov.anl.aps.cdb.portal.model.db.entities;
 
+import gov.anl.aps.cdb.portal.constants.EntityTypeName;
 import gov.anl.aps.cdb.portal.constants.ItemDomainName;
+import java.util.List;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
@@ -13,12 +15,35 @@ import javax.persistence.Entity;
  * @author djarosz
  */
 @Entity
-@DiscriminatorValue(value = ItemDomainName.MACHINE_DESIGN_ID + "")  
+@DiscriminatorValue(value = ItemDomainName.MACHINE_DESIGN_ID + "")
 public class ItemDomainMachineDesign extends Item {
+
+    private transient Boolean isItemTemplate = null;
 
     @Override
     public Item createInstance() {
-        return new ItemDomainMachineDesign(); 
+        return new ItemDomainMachineDesign();
     }
-    
+
+    public Boolean getIsItemTemplate() {
+        if (isItemTemplate == null) {
+            isItemTemplate = isItemTemplate(this);
+        }
+        return isItemTemplate;
+    }
+
+    public static boolean isItemTemplate(ItemDomainMachineDesign item) {
+        if (item != null) {
+            List<EntityType> entityTypeList = item.getEntityTypeList();
+            if (entityTypeList != null) {
+                for (EntityType entityType : entityTypeList) {
+                    if (entityType.getName().equals(EntityTypeName.template.getValue())) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
 }
