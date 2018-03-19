6fdfde1e9f785657fc27372b942d9639062b20e5
Dariusz Jarosz <dariuszjarosz@me.com>
2018-03-19 11:26:16 -0500

    Add a relationship type for relating machine desin items and templates from which they were created.

--- db/sql/static/populate_relationship_type.sql
+++ db/sql/static/populate_relationship_type.sql
@@ -2,6 +2,7 @@ LOCK TABLES `relationship_type` WRITE;
 /*!40000 ALTER TABLE `relationship_type` DISABLE KEYS */;
 INSERT INTO `relationship_type` VALUES
 (1,'Location','Location Relationship',1),
-(2,'MAARC Connection','MAARC connection relationship to inventory items.',NULL);
+(2,'MAARC Connection','MAARC connection relationship to inventory items.',NULL),
+(3,'machineDesignTemplate','',NULL);
 /*!40000 ALTER TABLE `relationship_type` ENABLE KEYS */;
 UNLOCK TABLES;
