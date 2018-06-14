0df05fe906bd1d43e8debb8ef37445d17734f796
Dariusz Jarosz <dariuszjarosz@me.com>
2018-06-14 13:26:27 -0500

    Update the template entity type name for more generic use.

--- db/sql/static/populate_allowed_entity_type_domain.sql
+++ db/sql/static/populate_allowed_entity_type_domain.sql
@@ -5,6 +5,8 @@ INSERT INTO `allowed_entity_type_domain` VALUES
 (2,2),
 (5,3),
 (5,4),
+(2,5),
+(3,5),
 (6,5);
 /*!40000 ALTER TABLE `allowed_entity_type_domain` ENABLE KEYS */;
 UNLOCK TABLES;
--- db/sql/static/populate_relationship_type.sql
+++ db/sql/static/populate_relationship_type.sql
@@ -3,6 +3,6 @@ LOCK TABLES `relationship_type` WRITE;
 INSERT INTO `relationship_type` VALUES
 (1,'Location','Location Relationship',1),
 (2,'MAARC Connection','MAARC connection relationship to inventory items.',NULL),
-(3,'machineDesignTemplate','',NULL);
+(3,'Created From Template','',NULL);
 /*!40000 ALTER TABLE `relationship_type` ENABLE KEYS */;
 UNLOCK TABLES;
