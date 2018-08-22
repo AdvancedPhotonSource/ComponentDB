50e3da7a117d5324f13fc4ba97cad7a1b0cb1bf8
Dariusz Jarosz <dariuszjarosz@me.com>
2018-08-22 14:14:34 -0500

    Add combined item element lists view to machine design to show elements of catalog assembly and machine design elements.

--- src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/model/db/entities/ItemDomainMachineDesign.java
+++ src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/model/db/entities/ItemDomainMachineDesign.java
@@ -5,6 +5,8 @@
 package gov.anl.aps.cdb.portal.model.db.entities;
 
 import gov.anl.aps.cdb.portal.constants.ItemDomainName;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
@@ -15,10 +17,36 @@ import javax.persistence.Entity;
 @Entity
 @DiscriminatorValue(value = ItemDomainName.MACHINE_DESIGN_ID + "")
 public class ItemDomainMachineDesign extends LocatableItem {   
+    
+    private transient List<ItemElement> combinedItemElementList; 
+    private transient ItemElement combinedItemElementListParentElement; 
 
     @Override
     public Item createInstance() {
         return new ItemDomainMachineDesign();
     }   
+
+    public List<ItemElement> getCombinedItemElementList(ItemElement element) {
+        if (combinedItemElementListParentElement != null) {
+            if (!element.equals(combinedItemElementListParentElement)) {
+                combinedItemElementList = null; 
+            }
+        } else {
+            combinedItemElementList = null; 
+        }
+        
+        if (combinedItemElementList == null) {
+            combinedItemElementList = new ArrayList<>();
+            combinedItemElementListParentElement = element; 
+            
+            Item containedItem2 = combinedItemElementListParentElement.getContainedItem2();
+            if (containedItem2 != null) {
+                combinedItemElementList.addAll(containedItem2.getItemElementDisplayList());
+            }
+            combinedItemElementList.addAll(getItemElementDisplayList()); 
+        } 
+        
+        return combinedItemElementList;
+    }
 
 }
