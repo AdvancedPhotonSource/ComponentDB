c5c4c9dfeb8547e0a7c59532e17d38de6e5b86aa
Dariusz Jarosz <dariuszjarosz@me.com>
2019-04-04 09:07:20 -0500

    Resolve issue with multi edit screen not working. Closes #297

--- src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/controllers/ItemControllerExtensionHelper.java
+++ src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/controllers/ItemControllerExtensionHelper.java
@@ -397,6 +397,10 @@ public abstract class ItemControllerExtensionHelper implements IItemController<I
         return getItemController().isDisplayRowExpansionLogs(item);
     }
     
+    public boolean renderRowExpansionContents(Item item) {
+        return getItemController().renderRowExpansionContents(item); 
+    }
+    
     @Override
     public String update() {
         return getItemController().update();
