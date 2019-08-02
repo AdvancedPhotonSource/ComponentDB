5d1a0a20804a713812aeb2d2bfd3f7ab8c723330
Dariusz Jarosz <dariuszjarosz@me.com>
2019-08-02 11:07:41 -0500

    Add info action functionality to alow user to view text files directly in portal. Closes #293

--- tools/developer_tools/cdb_plugins/plugins/dm/java/DmPluginManager.java
+++ tools/developer_tools/cdb_plugins/plugins/dm/java/DmPluginManager.java
@@ -4,9 +4,14 @@
  */
 package gov.anl.aps.cdb.portal.plugins.support.dm;
 
+import gov.anl.aps.cdb.common.exceptions.ExternalServiceError;
+import gov.anl.aps.cdb.portal.model.db.entities.PropertyValue;
 import gov.anl.aps.cdb.portal.model.jsf.handlers.PropertyTypeHandlerInterface;
 import gov.anl.aps.cdb.portal.plugins.PluginManagerBase;
+import gov.anl.aps.cdb.portal.utilities.SessionUtility;
+import java.io.ByteArrayOutputStream;
 import java.util.Properties;
+import org.apache.log4j.Logger;
 
 /**
  *
@@ -16,10 +21,14 @@ public class DmPluginManager extends PluginManagerBase {
     
     private static final Properties DM_PROPERTIES = getDefaultPropertiesForPlugin("dm"); 
     
+    private static final Logger LOGGER = Logger.getLogger(DmPluginManager.class.getName());
+    
     private static final String DM_SERVICE_URL_KEY = "serviceUrl"; 
     private static final String DM_SERVICE_USER_KEY = "serviceUser";     
     private static final String DM_SERVICE_PASS_KEY = "servicePass"; 
-    private static final String DM_PORTAL_URL_KEY = "dmPortalUrl";        
+    private static final String DM_PORTAL_URL_KEY = "dmPortalUrl";
+    
+    private String infoActionText = null;
     
     public static String getServiceUrlProperty() {
         return DM_PROPERTIES.getProperty(DM_SERVICE_URL_KEY, ""); 
@@ -35,6 +44,25 @@ public class DmPluginManager extends PluginManagerBase {
     
     public String getDmPortalUrl() {
         return DM_PROPERTIES.getProperty(DM_PORTAL_URL_KEY, ""); 
+    } 
+
+    @Override
+    public void performInfoActionLoad(PropertyValue propertyValue) {
+        DmPropertyTypeHandler handler = (DmPropertyTypeHandler) propertyTypeHandler;
+        infoActionText = null; 
+        try { 
+            ByteArrayOutputStream byteArrayOutputStream = handler.getByteArrayOutputStream(propertyValue);
+            byte[] toByteArray = byteArrayOutputStream.toByteArray();
+            infoActionText = new String(toByteArray);
+            super.performInfoActionLoad(propertyValue);
+        } catch (ExternalServiceError ex) {
+            LOGGER.error(ex);            
+            SessionUtility.addErrorMessage("Error", "Error fetching property data.");
+        }
+    }
+
+    public String getInfoActionText() {
+        return infoActionText;
     }
     
     @Override
