48565b8f4e623b1a66bb5aa6439daf929fe0c659
Dariusz Jarosz <dariuszjarosz@me.com>
2019-08-12 14:14:57 -0500

    fix update notes.

--- db/sql/updates/updateTo3.8.0.sql
+++ db/sql/updates/updateTo3.8.0.sql
@@ -3,9 +3,9 @@
 -- See LICENSE file.
 --
 
--- Execute by running `mysql CDB_DB_NAME --host=127.0.0.1 --user=cdb -p < updateTo3.5.0.sql`
+-- Execute by running `mysql CDB_DB_NAME --host=127.0.0.1 --user=cdb -p < updateTo3.8.0.sql`
 -- After executing this file backup and rebuild db from backup to apply other changes.
--- NOTE: Update support to Glassfish 5.0
+-- NOTE: Update support 
 
 # Settings Have been updated. 
 ### PLEASE REBUILD DB ###
