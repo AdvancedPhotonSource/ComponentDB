20304808b4f4d7b38f264ce1c2ea6bdd859864d7
craigmcchesney <auto@craigmcc.com>
2019-08-21 12:16:07 -0600

    add javadoc for ItemDomainCableDesignWizardClient

--- src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/view/objects/ItemDomainCableDesignWizardClient.java
+++ src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/view/objects/ItemDomainCableDesignWizardClient.java
@@ -5,11 +5,16 @@
 package gov.anl.aps.cdb.portal.view.objects;
 
 /**
- *
+ * This class defines the interface for clients of the class 
+ * {@link ItemDomainCableDesignWizard}.
  * @author craig
  */
 public interface ItemDomainCableDesignWizardClient {
 
+    /**
+     * Performs cleanup in the client after the cable wizard completes and
+     * closes.
+     */
     public void cleanupCableWizard();
     
 }
