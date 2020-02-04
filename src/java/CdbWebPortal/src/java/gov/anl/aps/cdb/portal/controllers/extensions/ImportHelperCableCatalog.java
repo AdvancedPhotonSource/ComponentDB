2eb3b1f3b3b8031f899c90ffbe8bd9c979f737af
craigmcchesney <auto@craigmcc.com>
2020-02-04 10:19:47 -0700

    #389: cable type import handling for official spreadsheet format and new columns

--- src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/controllers/extensions/ImportHelperCableCatalog.java
+++ src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/controllers/extensions/ImportHelperCableCatalog.java
@@ -5,19 +5,18 @@
 package gov.anl.aps.cdb.portal.controllers.extensions;
 
 import gov.anl.aps.cdb.common.exceptions.CdbException;
-import gov.anl.aps.cdb.common.exceptions.ObjectAlreadyExists;
 import gov.anl.aps.cdb.portal.controllers.ImportHelperBase;
 import gov.anl.aps.cdb.portal.controllers.ItemDomainCableCatalogController;
 import gov.anl.aps.cdb.portal.model.db.entities.ItemDomainCableCatalog;
-import gov.anl.aps.cdb.portal.utilities.SessionUtility;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
-import static org.apache.poi.ss.usermodel.CellType.STRING;
+import org.apache.poi.ss.usermodel.Hyperlink;
+import org.apache.poi.ss.usermodel.RichTextString;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFHyperlink;
 
 /**
  *
@@ -28,10 +27,20 @@ public class ImportHelperCableCatalog extends ImportHelperBase {
     public class CableCatalogRowModel extends RowModel {
 
         private String cableType = "";
-        private String weight = "";
-        private String diameter = "";
-        private String source = "";
+        private String description = "";
         private String url = "";
+        private String imageUrl = "";
+        private String manufacturer = "";
+        private String partNumber = "";
+        private String diameter = "";
+        private String weight = "";
+        private String conductors = "";
+        private String insulation = "";
+        private String jacketColor = "";
+        private String voltageRating = "";
+        private String fireLoad = "";
+        private String heatLimit = "";
+        private String bendRadius = "";
 
         public CableCatalogRowModel(ItemDomainCableCatalog c) {
             super(c);
@@ -41,45 +50,123 @@ public class ImportHelperCableCatalog extends ImportHelperBase {
             return ((ItemDomainCableCatalog)getEntity()).getCableType();
         }
         
-        public String getPartNumber() {
-            return ((ItemDomainCableCatalog)getEntity()).getPartNumber();
+        public String getDescription() {
+            return ((ItemDomainCableCatalog)getEntity()).getDescription();
+        }
+        
+        public String getUrl() {
+            return ((ItemDomainCableCatalog)getEntity()).getUrl();
+        }
+        
+        public String getImageUrl() {
+            return ((ItemDomainCableCatalog)getEntity()).getImageUrl();
+        }
+        
+        public String getManufacturer() {
+            return ((ItemDomainCableCatalog)getEntity()).getManufacturer();
         }
 
-        public String getWeight() {
-            return ((ItemDomainCableCatalog)getEntity()).getWeight();
+        public String getPartNumber() {
+            return ((ItemDomainCableCatalog)getEntity()).getPartNumber();
         }
 
         public String getDiameter() {
             return ((ItemDomainCableCatalog)getEntity()).getDiameter();
         }
 
-        public String getSource() {
-            return ((ItemDomainCableCatalog)getEntity()).getSource();
+        public String getWeight() {
+            return ((ItemDomainCableCatalog)getEntity()).getWeight();
         }
 
-        public String getUrl() {
-            return ((ItemDomainCableCatalog)getEntity()).getCableType();
+        public String getConductors() {
+            return ((ItemDomainCableCatalog)getEntity()).getConductors();
         }
+
+        public String getInsulation() {
+            return ((ItemDomainCableCatalog)getEntity()).getInsulation();
+        }
+
+        public String getJacketColor() {
+            return ((ItemDomainCableCatalog)getEntity()).getJacketColor();
+        }
+
+        public String getVoltageRating() {
+            return ((ItemDomainCableCatalog)getEntity()).getVoltageRating();
+        }
+
+        public String getFireLoad() {
+            return ((ItemDomainCableCatalog)getEntity()).getFireLoad();
+        }
+
+        public String getHeatLimit() {
+            return ((ItemDomainCableCatalog)getEntity()).getHeatLimit();
+        }
+
+        public String getBendRadius() {
+            return ((ItemDomainCableCatalog)getEntity()).getBendRadius();
+        }
+
     }
 
     protected static String cableTypeHeader = "Cable Type";
     protected static String cableTypeProperty = "cableType";
     protected static int cableTypeColumn = 0;
+    
+    protected static String descriptionHeader = "Description";
+    protected static String descriptionProperty = "description";
+    protected static int descriptionColumn = 1;
+    
+    protected static String urlHeader = "Link URL";
+    protected static String urlProperty = "url";
+    protected static int urlColumn = 2;
+    
+    protected static String imageUrlHeader = "Image URL";
+    protected static String imageUrlProperty = "imageUrl";
+    protected static int imageUrlColumn = 3;
+
+    protected static String manufacturerHeader = "Manufacturer";
+    protected static String manufacturerProperty = "manufacturer";
+    protected static int manufacturerColumn = 4;
+    
     protected static String partNumberHeader = "Part Number";
     protected static String partNumberProperty = "partNumber";
-    protected static int partNumberColumn = 1;
-    protected static String weightHeader = "Weight";
-    protected static String weightProperty = "weight";
-    protected static int weightColumn = 2;
+    protected static int partNumberColumn = 5;
+    
     protected static String diameterHeader = "Diameter";
     protected static String diameterProperty = "diameter";
-    protected static int diameterColumn = 3;
-    protected static String sourceHeader = "Source";
-    protected static String sourceProperty = "source";
-    protected static int sourceColumn = 4;
-    protected static String urlHeader = "URL";
-    protected static String urlProperty = "url";
-    protected static int urlColumn = 5;
+    protected static int diameterColumn = 6;
+
+    protected static String weightHeader = "Weight";
+    protected static String weightProperty = "weight";
+    protected static int weightColumn = 7;
+    
+    protected static String conductorsHeader = "Conductors";
+    protected static String conductorsProperty = "conductors";
+    protected static int conductorsColumn = 8;
+    
+    protected static String insulationHeader = "Insulation";
+    protected static String insulationProperty = "insulation";
+    protected static int insulationColumn = 9;
+    
+    protected static String jacketColorHeader = "Jacket Color";
+    protected static String jacketColorProperty = "jacketColor";
+    protected static int jacketColorColumn = 10;
+    
+    protected static String voltageRatingHeader = "Voltage Rating";
+    protected static String voltageRatingProperty = "voltageRating";
+    protected static int voltageRatingColumn = 11;
+    
+    protected static String fireLoadHeader = "Fire Load";
+    protected static String fireLoadProperty = "fireLoad";
+    protected static int fireLoadColumn = 12;
+    
+    protected static String heatLimitHeader = "Heat Limit";
+    protected static String heatLimitProperty = "heatLimit";
+    protected static int heatLimitColumn = 13;
+    
+    protected static String bendRadiusHeader = "Voltage Rating";
+    protected static String bendRadiusProperty = "bendRadius";
+    protected static int bendRadiusColumn = 14;
     
     protected static String completionUrlValue = "/views/itemDomainCableCatalog/list?faces-redirect=true";
     
@@ -96,13 +183,22 @@ public class ImportHelperCableCatalog extends ImportHelperBase {
     @Override
     protected void createColumnModels_() {
         columns.add(new ColumnModel(cableTypeHeader, cableTypeProperty));
-        columns.add(new ColumnModel(partNumberHeader, partNumberProperty));
-        columns.add(new ColumnModel(weightHeader, weightProperty));
-        columns.add(new ColumnModel(diameterHeader, diameterProperty));
-        columns.add(new ColumnModel(sourceHeader, sourceProperty));
+        columns.add(new ColumnModel(descriptionHeader, descriptionProperty));
         columns.add(new ColumnModel(urlHeader, urlProperty));
+        columns.add(new ColumnModel(imageUrlHeader, imageUrlProperty));
+        columns.add(new ColumnModel(manufacturerHeader, manufacturerProperty));
+        columns.add(new ColumnModel(partNumberHeader, partNumberProperty));
+        columns.add(new ColumnModel(diameterHeader, diameterProperty));
+        columns.add(new ColumnModel(weightHeader, weightProperty));
+        columns.add(new ColumnModel(conductorsHeader, conductorsProperty));
+        columns.add(new ColumnModel(insulationHeader, insulationProperty));
+        columns.add(new ColumnModel(jacketColorHeader, jacketColorProperty));
+        columns.add(new ColumnModel(voltageRatingHeader, voltageRatingProperty));
+        columns.add(new ColumnModel(fireLoadHeader, fireLoadProperty));
+        columns.add(new ColumnModel(heatLimitHeader, heatLimitProperty));
+        columns.add(new ColumnModel(bendRadiusHeader, bendRadiusProperty));
     }
-
+    
     @Override
     public int getDataStartRow() {
         return 1;
@@ -112,11 +208,21 @@ public class ImportHelperCableCatalog extends ImportHelperBase {
     public boolean parseRow(Row row) {
 
         String cableType = "";
-        String partNumber = "";
-        String weight = "";
-        String diameter = "";
-        String source = "";
+        String description = "";
         String url = "";
+        String imageUrl = "";
+        String manufacturer = "";
+        String partNumber = "";
+        String diameter = "";
+        String weight = "";
+        String conductors = "";
+        String insulation = "";
+        String jacketColor = "";
+        String voltageRating = "";
+        String fireLoad = "";
+        String heatLimit = "";
+        String bendRadius = "";
+        
         boolean isValid = true;
         String validString = "";
 
@@ -137,23 +243,56 @@ public class ImportHelperCableCatalog extends ImportHelperBase {
             }
         }
 
+        cell = row.getCell(descriptionColumn);
+        if (cell == null) {
+            description = "";
+        } else {
+            cell.setCellType(CellType.STRING);
+            description = cell.getStringCellValue();
+        }
+
+        cell = row.getCell(urlColumn);
+        if (cell == null) {
+            url = "";
+        } else {
+            XSSFHyperlink urlLink = (XSSFHyperlink)cell.getHyperlink();
+            if (urlLink != null) {
+                url = urlLink.getAddress();
+                if (url.length() > 256) {
+                    isValid = false;
+                    validString = "url length exceeds 256 characters";
+                }
+            }
+        }
+        
+        cell = row.getCell(imageUrlColumn);
+        if (cell == null) {
+            imageUrl = "";
+        } else {
+            XSSFHyperlink imageLink = (XSSFHyperlink)cell.getHyperlink();
+            if (imageLink != null) {
+                imageUrl = imageLink.getAddress();
+                if (imageUrl.length() > 256) {
+                    isValid = false;
+                    validString = "imageUrl length exceeds 256 characters";
+                }
+            }
+        }
+        
+        cell = row.getCell(manufacturerColumn);
+        if (cell == null) {
+            manufacturer = "";
+        } else {
+            cell.setCellType(CellType.STRING);
+            manufacturer = cell.getStringCellValue();
+        }
+
         cell = row.getCell(partNumberColumn);
         if (cell == null) {
             partNumber = "";
         } else {
             cell.setCellType(CellType.STRING);
             partNumber = cell.getStringCellValue();
-        }
-
-        cell = row.getCell(weightColumn);
-        if (cell == null) {
-            weight = "";
-        } else if (cell.getCellType() != CellType.NUMERIC) {
-            weight = "";
-            isValid = false;
-            validString = "weight is not a number";
-        } else {
-            weight = String.valueOf(cell.getNumericCellValue());
         }
 
         cell = row.getCell(diameterColumn);
@@ -167,31 +306,103 @@ public class ImportHelperCableCatalog extends ImportHelperBase {
             diameter = String.valueOf(cell.getNumericCellValue());
         }
 
-        cell = row.getCell(sourceColumn);
+        cell = row.getCell(weightColumn);
         if (cell == null) {
-            source = "";
+            weight = "";
+        } else if (cell.getCellType() != CellType.NUMERIC) {
+            weight = "";
+            isValid = false;
+            validString = "weight is not a number";
         } else {
-            cell.setCellType(CellType.STRING);
-            source = cell.getStringCellValue();
+            weight = String.valueOf(cell.getNumericCellValue());
         }
 
-        cell = row.getCell(urlColumn);
+        cell = row.getCell(conductorsColumn);
         if (cell == null) {
-            url = "";
+            conductors = "";
         } else {
             cell.setCellType(CellType.STRING);
-            url = cell.getStringCellValue();
+            conductors = cell.getStringCellValue();
         }
         
+        cell = row.getCell(insulationColumn);
+        if (cell == null) {
+            insulation = "";
+        } else {
+            cell.setCellType(CellType.STRING);
+            insulation = cell.getStringCellValue();
+        }
+        
+        cell = row.getCell(jacketColorColumn);
+        if (cell == null) {
+            jacketColor = "";
+        } else {
+            cell.setCellType(CellType.STRING);
+            jacketColor = cell.getStringCellValue();
+        }
+        
+        cell = row.getCell(voltageRatingColumn);
+        if (cell == null) {
+            voltageRating = "";
+        } else if (cell.getCellType() != CellType.NUMERIC) {
+            voltageRating = "";
+            isValid = false;
+            validString = "voltageRating is not a number";
+        } else {
+            voltageRating = String.valueOf(cell.getNumericCellValue());
+        }
+
+        cell = row.getCell(fireLoadColumn);
+        if (cell == null) {
+            fireLoad = "";
+        } else if (cell.getCellType() != CellType.NUMERIC) {
+            fireLoad = "";
+            isValid = false;
+            validString = "fireLoad is not a number";
+        } else {
+            fireLoad = String.valueOf(cell.getNumericCellValue());
+        }
+
+        cell = row.getCell(heatLimitColumn);
+        if (cell == null) {
+            heatLimit = "";
+        } else if (cell.getCellType() != CellType.NUMERIC) {
+            heatLimit = "";
+            isValid = false;
+            validString = "heatLimit is not a number";
+        } else {
+            heatLimit = String.valueOf(cell.getNumericCellValue());
+        }
+
+        cell = row.getCell(bendRadiusColumn);
+        if (cell == null) {
+            bendRadius = "";
+        } else if (cell.getCellType() != CellType.NUMERIC) {
+            bendRadius = "";
+            isValid = false;
+            validString = "bendRadius is not a number";
+        } else {
+            bendRadius = String.valueOf(cell.getNumericCellValue());
+        }
+
         ItemDomainCableCatalogController controller = ItemDomainCableCatalogController.getInstance();
         
         ItemDomainCableCatalog newType = controller.newEntityInstance();
         newType.setCableType(cableType);
-        newType.setPartNumber(partNumber);
-        newType.setWeight(weight);
-        newType.setDiameter(diameter);
-        newType.setSource(source);
+        newType.setDescription(description);
         newType.setUrl(url);
+        newType.setImageUrl(imageUrl);
+        newType.setManufacturer(manufacturer);
+        newType.setPartNumber(partNumber);
+        newType.setDiameter(diameter);
+        newType.setWeight(weight);
+        newType.setConductors(conductors);
+        newType.setInsulation(insulation);
+        newType.setJacketColor(jacketColor);
+        newType.setVoltageRating(voltageRating);
+        newType.setFireLoad(fireLoad);
+        newType.setHeatLimit(heatLimit);
+        newType.setBendRadius(bendRadius);
         
         CableCatalogRowModel info = new CableCatalogRowModel(newType);
         
--- src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/model/db/entities/ItemDomainCableCatalog.java
+++ src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/model/db/entities/ItemDomainCableCatalog.java
@@ -17,14 +17,32 @@ import javax.persistence.Entity;
 @DiscriminatorValue(value = ItemDomainName.CABLE_CATALOG_ID + "")   
 public class ItemDomainCableCatalog extends ItemDomainCatalogBase<ItemDomainCableInventory> {
     
+    private transient String url = null;
+    private transient String imageUrl = null;
+    private transient String manufacturer = null;
     private transient String weight = null;
     private transient String diameter = null;
-    private transient String source = null;
-    private transient String url = null;
+    private transient String conductors = null;
+    private transient String insulation = null;
+    private transient String jacketColor = null;
+    private transient String voltageRating = null;
+    private transient String fireLoad = null;
+    private transient String heatLimit = null;
+    private transient String bendRadius = null;
     
     private final static String CABLE_INTERNAL_PROPERTY_TYPE = "cable_internal_property_type"; 
+    private final static String CABLE_PROPERTY_URL_KEY = "url"; 
+    private final static String CABLE_PROPERTY_IMAGE_URL_KEY = "imageUrl"; 
+    private final static String CABLE_PROPERTY_MANUFACTURER_KEY = "manufacturer"; 
     private final static String CABLE_PROPERTY_WEIGHT_KEY = "weight"; 
     private final static String CABLE_PROPERTY_DIAMETER_KEY = "diameter"; 
+    private final static String CABLE_PROPERTY_CONDUCTORS_KEY = "conductors"; 
+    private final static String CABLE_PROPERTY_INSULATION_KEY = "insulation"; 
+    private final static String CABLE_PROPERTY_JACKET_COLOR_KEY = "jacketColor"; 
+    private final static String CABLE_PROPERTY_VOLTAGE_RATING_KEY = "voltageRating"; 
+    private final static String CABLE_PROPERTY_FIRE_LOAD_KEY = "fireLoad"; 
+    private final static String CABLE_PROPERTY_HEAT_LIMIT_KEY = "heatLimit"; 
+    private final static String CABLE_PROPERTY_BEND_RADIUS_KEY = "bendRadius"; 
 
     @Override
     public Item createInstance() {
@@ -51,6 +69,72 @@ public class ItemDomainCableCatalog extends ItemDomainCatalogBase<ItemDomainCabl
     
     public void setCableType(String t) {
         this.setName(t);
+    }
+    
+    public String getUrl() {
+        if (url == null) {
+            PropertyValue propertyValue = getInternalCablePropertyValue();
+            if (propertyValue == null) {
+                url = "";
+            } else {
+                url = propertyValue.getPropertyMetadataValueForKey(CABLE_PROPERTY_URL_KEY);
+            }
+        }
+        return url;
+    }
+    
+    public void setUrl(String w) {
+        url = w;
+        
+        PropertyValue propertyValue = getInternalCablePropertyValue();
+
+        if (propertyValue != null) {
+            propertyValue.setPropertyMetadataValue(CABLE_PROPERTY_URL_KEY, w);
+        }
+    }
+    
+    public String getImageUrl() {
+        if (imageUrl == null) {
+            PropertyValue propertyValue = getInternalCablePropertyValue();
+            if (propertyValue == null) {
+                imageUrl = "";
+            } else {
+                imageUrl = propertyValue.getPropertyMetadataValueForKey(CABLE_PROPERTY_IMAGE_URL_KEY);
+            }
+        }
+        return imageUrl;
+    }
+    
+    public void setImageUrl(String w) {
+        imageUrl = w;
+        
+        PropertyValue propertyValue = getInternalCablePropertyValue();
+
+        if (propertyValue != null) {
+            propertyValue.setPropertyMetadataValue(CABLE_PROPERTY_IMAGE_URL_KEY, w);
+        }
+    }
+    
+    public String getManufacturer() {
+        if (manufacturer == null) {
+            PropertyValue propertyValue = getInternalCablePropertyValue();
+            if (propertyValue == null) {
+                manufacturer = "";
+            } else {
+                manufacturer = propertyValue.getPropertyMetadataValueForKey(CABLE_PROPERTY_MANUFACTURER_KEY);
+            }
+        }
+        return manufacturer;
+    }
+    
+    public void setManufacturer(String w) {
+        manufacturer = w;
+        
+        PropertyValue propertyValue = getInternalCablePropertyValue();
+
+        if (propertyValue != null) {
+            propertyValue.setPropertyMetadataValue(CABLE_PROPERTY_MANUFACTURER_KEY, w);
+        }
     }
     
     public String getPartNumber() {
@@ -105,19 +189,158 @@ public class ItemDomainCableCatalog extends ItemDomainCatalogBase<ItemDomainCabl
         }
     }
     
-    public String getSource() {
-        return source;
+    public String getConductors() {
+        if (conductors == null) {
+            PropertyValue propertyValue = getInternalCablePropertyValue();
+            if (propertyValue == null) {
+                conductors = "";
+            } else {
+                conductors = propertyValue.getPropertyMetadataValueForKey(CABLE_PROPERTY_CONDUCTORS_KEY);
+            }
+        }
+        return conductors;
     }
     
-    public void setSource(String s) {
-        source = s;
+    public void setConductors(String w) {
+        conductors = w;
+        
+        PropertyValue propertyValue = getInternalCablePropertyValue();
+
+        if (propertyValue != null) {
+            propertyValue.setPropertyMetadataValue(CABLE_PROPERTY_CONDUCTORS_KEY, w);
+        }
     }
     
-    public String getUrl() {
-        return url;
+    public String getInsulation() {
+        if (insulation == null) {
+            PropertyValue propertyValue = getInternalCablePropertyValue();
+            if (propertyValue == null) {
+                insulation = "";
+            } else {
+                insulation = propertyValue.getPropertyMetadataValueForKey(CABLE_PROPERTY_INSULATION_KEY);
+            }
+        }
+        return insulation;
     }
     
-    public void setUrl(String u) {
-        url = u;
+    public void setInsulation(String w) {
+        insulation = w;
+        
+        PropertyValue propertyValue = getInternalCablePropertyValue();
+
+        if (propertyValue != null) {
+            propertyValue.setPropertyMetadataValue(CABLE_PROPERTY_INSULATION_KEY, w);
+        }
     }
+    
+    public String getJacketColor() {
+        if (jacketColor == null) {
+            PropertyValue propertyValue = getInternalCablePropertyValue();
+            if (propertyValue == null) {
+                jacketColor = "";
+            } else {
+                jacketColor = propertyValue.getPropertyMetadataValueForKey(CABLE_PROPERTY_JACKET_COLOR_KEY);
+            }
+        }
+        return jacketColor;
+    }
+    
+    public void setJacketColor(String w) {
+        jacketColor = w;
+        
+        PropertyValue propertyValue = getInternalCablePropertyValue();
+
+        if (propertyValue != null) {
+            propertyValue.setPropertyMetadataValue(CABLE_PROPERTY_JACKET_COLOR_KEY, w);
+        }
+    }
+    
+    public String getVoltageRating() {
+        if (voltageRating == null) {
+            PropertyValue propertyValue = getInternalCablePropertyValue();
+            if (propertyValue == null) {
+                voltageRating = "";
+            } else {
+                voltageRating = propertyValue.getPropertyMetadataValueForKey(CABLE_PROPERTY_VOLTAGE_RATING_KEY);
+            }
+        }
+        return voltageRating;
+    }
+    
+    public void setVoltageRating(String w) {
+        voltageRating = w;
+        
+        PropertyValue propertyValue = getInternalCablePropertyValue();
+
+        if (propertyValue != null) {
+            propertyValue.setPropertyMetadataValue(CABLE_PROPERTY_VOLTAGE_RATING_KEY, w);
+        }
+    }
+    
+    public String getFireLoad() {
+        if (fireLoad == null) {
+            PropertyValue propertyValue = getInternalCablePropertyValue();
+            if (propertyValue == null) {
+                fireLoad = "";
+            } else {
+                fireLoad = propertyValue.getPropertyMetadataValueForKey(CABLE_PROPERTY_FIRE_LOAD_KEY);
+            }
+        }
+        return fireLoad;
+    }
+    
+    public void setFireLoad(String w) {
+        fireLoad = w;
+        
+        PropertyValue propertyValue = getInternalCablePropertyValue();
+
+        if (propertyValue != null) {
+            propertyValue.setPropertyMetadataValue(CABLE_PROPERTY_FIRE_LOAD_KEY, w);
+        }
+    }
+    
+    public String getHeatLimit() {
+        if (heatLimit == null) {
+            PropertyValue propertyValue = getInternalCablePropertyValue();
+            if (propertyValue == null) {
+                heatLimit = "";
+            } else {
+                heatLimit = propertyValue.getPropertyMetadataValueForKey(CABLE_PROPERTY_HEAT_LIMIT_KEY);
+            }
+        }
+        return heatLimit;
+    }
+    
+    public void setHeatLimit(String w) {
+        heatLimit = w;
+        
+        PropertyValue propertyValue = getInternalCablePropertyValue();
+
+        if (propertyValue != null) {
+            propertyValue.setPropertyMetadataValue(CABLE_PROPERTY_HEAT_LIMIT_KEY, w);
+        }
+    }
+    
+    public String getBendRadius() {
+        if (bendRadius == null) {
+            PropertyValue propertyValue = getInternalCablePropertyValue();
+            if (propertyValue == null) {
+                bendRadius = "";
+            } else {
+                bendRadius = propertyValue.getPropertyMetadataValueForKey(CABLE_PROPERTY_BEND_RADIUS_KEY);
+            }
+        }
+        return bendRadius;
+    }
+    
+    public void setBendRadius(String w) {
+        bendRadius = w;
+        
+        PropertyValue propertyValue = getInternalCablePropertyValue();
+
+        if (propertyValue != null) {
+            propertyValue.setPropertyMetadataValue(CABLE_PROPERTY_BEND_RADIUS_KEY, w);
+        }
+    }
+    
 }
