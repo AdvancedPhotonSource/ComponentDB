47eb63d774e937977fea0fb006cbc2953a2faa6b
craigmcchesney <auto@craigmcc.com>
2020-04-10 16:10:50 -0600

    #543: add helper type command-line parsing and validation, a mechanism for helper subclasses to add arguements in command line parsing, and logging to pre-import script

--- tools/developer_tools/utilities/cable import/preImport.py
+++ tools/developer_tools/utilities/cable import/preImport.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 import sys
 from abc import ABC, abstractmethod
 
@@ -17,13 +18,12 @@ class PreImportHelper(ABC):
 
     helperDict = {}
 
-    def __init__(self, api, opts):
-        self.api = api
-        self.opts = opts
+    def __init__(self):
         self.input_columns = {}
         self.initialize_input_columns()
         self.output_columns = {}
         self.initialize_output_columns()
+        self.api = None
 
     # registers helper concrete classes for lookup by tag
     @classmethod
@@ -42,10 +42,16 @@ class PreImportHelper(ABC):
 
     # creates instance of class with specified tag
     @classmethod
-    def create_helper(cls, tag, api, helper_opts):
+    def create_helper(cls, tag):
         helper_class = cls.helperDict[tag]
-        helper_instance = helper_class(api, helper_opts)
+        helper_instance = helper_class()
         return helper_instance
+
+    # Allows subclasses to add command line parser args.  Default behavior is to do nothing.
+    # e.g., "parser.add_argument("--cdbUser", help="CDB User ID for API login", required=True)"
+    @staticmethod
+    def add_parser_args(parser):
+        pass
 
     # Returns registered tag for subclass.
     @staticmethod
@@ -79,6 +85,14 @@ class PreImportHelper(ABC):
     def output_column_list():
         pass
 
+    # Returns an output object for the specified input object, or None if the input object is duplicate.
+    @abstractmethod
+    def get_output_object(self, obj):
+        pass
+
+    def set_api(self, api):
+        self.api = api
+
     # Builds dictionary whose keys are column index and value is column model object.
     def initialize_input_columns(self):
         for col in self.input_column_list():
@@ -94,11 +108,6 @@ class PreImportHelper(ABC):
         # use reflection to invoke column setter method on supplied object
         getattr(obj, self.input_columns[index].method)(value)
 
-    # Returns an output object for the specified input object, or None if the input object is duplicate.
-    @abstractmethod
-    def get_output_object(obj):
-        pass
-
     # Returns column label for specified column index.
     def get_output_column_label(self, col_index):
         return self.output_columns[col_index].label
@@ -107,15 +116,15 @@ class PreImportHelper(ABC):
     def get_output_cell_value(self, obj, index):
         # use reflection to invoke column getter method on supplied object
         val = getattr(obj, self.output_columns[index].method)()
-        print("index: %d method: %s value: %s" % (index, self.output_columns[index].method, val))
+        logging.debug("index: %d method: %s value: %s" % (index, self.output_columns[index].method, val))
         return val
 
 
 @register
 class SourceHelper(PreImportHelper):
 
-    def __init__(self, api, opts):
-        super().__init__(api, opts)
+    def __init__(self):
+        super().__init__()
         self.manufacturers = set()
 
     @staticmethod
@@ -150,7 +159,7 @@ class SourceHelper(PreImportHelper):
     def get_output_object(self, obj):
 
         manufacturer = obj.get_manufacturer()
-        print("found manufacturer: %s" % manufacturer)
+        logging.debug("found manufacturer: %s" % manufacturer)
 
         if manufacturer not in self.manufacturers:
             # check to see if manufacturer exists as a CDB Source
@@ -158,18 +167,18 @@ class SourceHelper(PreImportHelper):
                 mfr_source = self.api.getSourceApi().get_source_by_name(manufacturer)
             except ApiException as ex:
                 if "ObjectNotFound" not in ex.body:
-                    print("exception retrieving source for manufacturer: %s - %s" % (manufacturer, ex.body))
+                    logging.error("exception retrieving source for manufacturer: %s - %s" % (manufacturer, ex.body))
                 mfr_source = None
             if mfr_source:
-                print("source already exists for manufacturer: %s, skipping" % manufacturer)
+                logging.debug("source already exists for manufacturer: %s, skipping" % manufacturer)
                 return None
             else:
-                print("adding output object for unique manufacturer: %s" % manufacturer)
+                logging.debug("adding output object for unique manufacturer: %s" % manufacturer)
                 self.manufacturers.add(manufacturer)
                 return SourceOutputObject(input_object=obj)
 
         else:
-            print("ignoring duplicate manufacturer: %s" % manufacturer)
+            logging.debug("ignoring duplicate manufacturer: %s" % manufacturer)
             return None
 
 
@@ -226,14 +235,31 @@ class SourceOutputObject(OutputObject):
 
 def main():
 
-    # process command line
+    # find --type command line parameter so we can create the appropriate helper subclass before proceeding
+    type_arg = "--type"
+    if type_arg not in sys.argv:
+        sys.exit("--type command line parameter not specified, exiting")
+    else:
+        type_index = sys.argv.index(type_arg)
+        if type_index < (len(sys.argv) - 1):
+            type_arg_value = sys.argv[type_index+1]
+        else:
+            sys.exit("no value specified for --type command line parameter, exiting")
+
+    # create instance of appropriate helper subclass
+    if not PreImportHelper.is_valid_type(type_arg_value):
+        sys.exit("unknown value for type parameter, expected Source, CableType, or CableDesign, got: %s" % type_arg_value)
+    helper = PreImportHelper.create_helper(type_arg_value)
+
+    # process other command line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("inputFile", help="Data collection workbook xlsx file with 'cable specs' tab")
     parser.add_argument("outputFile", help="Official CDB Sources import format xlsx file")
-    parser.add_argument("--type", help="Type of pre-import processing (Source, CableType, or CableDesign)", required=True)
+    parser.add_argument(type_arg, help="Type of pre-import processing (Source, CableType, or CableDesign)", required=True)
     parser.add_argument("--logFile", help="File for log output", required=True)
     parser.add_argument("--cdbUser", help="CDB User ID for API login", required=True)
     parser.add_argument("--cdbPassword", help="CDB User password for API login", required=True)
+    helper.add_parser_args(parser)
     args = parser.parse_args()
     print("using inputFile: %s" % args.inputFile)
     print("using outputFile: %s" % args.outputFile)
@@ -242,6 +268,9 @@ def main():
     print("cdb user id: %s" % args.cdbUser)
     print("cdb user password: %s" % args.cdbPassword)
 
+    # configure logging
+    logging.basicConfig(filename=args.logFile, filemode='w', level=logging.DEBUG, format='%(levelname)s - %(message)s')
+
     # open connection to CDB
     api = CdbApiFactory("http://localhost:8080/cdb")
     try:
@@ -249,15 +278,12 @@ def main():
         api.testAuthenticated()
     except ApiException:
         sys.exit("CDB login failed")
+    helper.set_api(api)
 
     # open input spreadsheet
     input_book = xlrd.open_workbook(args.inputFile)
     input_sheet = input_book.sheet_by_index(0)
-    print("input spreadsheet dimensions: %d x %d" % (input_sheet.nrows, input_sheet.ncols))
-
-    # create instance of appropriate helper subclass
-    opts = None
-    helper = PreImportHelper.create_helper(args.type, api, opts)
+    logging.info("input spreadsheet dimensions: %d x %d" % (input_sheet.nrows, input_sheet.ncols))
 
     # validate input spreadsheet dimensions
     if input_sheet.nrows < 2:
@@ -267,13 +293,16 @@ def main():
 
     # process rows from input spreadsheet
     output_objects = []
+    input_rows = 0
     for row_count in range(input_sheet.nrows):
 
         # skip header
         if row_count == 0:
             continue
 
-        print("processing row %d from input spreadsheet" % (row_count+1))
+        input_rows = input_rows + 1
+
+        logging.debug("processing row %d from input spreadsheet" % (row_count+1))
 
         input_object = SourceInputObject()
 
@@ -299,13 +328,15 @@ def main():
             output_sheet.write(row_count, col_count, label)
 
     # process output spreadsheet rows
+    output_rows = 0
     if len(output_objects) == 0:
-        print("no output objects, output spreadsheet will be empty")
+        logging.info("no output objects, output spreadsheet will be empty")
     for output_obj in output_objects:
 
         row_count = row_count + 1
+        output_rows = output_rows + 1
 
-        print("processing row %d in output spreadsheet" % (row_count+1))
+        logging.debug("processing row %d in output spreadsheet" % (row_count+1))
 
         for col_count in range(helper.num_output_cols()):
             if col_count in helper.output_columns:
@@ -321,6 +352,12 @@ def main():
         api.logOutUser()
     except ApiException:
         sys.exit("CDB logout failed")
+
+    # print summary
+    summary_msg = "SUMMARY: processed %d input rows and wrote %d output rows" % (input_rows, output_rows)
+    print()
+    print(summary_msg)
+    logging.info(summary_msg)
 
 
 if __name__ == '__main__':
