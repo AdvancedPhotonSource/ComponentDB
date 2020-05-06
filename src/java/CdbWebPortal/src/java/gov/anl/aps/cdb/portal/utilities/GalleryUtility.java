3b4abf0350acdcdc256fff016ca32c6a2ff2331a
Dariusz <dariuszjarosz@me.com>
2020-05-06 14:27:08 -0500

    Add functionality for generating a PDF using excel data. Closes #590

--- src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/utilities/GalleryUtility.java
+++ src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/utilities/GalleryUtility.java
@@ -4,23 +4,45 @@
  */
 package gov.anl.aps.cdb.portal.utilities;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 import gov.anl.aps.cdb.common.constants.CdbPropertyValue;
+import gov.anl.aps.cdb.common.exceptions.ExternalServiceError;
 import gov.anl.aps.cdb.common.exceptions.ImageProcessingFailed;
+import gov.anl.aps.cdb.common.utilities.FileUtility;
 import gov.anl.aps.cdb.common.utilities.ImageUtility;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Iterator;
 import javax.faces.context.FacesContext;
 import javax.imageio.ImageIO;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.rendering.PDFRenderer;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.util.CellAddress;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
 
 /**
  * This Utility is used to generate and update files for the gallery.
@@ -28,8 +50,11 @@ import org.apache.pdfbox.rendering.PDFRenderer;
 public class GalleryUtility {
 
     private static final Logger logger = LogManager.getLogger(GalleryUtility.class.getName());
+
+    protected static boolean utilMode = false;
     
-    protected static boolean utilMode = false; 
+    protected static String XLS_EXCEL_FORMAT = "xls";
+    protected static String XLSX_EXCEL_FORMAT = "xlsx";
 
     public static String getImageFormat(String fileName) {
         if (fileName.endsWith(CdbPropertyValue.ORIGINAL_IMAGE_EXTENSION)) {
@@ -91,19 +116,19 @@ public class GalleryUtility {
 
             // Generate Preview to scale for pdf images.
             if (imageFormat.equalsIgnoreCase("pdf")) {
-                byte[] pdfBytes = Files.readAllBytes(originalFile.toPath());               
+                byte[] pdfBytes = Files.readAllBytes(originalFile.toPath());
                 imageFormat = "png";
-                originalData = createPNGFromPDF(pdfBytes); 
-                
-                if (originalData == null) { 
-                    return; 
+                originalData = createPNGFromPDF(pdfBytes);
+
+                if (originalData == null) {
+                    return;
                 }
             } else {
                 originalData = Files.readAllBytes(originalFile.toPath());
             }
-        
-            String basePath = originalFile.getParentFile().getAbsolutePath();                
-            storePreviewsFromViewableData(originalData, imageFormat, basePath, originalName);                                                                               
+
+            String basePath = originalFile.getParentFile().getAbsolutePath();
+            storePreviewsFromViewableData(originalData, imageFormat, basePath, originalName);
         } catch (IOException ex) {
             logger.error(ex);
             // Check allows this class to run as a utility without server running. 
@@ -115,29 +140,28 @@ public class GalleryUtility {
             }
         }
     }
-    
-    
+
     public static void storePreviewsFromViewableData(byte[] data, String imageFormat, String basePath, String fileName) {
         try {
             byte[] thumbData = ImageUtility.resizeImage(data, StorageUtility.THUMBNAIL_IMAGE_SIZE, imageFormat);
-            
-            String thumbnailName = fileName + CdbPropertyValue.THUMBNAIL_IMAGE_EXTENSION; 
-            String thumbFilePath = basePath + "/" + thumbnailName; 
+
+            String thumbnailName = fileName + CdbPropertyValue.THUMBNAIL_IMAGE_EXTENSION;
+            String thumbFilePath = basePath + "/" + thumbnailName;
             //String thumbFileName = originalFile.getAbsolutePath().replace(originalName, originalName + CdbPropertyValue.THUMBNAIL_IMAGE_EXTENSION);
             thumbFilePath = thumbFilePath.replace(CdbPropertyValue.ORIGINAL_IMAGE_EXTENSION, "");
             Path thumbPath = Paths.get(thumbFilePath);
             Files.write(thumbPath, thumbData);
             logger.debug("Saved File: " + thumbFilePath);
-            
+
             byte[] scaledData;
             if (ImageUtility.verifyImageSizeBigger(data, StorageUtility.SCALED_IMAGE_SIZE)) {
                 scaledData = ImageUtility.resizeImage(data, StorageUtility.SCALED_IMAGE_SIZE, imageFormat);
             } else {
                 scaledData = data;
             }
-            
-            String scaledFileName = fileName + CdbPropertyValue.SCALED_IMAGE_EXTENSION; 
-            String scaledFilePath = basePath + "/" + scaledFileName; 
+
+            String scaledFileName = fileName + CdbPropertyValue.SCALED_IMAGE_EXTENSION;
+            String scaledFilePath = basePath + "/" + scaledFileName;
             scaledFilePath = scaledFilePath.replace(CdbPropertyValue.ORIGINAL_IMAGE_EXTENSION, "");
             Path scaledPath = Paths.get(scaledFilePath);
             Files.write(scaledPath, scaledData);
@@ -168,7 +192,7 @@ public class GalleryUtility {
             try (ByteArrayOutputStream imageBaos = new ByteArrayOutputStream()) {
                 ImageIO.write(image, "PNG", imageBaos);
                 imageBaos.flush();
-                originalData = imageBaos.toByteArray();               
+                originalData = imageBaos.toByteArray();
             }
 
             // It is not possible to catch certain errors during gerneration of a page preview. 
@@ -178,10 +202,131 @@ public class GalleryUtility {
             }
         } catch (IOException ex) {
             logger.error(ex);
-            return null; 
+            return null;
         }
-        
-        return originalData; 
+
+        return originalData;
+    }
+    
+    public static boolean isFileNameExcel(String fileName) {
+        if (fileName != null) {
+            String fileType = FileUtility.getFileExtension(fileName);
+            return fileType.equals(XLS_EXCEL_FORMAT) || fileType.equals(XLSX_EXCEL_FORMAT); 
+            
+        }
+        return false;         
+    }
+
+    public static StreamedContent convertExcelToPDF(InputStream stream, String fileName) throws DocumentException, ExternalServiceError, IOException {
+        String fileType = FileUtility.getFileExtension(fileName);
+
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+        Document document = new Document(PageSize.ARCH_E, 0, 0, 0, 0);
+        document.addTitle(fileName);
+
+        PdfWriter writer = PdfWriter.getInstance(document, output);
+        document.open();
+        document.addDocListener(writer);
+
+        Iterator<Sheet> sheetIterator = null;
+        if (fileType.equals(XLS_EXCEL_FORMAT)) {
+            HSSFWorkbook workbook = new HSSFWorkbook(stream);
+            sheetIterator = workbook.iterator();
+        }
+        if (fileType.equals(XLSX_EXCEL_FORMAT)) {
+            XSSFWorkbook workbook = new XSSFWorkbook(stream);
+            sheetIterator = workbook.iterator();
+        }
+
+        if (sheetIterator != null) {
+            while (sheetIterator.hasNext()) {
+                Sheet sheet = sheetIterator.next();
+                Iterator<Row> rowIterator = sheet.iterator();
+
+                Paragraph para = new Paragraph(sheet.getSheetName());
+                para.setSpacingAfter(20);
+                para.setSpacingBefore(20);
+                document.add(para);
+
+                fillInDataForSheet(rowIterator, document);
+            }
+        }
+
+        document.close();
+        logger.debug("Excel file converted to PDF successfully");
+
+        InputStream inputStream = new ByteArrayInputStream(output.toByteArray());
+
+        return new DefaultStreamedContent(inputStream);
+    }
+
+    private static void fillInDataForSheet(Iterator<Row> rowIterator, Document document) throws DocumentException {
+        PdfPTable outerTable = new PdfPTable(1);
+
+        while (rowIterator.hasNext()) {
+            Row row = rowIterator.next();
+
+            int columns = row.getLastCellNum();
+            if (columns < 0) {
+                outerTable.addCell(" ");
+                continue;
+            }
+
+            PdfPTable my_table = new PdfPTable(columns);
+            my_table.setHorizontalAlignment(Element.ALIGN_CENTER);
+            my_table.setWidthPercentage(100);
+            my_table.setSpacingBefore(0f);
+            my_table.setSpacingAfter(0f);
+            PdfPCell table_cell;
+
+            Iterator<Cell> cellIterator = row.cellIterator();
+
+            boolean blank = true;
+
+            Cell cell = null;
+
+            int cellNum = -1;
+
+            while (cellIterator.hasNext()) {
+                cellNum++;
+
+                if (cell == null) {
+                    cell = cellIterator.next();
+                }
+
+                CellAddress address = cell.getAddress();
+
+                if (address.getColumn() != cellNum) {
+                    my_table.addCell(" ");
+                    continue;
+                }
+
+                switch (cell.getCellType()) {
+                    case STRING:
+                        //Push the data from Excel to PDF Cell
+                        table_cell = new PdfPCell(new Phrase(cell.getStringCellValue()));
+                        my_table.addCell(table_cell);
+                        blank = false;
+                        break;
+                    case NUMERIC:
+                    case FORMULA:
+                        table_cell = new PdfPCell(new Phrase(cell.getNumericCellValue() + ""));
+                        my_table.addCell(table_cell);
+                        blank = false;
+                        break;
+                    default:
+                        my_table.addCell(" ");
+                        break;
+                }
+
+                cell = null;
+            }
+
+            if (!blank) {
+                document.add(my_table);
+            }
+        }
     }
 
     /**
@@ -193,7 +338,7 @@ public class GalleryUtility {
      * @param args
      */
     public static void main(String[] args) throws Exception {
-        GalleryUtility.utilMode = true; 
+        GalleryUtility.utilMode = true;
         Path imageUploadDirPath;
         Path documentUploadDirPath;
 
