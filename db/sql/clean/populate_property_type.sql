01c9b654b9e9ba10b9b1758649f929bc60006516
craigmcchesney <auto@craigmcc.com>
2020-07-31 14:54:22 -0600

    #688: creation of traveler template and instance properties should set internal flag

--- db/sql/clean/populate_property_type.sql
+++ db/sql/clean/populate_property_type.sql
@@ -8,8 +8,8 @@ LOCK TABLES `property_type` WRITE;
 INSERT INTO `property_type` VALUES
 (1,'Image',NULL,NULL,1,2,NULL,NULL,0,0,0,1,1),
 (2,'Document (Upload)','',NULL,1,1,'','',0,0,0,1,1),
-(3,'Traveler Template (Electronic)','Allows integration of traveler templates from traveler application.',NULL,2,7,'','',0,0,0,1,1),
-(4,'Traveler Instance (Electronic)','Allows integration of traveler instances from traveler application.',NULL,2,8,'','',0,0,0,1,1),
+(3,'Traveler Template (Electronic)','Allows integration of traveler templates from traveler application.',NULL,2,7,'','',0,0,1,1,1),
+(4,'Traveler Instance (Electronic)','Allows integration of traveler instances from traveler application.',NULL,2,8,'','',0,0,1,1,1),
 (5,'Item Element Row Color','',NULL,4,NULL,'','',0,0,0,1,1),
 (6,'Spare Parts Configuration','',NULL,3,NULL,'','',0,0,1,1,1),
 (8,'Component Instance Status','','',3,NULL,'','',0,0,1,1,1);
