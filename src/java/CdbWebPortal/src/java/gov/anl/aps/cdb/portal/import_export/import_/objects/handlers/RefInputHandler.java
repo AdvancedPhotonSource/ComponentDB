8f777938dd582cf9905722b066884e861a39079e
craigmcchesney <auto@craigmcc.com>
2020-09-10 08:16:11 -0600

    #745: refactor RefInputHandler to create RefObjectManager

--- src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/import_export/import_/objects/handlers/RefInputHandler.java
+++ src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/import_export/import_/objects/handlers/RefInputHandler.java
@@ -4,14 +4,12 @@
  */
 package gov.anl.aps.cdb.portal.import_export.import_.objects.handlers;
 
-import gov.anl.aps.cdb.common.exceptions.CdbException;
 import gov.anl.aps.cdb.portal.controllers.CdbEntityController;
 import gov.anl.aps.cdb.portal.import_export.import_.objects.ParseInfo;
+import gov.anl.aps.cdb.portal.import_export.import_.objects.RefObjectManager;
 import gov.anl.aps.cdb.portal.model.db.entities.CdbEntity;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  *
@@ -21,14 +19,13 @@ public class RefInputHandler extends SimpleInputHandler {
 
     private static final String SEPARATOR = ",";
 
-    private CdbEntityController controller;
     private Class paramType;
+    private CdbEntityController controller;
     private String domainNameFilter = null;
     private boolean idOnly = false;
     private boolean singleValue = true;
+    private RefObjectManager objectManager;
     
-    private Map<Object, CdbEntity> objectIdMap = new HashMap<>();
-
     public RefInputHandler(
             int columnIndex,
             String columnName,
@@ -85,57 +82,17 @@ public class RefInputHandler extends SimpleInputHandler {
         this.idOnly = idOnly;
         this.singleValue = singleValue;
     }
+    
+    private RefObjectManager getObjectManager() {
+        if (objectManager == null) {
+            objectManager = new RefObjectManager(controller, domainNameFilter);
+        }
+        return objectManager;
+    }
 
     @Override
     public Class getParamType() {
         return paramType;
-    }
-
-
-    protected CdbEntity getObjectWithId(String idString, String message_o) {
-        
-        CdbEntity objValue = null;
-        try {
-            int id = Integer.valueOf(idString.trim());
-            if (objectIdMap.containsKey(id)) {
-                objValue = objectIdMap.get(id);
-            } else {
-                objValue = controller.findById(id);
-                if (objValue != null) {
-                    objectIdMap.put(objValue.getId(), objValue);
-                }
-            }
-        } catch (NumberFormatException ex) {
-            message_o = "Invalid number format id: " + idString
-                    + " for: " + getColumnName();
-        }
-        
-        return objValue;
-    }
-    
-    protected CdbEntity getObjectWithName(String nameString, String message_o) {
-        
-        CdbEntity objValue = null;
-                
-        try {
-            objValue = controller.findUniqueByName(nameString, domainNameFilter);
-            if (objValue != null) {
-                // check cache for object so different references use same instance
-                int id = (Integer) objValue.getId();
-                if (objectIdMap.containsKey(id)) {
-                    objValue = objectIdMap.get(id);
-                } else {
-                    // add this instance to cache
-                    objectIdMap.put(id, objValue);
-                }
-            }
-        } catch (CdbException ex) {
-            message_o = "Exception searching for object with name: " + nameString
-                    + " for: " + getColumnName()
-                    + " reason: " + ex.getMessage();
-        }
-        
-        return objValue;
     }
 
     @Override
@@ -159,7 +116,8 @@ public class RefInputHandler extends SimpleInputHandler {
                         // parse and lookup single name
                         
                         String info_o = null;
-                        CdbEntity objValue = getObjectWithName(nameString.trim(), info_o);
+                        CdbEntity objValue = 
+                                getObjectManager().getObjectWithName(nameString.trim(), info_o);
                         if (objValue == null) {
                             String msg;
                             if (info_o != null) {
@@ -180,7 +138,8 @@ public class RefInputHandler extends SimpleInputHandler {
                         String[] nameTokens = nameString.split(SEPARATOR);
                         for (String nameToken : nameTokens) {
                             String info_o = null;
-                            CdbEntity objValue = getObjectWithName(nameToken.trim(), info_o);
+                            CdbEntity objValue = 
+                                    getObjectManager().getObjectWithName(nameToken.trim(), info_o);
                             if (objValue == null) {
                                 String msg;
                                 if (info_o != null) {
@@ -208,7 +167,8 @@ public class RefInputHandler extends SimpleInputHandler {
                     // parse and lookup single id
                     
                         String info_o = null;
-                        CdbEntity objValue = getObjectWithId(strValue.trim(), info_o);
+                        CdbEntity objValue = 
+                                getObjectManager().getObjectWithId(strValue.trim(), info_o);
                         if (objValue == null) {
                             String msg;
                             if (info_o != null) {
@@ -229,7 +189,7 @@ public class RefInputHandler extends SimpleInputHandler {
                     String[] idTokens = strValue.split(SEPARATOR);
                     for (String idToken : idTokens) {
                         String info_o = null;
-                        CdbEntity objValue = getObjectWithId(idToken.trim(), info_o);
+                        CdbEntity objValue = getObjectManager().getObjectWithId(idToken.trim(), info_o);
                         if (objValue == null) {
                             String msg;
                             if (info_o != null) {
