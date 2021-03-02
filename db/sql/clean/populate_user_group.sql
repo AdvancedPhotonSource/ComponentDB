e9028960d8b2995d16c5ca3ef4d526fccb236bd9
Dariusz <dariuszjarosz@me.com>
2021-03-02 08:57:59 -0600

    Add a maintainer role group to db. Closes #929

--- db/sql/clean/populate_user_group.sql
+++ db/sql/clean/populate_user_group.sql
@@ -6,6 +6,7 @@
 LOCK TABLES `user_group` WRITE;
 /*!40000 ALTER TABLE `user_group` DISABLE KEYS */;
 INSERT INTO `user_group` VALUES
-(1,'CDB_ADMIN','System Admin Group');
+(1,'CDB_ADMIN','System Admin Group'),
+(2,'CDB_MAINTAINER', 'System Maintainer Group');
 /*!40000 ALTER TABLE `user_group` ENABLE KEYS */;
 UNLOCK TABLES;
