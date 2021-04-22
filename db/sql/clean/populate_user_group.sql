d9a984e5702a906b39898fc9e0ef85f5e3a10f40
Dariusz <dariuszjarosz@me.com>
2021-04-22 09:14:21 -0500

    Update DB for the advanced user role. Closes #986

--- db/sql/clean/populate_user_group.sql
+++ db/sql/clean/populate_user_group.sql
@@ -7,6 +7,7 @@ LOCK TABLES `user_group` WRITE;
 /*!40000 ALTER TABLE `user_group` DISABLE KEYS */;
 INSERT INTO `user_group` VALUES
 (1,'CDB_ADMIN','System Admin Group'),
-(2,'CDB_MAINTAINER', 'System Maintainer Group');
+(2,'CDB_MAINTAINER', 'System Maintainer Group'),
+(3,'CDB_ADVANCED', 'System Advanced Group');
 /*!40000 ALTER TABLE `user_group` ENABLE KEYS */;
 UNLOCK TABLES;
