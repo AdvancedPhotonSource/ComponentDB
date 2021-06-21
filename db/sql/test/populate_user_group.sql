f2b36cf8b9964158ffd7ef31d967c1971313a624
Dariusz <dariuszjarosz@me.com>
2021-06-21 11:41:16 -0500

    Update tests to allow successful test of current code.

--- db/sql/test/populate_user_group.sql
+++ db/sql/test/populate_user_group.sql
@@ -2,6 +2,8 @@ LOCK TABLES `user_group` WRITE;
 /*!40000 ALTER TABLE `user_group` DISABLE KEYS */;
 INSERT INTO `user_group` VALUES
 (1,'CDB_ADMIN','System Admin Group'),
-(2,'Test','');
+(2,'Test',''),
+(3,'CDB_MAINTAINER','System Maintainer Group'),
+(4,'CDB_ADVANCED','System Advanced Group');
 /*!40000 ALTER TABLE `user_group` ENABLE KEYS */;
 UNLOCK TABLES;
