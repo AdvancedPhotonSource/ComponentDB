8edbd2786cbae3b23e220b0b3e22b1a7193de6ec
Dariusz <dariuszjarosz@me.com>
2021-06-28 09:48:06 -0500

    Handle multiple close matches for chromedriver. Closes #1072

--- sbin/cdb_test.sh
+++ sbin/cdb_test.sh
@@ -61,6 +61,12 @@ chromedriver_version_start=`google-chrome --version | grep -oP "[^A-Z ][0-9]+.[0
 chromedriver_version_regex="$chromedriver_version_start.[0-9]+/chromedriver_linux64.zip"
 chromedriver_version=`curl https://chromedriver.storage.googleapis.com/ | grep -oP $chromedriver_version_regex`
 
+count_results=`echo $chromedriver_version | wc -l`
+
+if [ $count_results > 1 ]; then
+    chromedriver_version=`echo ${chromedriver_version%%$'\n'*}`
+fi
+
 if [ -f "chromedriver" ]; then 
     current_version_result=`./chromedriver --version | grep -oP $chromedriver_version`
     if [[ -z "$current_version_result" ]]; then        
