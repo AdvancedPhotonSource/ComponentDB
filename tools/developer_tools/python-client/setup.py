10e88ca53a28c8ad0d9db9d5962326d9e4b8607d
Dariusz <dariuszjarosz@me.com>
2021-08-06 14:19:06 -0500

    Prepare release patch 3.12.3

--- tools/developer_tools/python-client/setup.py
+++ tools/developer_tools/python-client/setup.py
@@ -16,7 +16,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='ComponentDB-API',
-      version='3.12.3.dev0',
+      version='3.12.3',
       packages=find_packages(),
       py_modules=["CdbApiFactory"],
       install_requires=['python-dateutil', 
