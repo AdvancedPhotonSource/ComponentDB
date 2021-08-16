0e11f0510bc54eb2d82457dbe44216ce1c52a0ac
Dariusz <dariuszjarosz@me.com>
2021-08-16 10:11:34 -0500

    Add new entity types and relationship for powered and controled machines. Closes #1116

--- db/sql/static/populate_entity_type.sql
+++ db/sql/static/populate_entity_type.sql
@@ -8,6 +8,8 @@ INSERT INTO `entity_type` VALUES
 (5,'Template', 'classification for a machine design template items'),
 (6,'Measurement Data', 'classification for MAARC measurment data type items.'),
 (7,'Inventory','Entity type used for marking a sub domain of intevntory type items.'),
-(8,'Deleted','Entity type used for marking an item deleted.');
+(8,'Deleted','Entity type used for marking an item deleted.'),
+(9,'Control','Entity type used for marking a sub domain of control type items.'),
+(10,'Power','Entity type used for marking a sub domain of power type items.');
 /*!40000 ALTER TABLE `entity_type` ENABLE KEYS */;
 UNLOCK TABLES;
