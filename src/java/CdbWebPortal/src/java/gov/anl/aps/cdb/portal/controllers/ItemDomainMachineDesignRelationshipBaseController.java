1f18db0c5e35795771f5b4e899a3d0a8c74d6244
Dariusz <dariuszjarosz@me.com>
2021-08-24 16:28:39 -0500

    Standardzie loading of items related to item. Resolve issues with selection tree and localize tree instead of using the one from machine design controller. Add constraints for adding related item. Closes #1116

--- src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/controllers/ItemDomainMachineDesignRelationshipBaseController.java
+++ src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/controllers/ItemDomainMachineDesignRelationshipBaseController.java
@@ -4,8 +4,11 @@
  */
 package gov.anl.aps.cdb.portal.controllers;
 
+import gov.anl.aps.cdb.common.exceptions.InvalidArgument;
 import gov.anl.aps.cdb.portal.constants.EntityTypeName;
 import gov.anl.aps.cdb.portal.constants.ItemElementRelationshipTypeNames;
+import gov.anl.aps.cdb.portal.model.ItemDomainMachineDesignRelationshipTreeNode;
+import gov.anl.aps.cdb.portal.model.ItemDomainMachineDesignTreeNode;
 import gov.anl.aps.cdb.portal.model.db.entities.ItemDomainMachineDesign;
 import gov.anl.aps.cdb.portal.model.db.entities.ItemElement;
 import gov.anl.aps.cdb.portal.model.db.entities.ItemElementRelationship;
@@ -13,99 +16,151 @@ import gov.anl.aps.cdb.portal.model.db.entities.RelationshipType;
 import gov.anl.aps.cdb.portal.utilities.SessionUtility;
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.primefaces.event.NodeSelectEvent;
 
 public abstract class ItemDomainMachineDesignRelationshipBaseController extends ItemDomainMachineDesignBaseController {
+
+    private static final Logger LOGGER = LogManager.getLogger(ItemDomainMachineDesignRelationshipBaseController.class.getName());
     
-    private ItemDomainMachineDesign machineRelatedByCurrent; 
-    private boolean displaySelectMachineRelatedByCurrent; 
-    
-    
-    protected abstract ItemElementRelationshipTypeNames getRelationshipTypeName(); 
-    protected abstract EntityTypeName getRelationshipMachineEntityType(); 
-    
+    private ItemDomainMachineDesign machineRelatedByCurrent;
+    private ItemDomainMachineDesignTreeNode selectedMachineRelatedByCurrentNode = null;
+    private boolean displaySelectMachineRelatedByCurrent;
+
+    ItemDomainMachineDesignTreeNode standardMachineDesignRootTreeNode;
+
+    protected abstract ItemElementRelationshipTypeNames getRelationshipTypeName();
+
+    protected abstract EntityTypeName getRelationshipMachineEntityType();
+
     public List<ItemDomainMachineDesign> getMachineElementsRelatedToCurrent() {
         ItemDomainMachineDesign current = getCurrent();
-        List<ItemDomainMachineDesign> machineElementsRelatedToCurrent = null; 
+        List<ItemDomainMachineDesign> machineElementsRelatedToCurrent = null;
         if (current != null) {
             machineElementsRelatedToCurrent = current.getMachineElementsRelatedToThis();
-            
-            // Todo use loaded boolean. 
+
             if (machineElementsRelatedToCurrent == null) {
-                machineElementsRelatedToCurrent = new ArrayList<>(); 
-                
+                machineElementsRelatedToCurrent = new ArrayList<>();
+
                 List<ItemElementRelationship> itemElementRelationshipList = current.getItemElementRelationshipList1();
-                
-                for (ItemElementRelationship ier : itemElementRelationshipList) {
-                    RelationshipType relationshipType = ier.getRelationshipType();
-                    String name = relationshipType.getName();
-                    String relationshipTypeName = getRelationshipTypeName().getValue();
-                    
-                    if (name.equals(relationshipTypeName)) {
-                        ItemElement firstItemElement = ier.getFirstItemElement();
-                        ItemDomainMachineDesign machine = (ItemDomainMachineDesign) firstItemElement.getParentItem();
-                        machineElementsRelatedToCurrent.add(machine);
-                    }
-                    
-                    current.setMachineElementsRelatedToThis(machineElementsRelatedToCurrent); 
-                }
+
+                loadMachineItemsWithRelationship(itemElementRelationshipList, machineElementsRelatedToCurrent, true);
+
+                current.setMachineElementsRelatedToThis(machineElementsRelatedToCurrent);
             }
         }
-        
-        return machineElementsRelatedToCurrent; 
+
+        return machineElementsRelatedToCurrent;
     }
-    
+
+    private void loadMachineItemsWithRelationship(
+            List<ItemElementRelationship> itemElementRelationshipList,
+            List<ItemDomainMachineDesign> machineItems,
+            boolean first) {
+
+        String relationshipTypeName = getRelationshipTypeName().getValue();
+
+        for (ItemElementRelationship ier : itemElementRelationshipList) {
+            RelationshipType relationshipType = ier.getRelationshipType();
+            String name = relationshipType.getName();
+
+            if (name.equals(relationshipTypeName)) {
+                ItemElement itemElement = null; 
+                if (first) {
+                    itemElement = ier.getFirstItemElement();
+                } else {
+                    itemElement = ier.getSecondItemElement(); 
+                }
+                ItemDomainMachineDesign machine = (ItemDomainMachineDesign) itemElement.getParentItem();
+                machineItems.add(machine);
+            }
+        }
+    }
+
     @Override
     public boolean isDisplayFollowInstructionOnRightOnBlockUI() {
-        return super.isDisplayFollowInstructionOnRightOnBlockUI() 
-                || displaySelectMachineRelatedByCurrent; 
-    } 
+        return super.isDisplayFollowInstructionOnRightOnBlockUI()
+                || displaySelectMachineRelatedByCurrent;
+    }
 
     @Override
     public void resetListConfigurationVariables() {
-        super.resetListConfigurationVariables(); 
-        displaySelectMachineRelatedByCurrent = false; 
+        super.resetListConfigurationVariables();
+        displaySelectMachineRelatedByCurrent = false;
     }
-       
+
+    @Override
+    public void resetListDataModel() {
+        super.resetListDataModel();
+        standardMachineDesignRootTreeNode = null;
+    }
+
     public void prepareSelectMachinedRelatedByNode() {
-        prepareAddNewMachineDesignListConfiguration();        
+        setSelectedItemInListTreeTable(null);
+        prepareAddNewMachineDesignListConfiguration();
         displaySelectMachineRelatedByCurrent = true;
     }
-    
-      public void saveSelectedMachineRelatedByCurrent() {
+
+    public void saveSelectedMachineRelatedByCurrent() {
         if (machineRelatedByCurrent == null) {
             SessionUtility.addWarningMessage("No machine element selected", "Please select machine and try again.");
             return;
         }
-        
+
         updateCurrentUsingSelectedItemInTreeTable();
-        
-        applyRelationship(machineRelatedByCurrent, getCurrent());
-        
+
+        try {
+            applyRelationship(machineRelatedByCurrent, getCurrent());
+        } catch (InvalidArgument ex) {
+            LOGGER.error(ex);
+            SessionUtility.addErrorMessage("Error", ex.getMessage());
+            
+            return;
+        }
+
         update();
 
         resetListConfigurationVariables();
         resetListDataModel();
         expandToSelectedTreeNodeAndSelect();
     }
-    
-    public void applyRelationship(ItemDomainMachineDesign machineElement, ItemDomainMachineDesign relatedElement) {
+
+    public void applyRelationship(ItemDomainMachineDesign machineElement, ItemDomainMachineDesign relatedElement) throws InvalidArgument {
         ItemElementRelationshipTypeNames relationshipTypeName = getRelationshipTypeName();
         RelationshipType templateRelationship
                 = relationshipTypeFacade.findByName(relationshipTypeName.getValue());
-        
-        // Todo check if a relationship already exists.
 
+        List<ItemDomainMachineDesign> machineItems = new ArrayList<>();
+        List<ItemElementRelationship> itemElementRelationshipList = machineElement.getItemElementRelationshipList();
+        loadMachineItemsWithRelationship(itemElementRelationshipList, machineItems, false);
+        
+        switch (machineItems.size()) {
+            case 0:
+                // Perfect to proceed
+                break;
+            case 1:
+                ItemDomainMachineDesign relatedItem = machineItems.get(0); 
+                String name = relatedItem.getName();
+                if (relatedItem.equals(relatedElement)) {
+                    throw new InvalidArgument("Relationship with " + name + " already exists");
+                }
+                throw new InvalidArgument("The item is already related by: " + name);
+            default:    
+                throw new InvalidArgument("The item already has relationship defined");                          
+        }
+
+        // Todo check if a relationship already exists.
         // Create item element relationship between the template and the clone 
         ItemElementRelationship itemElementRelationship = new ItemElementRelationship();
         itemElementRelationship.setRelationshipType(templateRelationship);
         itemElementRelationship.setFirstItemElement(machineElement.getSelfElement());
         itemElementRelationship.setSecondItemElement(relatedElement.getSelfElement());
-        
+
         machineElement.getItemElementRelationshipList().add(itemElementRelationship);
-        relatedElement.getItemElementRelationshipList().add(itemElementRelationship); 
+        relatedElement.getItemElementRelationshipList1().add(itemElementRelationship);
     }
-    
+
     @Override
     protected final ItemDomainMachineDesign performItemRedirection(ItemDomainMachineDesign item, String paramString, boolean forceRedirection) {
         boolean matchEntityType = isMachineDesignAndEntityType(item, getRelationshipMachineEntityType());
@@ -128,7 +183,39 @@ public abstract class ItemDomainMachineDesignRelationshipBaseController extends 
             loadViewModeUrlParameter();
         }
     }
-    
+
+    @Override
+    public ItemDomainMachineDesignTreeNode loadMachineDesignRootTreeNode(List<ItemDomainMachineDesign> itemsWithoutParents) {
+        ItemDomainMachineDesignRelationshipTreeNode rootTreeNode = new ItemDomainMachineDesignRelationshipTreeNode(
+                itemsWithoutParents,
+                getDefaultDomain(),
+                getEntityDbFacade(),
+                getRelationshipTypeName()
+        );
+
+        return rootTreeNode;
+    }
+
+    public ItemDomainMachineDesignTreeNode getStandardMachineDesignRootTreeNode() {
+        if (standardMachineDesignRootTreeNode == null) {
+            List<ItemDomainMachineDesign> defaultTopLevelMachineList = super.getDefaultTopLevelMachineList();
+            standardMachineDesignRootTreeNode = new ItemDomainMachineDesignTreeNode(defaultTopLevelMachineList, getDefaultDomain(), getEntityDbFacade());
+        }
+        return standardMachineDesignRootTreeNode;
+    }
+
+    public void setStandardMachineDesignRootTreeNode(ItemDomainMachineDesignTreeNode standardMachineDesignRootTreeNode) {
+        this.standardMachineDesignRootTreeNode = standardMachineDesignRootTreeNode;
+    }
+
+    public ItemDomainMachineDesignTreeNode getSelectedMachineRelatedByCurrentNode() {
+        return selectedMachineRelatedByCurrentNode;
+    }
+
+    public void setSelectedMachineRelatedByCurrentNode(ItemDomainMachineDesignTreeNode SelectedMachineRelatedByCurrentNode) {
+        this.selectedMachineRelatedByCurrentNode = SelectedMachineRelatedByCurrentNode;
+    }
+
     public void machineRelatedByCurrentItemSelected(NodeSelectEvent nodeSelection) {
         machineRelatedByCurrent = getMachineFromNodeSelectEvent(nodeSelection);
     }
