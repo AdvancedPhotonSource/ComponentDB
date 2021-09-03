b2847fc42935d2bb7e170c2b69f30a0b9d801881
Dariusz <dariuszjarosz@me.com>
2021-09-03 10:36:17 -0500

    Utilize standard machine tree node and add show cable toggle functionality. Closes #1127

--- src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/controllers/ItemDomainMachineDesignController.java
+++ src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/controllers/ItemDomainMachineDesignController.java
@@ -6,7 +6,10 @@ package gov.anl.aps.cdb.portal.controllers;
 
 import gov.anl.aps.cdb.portal.controllers.settings.ItemDomainMachineDesignSettings;
 import gov.anl.aps.cdb.portal.controllers.utilities.ItemDomainMachineDesignControllerUtility;
+import gov.anl.aps.cdb.portal.model.ItemDomainMachineDesignTreeNode;
+import gov.anl.aps.cdb.portal.model.db.entities.ItemDomainMachineDesign;
 import gov.anl.aps.cdb.portal.utilities.SessionUtility;
+import java.util.List;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 import org.apache.logging.log4j.LogManager;
@@ -18,24 +21,46 @@ import org.apache.logging.log4j.Logger;
  */
 @Named(ItemDomainMachineDesignController.controllerNamed)
 @SessionScoped
-public class ItemDomainMachineDesignController extends ItemDomainMachineDesignBaseController implements ItemDomainCableDesignWizardClient {
+public class ItemDomainMachineDesignController extends ItemDomainMachineDesignBaseController<ItemDomainMachineDesignTreeNode> implements ItemDomainCableDesignWizardClient {
 
     private static final Logger LOGGER = LogManager.getLogger(ItemDomainMachineDesignController.class.getName());
 
-    public final static String controllerNamed = "itemDomainMachineDesignController";  
-    
+    public final static String controllerNamed = "itemDomainMachineDesignController";
+
     @Override
     protected ItemDomainMachineDesignSettings createNewSettingObject() {
         return new ItemDomainMachineDesignSettings(this);
     }
-    
+
     public static ItemDomainMachineDesignController getInstance() {
         return (ItemDomainMachineDesignController) SessionUtility.findBean(controllerNamed);
+    }
+    
+    public boolean isCablesShown() {
+        ItemDomainMachineDesignTreeNode.MachineTreeConfiguration config = getCurrentMachineDesignListRootTreeNode().getConfig();
+        return config.isShowCables();
+    }
+
+    public void setCablesShown(boolean cablesShown) {
+        ItemDomainMachineDesignTreeNode.MachineTreeConfiguration config = getCurrentMachineDesignListRootTreeNode().getConfig();
+        config.setShowCables(cablesShown);
     }
 
     @Override
     protected ItemDomainMachineDesignControllerUtility createControllerUtilityInstance() {
-        return new ItemDomainMachineDesignControllerUtility(); 
+        return new ItemDomainMachineDesignControllerUtility();
     }
-    
+
+    @Override
+    public ItemDomainMachineDesignTreeNode loadMachineDesignRootTreeNode(List<ItemDomainMachineDesign> itemsWithoutParents) {
+        ItemDomainMachineDesignTreeNode rootTreeNode = new ItemDomainMachineDesignTreeNode(itemsWithoutParents, getDefaultDomain(), getEntityDbFacade());
+
+        return rootTreeNode;
+    }
+
+    @Override
+    public ItemDomainMachineDesignTreeNode createMachineTreeNodeInstance() {
+        return new ItemDomainMachineDesignTreeNode(); 
+    }
+
 }
