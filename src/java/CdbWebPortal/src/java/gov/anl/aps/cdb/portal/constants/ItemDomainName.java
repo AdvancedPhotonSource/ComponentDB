c2da8b915f9cd6b9167061165dd79628d0128f52
Dariusz <dariuszjarosz@me.com>
2021-12-02 09:36:51 -0600

    Add id to the record of a domain constant. Closes 1246

--- src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/constants/ItemDomainName.java
+++ src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/constants/ItemDomainName.java
@@ -5,38 +5,44 @@
 package gov.anl.aps.cdb.portal.constants;
 
 public enum ItemDomainName {
-        location("Location"),
-        catalog("Catalog"),
-        inventory("Inventory"),
-        // Deprecated Domain
-        cable("Cable"),
-        
-        maarc("MAARC"),
-        machineDesign("Machine Design"),
-        cableCatalog("Cable Catalog"),
-        cableInventory("Cable Inventory"), 
-        cableDesign("Cable Design"); 
-        
-        public final static int LOCATION_ID = 1; 
-        public final static int CATALOG_ID = 2; 
-        public final static int INVENTORY_ID = 3; 
-        
-        // Deprecated Domain
-        public final static int CABLE_ID = 4; 
-        
-        public final static int MAARC_ID = 5; 
-        public final static int MACHINE_DESIGN_ID = 6; 
-        public final static int CABLE_CATALOG_ID = 7; 
-        public final static int CABLE_INVENTORY_ID = 8; 
-        public final static int CABLE_DESIGN_ID = 9; 
+    location("Location", ItemDomainName.LOCATION_ID),
+    catalog("Catalog", ItemDomainName.CATALOG_ID),
+    inventory("Inventory", ItemDomainName.INVENTORY_ID),
+    maarc("MAARC", ItemDomainName.MAARC_ID),
+    machineDesign("Machine Design", ItemDomainName.MACHINE_DESIGN_ID),
+    cableCatalog("Cable Catalog", ItemDomainName.CABLE_CATALOG_ID),
+    cableInventory("Cable Inventory", ItemDomainName.CABLE_INVENTORY_ID),
+    cableDesign("Cable Design", ItemDomainName.CABLE_DESIGN_ID),
+    
+    // Deprecated Domain
+    cable("Cable", ItemDomainName.CABLE_ID);
 
-        private String value;
+    public final static int LOCATION_ID = 1;
+    public final static int CATALOG_ID = 2;
+    public final static int INVENTORY_ID = 3;
 
-        private ItemDomainName(String value) {
-            this.value = value;
-        }               
+    // Deprecated Domain
+    public final static int CABLE_ID = 4;
 
-        public final String getValue() {
-            return value;
-        }
-    };
+    public final static int MAARC_ID = 5;
+    public final static int MACHINE_DESIGN_ID = 6;
+    public final static int CABLE_CATALOG_ID = 7;
+    public final static int CABLE_INVENTORY_ID = 8;
+    public final static int CABLE_DESIGN_ID = 9;
+
+    private String value;
+    private Integer id;
+
+    private ItemDomainName(String value, Integer id) {
+        this.value = value;
+        this.id = id;
+    }
+
+    public final String getValue() {
+        return value;
+    }
+
+    public final Integer getId() {
+        return id;
+    }
+};
