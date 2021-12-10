e9d2d17bd7924a706afcf1c1f6a6a120fd38c309
Dariusz <dariuszjarosz@me.com>
2021-12-10 09:47:40 -0600

    Improve exception reading for python api. Closes #1243

--- src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/rest/provider/GenericAPIExceptionProvider.java
+++ src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/rest/provider/GenericAPIExceptionProvider.java
@@ -5,6 +5,7 @@
 package gov.anl.aps.cdb.rest.provider;
 
 import gov.anl.aps.cdb.common.exceptions.AuthenticationError;
+import gov.anl.aps.cdb.common.exceptions.ObjectNotFound;
 import gov.anl.aps.cdb.rest.entities.ApiExceptionMessage;
 import javax.ws.rs.NotAuthorizedException;
 import javax.ws.rs.core.MediaType;
@@ -19,7 +20,7 @@ import org.apache.logging.log4j.Logger;
  * @author djarosz
  */
 @Provider
-public class GenericAPIExceptionProvider  extends Exception implements ExceptionMapper<Exception> {
+public class GenericAPIExceptionProvider extends Exception implements ExceptionMapper<Exception> {
 
     private static final Logger logger = LogManager.getLogger(GenericAPIExceptionProvider.class.getName());
     
@@ -30,9 +31,12 @@ public class GenericAPIExceptionProvider  extends Exception implements Exception
         if (exception instanceof NotAuthorizedException || exception instanceof AuthenticationError) {
             statusCode = 401;
         }
+        if (exception instanceof ObjectNotFound) {
+            statusCode = 404; 
+        }
         
         logger.error(exception); 
-        ApiExceptionMessage message = new ApiExceptionMessage(exception); 
+        ApiExceptionMessage message = new ApiExceptionMessage(exception);         
         return Response.status(statusCode).entity(message).type(MediaType.APPLICATION_JSON).build();
     }
     
