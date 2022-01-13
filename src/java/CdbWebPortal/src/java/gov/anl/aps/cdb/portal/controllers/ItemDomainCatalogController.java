90ee460651296644e3edcc88c42b106c98f93167
Dariusz <dariuszjarosz@me.com>
2022-01-13 08:46:02 -0600

    Resolve display inventory row expansion. Closes #1296

--- src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/controllers/ItemDomainCatalogController.java
+++ src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/controllers/ItemDomainCatalogController.java
@@ -12,6 +12,7 @@ import gov.anl.aps.cdb.portal.controllers.settings.ItemDomainCatalogSettings;
 import gov.anl.aps.cdb.portal.controllers.utilities.ItemDomainCatalogControllerUtility;
 import gov.anl.aps.cdb.portal.import_export.import_.helpers.ImportHelperCatalogPorts;
 import gov.anl.aps.cdb.portal.model.db.beans.ItemDomainCatalogFacade;
+import gov.anl.aps.cdb.portal.model.db.entities.Item;
 import gov.anl.aps.cdb.portal.model.db.entities.ItemDomainCatalog;
 import gov.anl.aps.cdb.portal.model.db.entities.ItemDomainInventory;
 import gov.anl.aps.cdb.portal.model.jsf.beans.SparePartsBean;
@@ -180,6 +181,18 @@ public class ItemDomainCatalogController extends ItemDomainCatalogBaseController
     public void setItemElementSelectionControllerString(String controllerName) {
         ItemDomainCatalogBaseController controller = (ItemDomainCatalogBaseController) SessionUtility.findBean(controllerName); 
         setItemElementSelectionController(controller);
+    } 
+
+    @Override
+    public boolean isDisplayRowExpansionItemsDerivedFromItem(Item item) {
+        ItemDomainCatalogSettings settingObject = getSettingObject();
+        Boolean displayInventorySetting = settingObject.getDisplayComponentInstanceRowExpansion();
+        
+        if (displayInventorySetting) {
+            return super.isDisplayRowExpansionItemsDerivedFromItem(item); 
+        }
+        
+        return displayInventorySetting; 
     }
 
     // <editor-fold defaultstate="collapsed" desc="import/export support">   
