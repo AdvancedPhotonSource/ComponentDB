0b11e3617ffa0f0f86cf2635a61866a151585c9c
Dariusz <dariuszjarosz@me.com>
2022-01-17 12:32:59 -0600

    Add a CLI setup file to the CLI based functionality. Closes #1300

--- tools/developer_tools/python-client/setup-api.py
+++ tools/developer_tools/python-client/setup-api.py
@@ -16,7 +16,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='ComponentDB-API',
-      version='3.13.dev1',
+      version='3.13.dev3',
       packages=["cdbApi",
                 "cdbApi.api",
                 "cdbApi.models"],
