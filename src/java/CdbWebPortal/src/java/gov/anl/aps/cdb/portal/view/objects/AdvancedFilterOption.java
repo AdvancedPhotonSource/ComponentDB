3eabe7cfa167176855536a519237a933ef5f56fb
craigmcchesney <auto@craigmcc.com>
2022-02-10 14:06:29 -0700

    #1321: change advanced filter display mode options to use menu instead of individual fields

--- src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/view/objects/AdvancedFilterOption.java
+++ src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/view/objects/AdvancedFilterOption.java
@@ -17,5 +17,21 @@ public class AdvancedFilterOption {
         this.name = name;
         this.description = description;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
     
 }
