c0680002be7ed7e50a3473a0bb765a0704fd095d
Dariusz <dariuszjarosz@me.com>
2022-04-12 16:41:54 -0500

    Add support for listing travelers from template for machine template. Closes #1412

--- tools/developer_tools/cdb_plugins/plugins/traveler/java/ItemTravelerDomainMachineTemplateController.java
+++ tools/developer_tools/cdb_plugins/plugins/traveler/java/ItemTravelerDomainMachineTemplateController.java
@@ -6,7 +6,9 @@ package gov.anl.aps.cdb.portal.plugins.support.traveler;
 
 import gov.anl.aps.cdb.portal.controllers.ItemController;
 import gov.anl.aps.cdb.portal.controllers.ItemDomainMachineDesignController;
+import gov.anl.aps.cdb.portal.model.db.entities.Item;
 import java.io.Serializable;
+import java.util.List;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
@@ -28,5 +30,11 @@ public class ItemTravelerDomainMachineTemplateController extends ItemTravelerDom
             itemDomainMachineDesignController = ItemDomainMachineDesignController.getInstance();
         }
         return itemDomainMachineDesignController;
+    }
+
+    @Override
+    protected List<Item> getItemListForTravelersCreatedFromTemplate() {
+        Item current = getCurrent();        
+        return current.getItemsCreatedFromThisTemplateItem();        
     }
 }
