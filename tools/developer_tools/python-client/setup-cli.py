c2450e425654b52c7d091b32bc84290e1a2ee9d5
Dariusz <dariuszjarosz@me.com>
2022-04-18 11:43:26 -0500

    Prepare CLI for release of first pre-release.

--- tools/developer_tools/python-client/setup-cli.py
+++ tools/developer_tools/python-client/setup-cli.py
@@ -16,30 +16,29 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='ComponentDB-CLI',
-      version='3.13.4',
+      version='3.13.4.dev3',
       packages=['cdbCli',
                 'cdbCli.common',
                 'cdbCli.common.cli',
                 'cdbCli.common.utility',
                 'cdbCli.service',
-                'cdbCli.service.cli'],
+                'cdbCli.service.cli',
+                'cdbCli.service.cli.cdbCliCmnds',],
       install_requires=['python-dateutil', 
                         'urllib3',
                         'six',
                         'paho-mqtt',
                         'click',
-                        'ComponentDB-API==3.13.4'],
+                        'pandas',
+                        'rich',
+                        'ComponentDB-API==3.13.0'],
       license='Copyright (c) UChicago Argonne, LLC. All rights reserved.',
       description='Python APIs used to communicate with java hosted ComponentDB API.',
       maintainer='Dariusz Jarosz',
       maintainer_email='djarosz@aps.anl.gov',
-      url='https://github.com/AdvancedPhotonSource/ComponentDB',
-      scripts=[
-          'cdbCli/service/cli/CDBclick.py'
-          ],
+      url='https://github.com/AdvancedPhotonSource/ComponentDB',      
       entry_points={
-        'console_scripts': [
-          'click-python-client-test = CdbApiFactory:run_command',
-          'cdb-cli = cdbCli.service.cli.CDBclick:main',
+        'console_scripts': [          
+          'cdb-cli = cdbCli.service.cli.cli:main',
         ]
       })
