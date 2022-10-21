3e3764fb934bfdf837ac35caccd7a7aeeb0932ec
Dariusz <dariuszjarosz@me.com>
2022-10-21 14:38:07 -0500

    Add stored procedure that can verify another item on the same branch to prevent circular reference. Closes #1532

--- db/sql/create_stored_procedures.sql
+++ db/sql/create_stored_procedures.sql
@@ -189,6 +189,44 @@ BEGIN
 	FROM assembly inner join item on assembly.child_item_id = item.id; 
 END //
 
+DROP PROCEDURE IF EXISTS is_item_relationship_have_circular_reference;//
+CREATE PROCEDURE `is_item_relationship_have_circular_reference` (IN relationship_type_id int, IN parent_item_id int, IN proposed_child_item_id int)
+BEGIN
+	SELECT * from item where ID in (
+	WITH RECURSIVE child_relationship_hierarchy AS (
+	SELECT *
+	FROM v_relationship_hierarchy vrh
+	WHERE vrh.parent_item_id = parent_item_id
+	AND vrh.relationship_type_id = relationship_type_id
+	UNION
+	SELECT vrh2.*
+	FROM v_relationship_hierarchy vrh2, child_relationship_hierarchy a
+	WHERE vrh2.parent_item_id = a.child_item_id
+	AND vrh2.relationship_type_id = relationship_type_id
+	)
+	SELECT crh.parent_item_id
+	FROM child_relationship_hierarchy crh
+	WHERE child_item_id = proposed_child_item_id
+	)
+	UNION ALL
+	SELECT * FROM item WHERE id in (
+	WITH RECURSIVE parent_relationship_hierarchy AS (
+	SELECT *
+	FROM v_relationship_hierarchy vrh
+	WHERE vrh.child_item_id = parent_item_id
+	AND vrh.relationship_type_id = relationship_type_id
+	UNION
+	SELECT vrh2.*
+	FROM v_relationship_hierarchy vrh2, parent_relationship_hierarchy a
+	WHERE vrh2.child_item_id = a.parent_item_id
+	AND vrh2.relationship_type_id = relationship_type_id
+	)
+	SELECT prh.child_item_id
+	FROM parent_relationship_hierarchy as prh
+	WHERE prh.parent_item_id = proposed_child_item_id
+	);
+END //
+
 DROP PROCEDURE IF EXISTS fetch_items_with_property_value;//
 CREATE PROCEDURE `fetch_items_with_property_value` (IN property_value_id int) 
 BEGIN
