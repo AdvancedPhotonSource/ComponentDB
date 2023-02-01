822d193cc09c3f38c9236898b78843dfee2e0afe
Dariusz <dariuszjarosz@me.com>
2023-02-01 17:05:27 -0600

    Add db and settings. Closes #1571

--- db/sql/static/populate_setting_type.sql
+++ db/sql/static/populate_setting_type.sql
@@ -640,6 +640,22 @@ description.','true'),
 (25046,'ItemDomainCableInventory.List.Display.Length','Display column for length in cable inventory list.','true'),
 (25047,'ItemDomainCableInventory.List.FilterBy.Length','Filter for components by length.',NULL),
 (25048,'ItemDomainCableInventory.List.FilterBy.Status','Filter for components by status.',NULL),
-(25049,'ItemDomainCableInventory.List.Display.Status','Display column for status in cable inventory list.','true');
+(25049,'ItemDomainCableInventory.List.Display.Status','Display column for status in cable inventory list.','true'),
+(26000,'ItemDomainApp.List.Display.Id','Display for components by description.','false'),
+(26001,'ItemDomainApp.List.Display.DisplayItemType','Display for components by description.','true'),
+(26002,'ItemDomainApp.List.Display.DisplayItemCategory','Display for components by description.','true'),
+(26003,'ItemDomainApp.List.Display.Description','Display for components by description.','true'),
+(26004,'ItemDomainApp.List.Display.CreatedByUser','Display for components that were created by username.','false'),
+(26005,'ItemDomainApp.List.Display.CreatedOnDateTime','Display for components that were created on date/time.','false'),
+(26006,'ItemDomainApp.List.Display.LastModifiedByUser','Display for components that were last modified by username.','false'),
+(26007,'ItemDomainApp.List.Display.LastModifiedOnDateTime','Display for components that were last modified on date/time.','false'),
+(26008,'ItemDomainApp.List.Display.OwnerUser','Display for component instances by owner username.','false'),
+(26009,'ItemDomainApp.List.Display.OwnerGroup','Display for component instances by owner group name.','false'),
+(26010,'ItemDomainApp.List.Display.PropertyTypeId1','Display for property value with displayed property type id #1.',''),
+(26011,'ItemDomainApp.List.Display.PropertyTypeId2','Display for property value with displayed property type id #2.',''),
+(26012,'ItemDomainApp.List.Display.PropertyTypeId3','Display for property value with displayed property type id #3.',''),
+(26013,'ItemDomainApp.List.Display.PropertyTypeId4','Display for property value with displayed property type id #4.',''),
+(26014,'ItemDomainApp.List.Display.PropertyTypeId5','Display for property value with displayed property type id #5.',''),
+(26015,'ItemDomainApp.List.Display.NumberOfItemsPerPage','Display for components by description.','false');
 /*!40000 ALTER TABLE `setting_type` ENABLE KEYS */;
 UNLOCK TABLES;
