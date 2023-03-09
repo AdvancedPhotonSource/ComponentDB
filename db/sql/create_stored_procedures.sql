2b4b549381ec2424e00e50ee53584d747c186249
Dariusz <dariuszjarosz@me.com>
2023-03-09 16:20:27 -0600

    Resolve issue with circular reference check. Closes #1590

--- db/sql/create_stored_procedures.sql
+++ db/sql/create_stored_procedures.sql
@@ -257,39 +257,41 @@ END //
 
 DROP PROCEDURE IF EXISTS is_item_relationship_have_circular_reference;//
 CREATE PROCEDURE `is_item_relationship_have_circular_reference` (IN relationship_type_id int, IN parent_item_id int, IN proposed_child_item_id int)
-BEGIN
-	SELECT * from item where ID in (
-	WITH RECURSIVE child_relationship_hierarchy AS (
-	SELECT *
-	FROM v_relationship_hierarchy vrh
-	WHERE vrh.parent_item_id = parent_item_id
-	AND vrh.relationship_type_id = relationship_type_id
-	UNION
-	SELECT vrh2.*
-	FROM v_relationship_hierarchy vrh2, child_relationship_hierarchy a
-	WHERE vrh2.parent_item_id = a.child_item_id
-	AND vrh2.relationship_type_id = relationship_type_id
-	)
-	SELECT crh.parent_item_id
-	FROM child_relationship_hierarchy crh
-	WHERE child_item_id = proposed_child_item_id
-	)
-	UNION ALL
-	SELECT * FROM item WHERE id in (
-	WITH RECURSIVE parent_relationship_hierarchy AS (
-	SELECT *
-	FROM v_relationship_hierarchy vrh
-	WHERE vrh.child_item_id = parent_item_id
-	AND vrh.relationship_type_id = relationship_type_id
-	UNION
-	SELECT vrh2.*
-	FROM v_relationship_hierarchy vrh2, parent_relationship_hierarchy a
-	WHERE vrh2.child_item_id = a.parent_item_id
-	AND vrh2.relationship_type_id = relationship_type_id
-	)
-	SELECT prh.child_item_id
-	FROM parent_relationship_hierarchy as prh
-	WHERE prh.parent_item_id = proposed_child_item_id
+BEGIN	
+	-- Get all parents for the existing parent item.
+	SELECT * FROM item WHERE ID IN (
+		WITH RECURSIVE parent_relationship_hierarchy AS (
+			SELECT * 
+			FROM v_relationship_hierarchy vrh
+			WHERE vrh.child_item_id = parent_item_id
+			AND vrh.relationship_type_id = relationship_type_id
+			UNION
+			SELECT vrh2.*
+			FROM v_relationship_hierarchy vrh2, parent_relationship_hierarchy a
+			WHERE vrh2.child_item_id = a.parent_item_id
+			AND vrh2.relationship_type_id = relationship_type_id
+		)		
+		SELECT prh.parent_item_id
+		FROM parent_relationship_hierarchy prh
+	) and ID IN (
+	-- Compare if any children match the parent in proposed children items.	
+	SELECT id FROM item WHERE 
+	-- Ensure single level circular reference doesn't occur.
+	ID = proposed_child_item_id 
+	OR ID IN (
+		WITH RECURSIVE child_relationship_hierarchy AS (
+			SELECT * 
+			FROM v_relationship_hierarchy vrh
+			WHERE vrh.parent_item_id = proposed_child_item_id
+			AND vrh.relationship_type_id = relationship_type_id
+			UNION
+			SELECT vrh2.*
+			FROM v_relationship_hierarchy vrh2, child_relationship_hierarchy a
+			WHERE vrh2.parent_item_id = a.child_item_id
+			AND vrh2.relationship_type_id = relationship_type_id
+		)		
+		SELECT crh.child_item_id
+		FROM child_relationship_hierarchy crh)
 	);
 END //
 
