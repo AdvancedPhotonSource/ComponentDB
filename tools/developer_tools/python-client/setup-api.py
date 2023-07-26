b60574ebd240d5d9c8dc14e5df3fb618bc9eecb6
Dariusz <dariuszjarosz@me.com>
2023-07-26 13:50:17 -0500

    Prepare release for 3.15.7

--- tools/developer_tools/python-client/setup-api.py
+++ tools/developer_tools/python-client/setup-api.py
@@ -16,7 +16,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='ComponentDB-API',
-      version='3.15.6',
+      version='3.15.7',
       packages=["cdbApi",
                 "cdbApi.api",
                 "cdbApi.models"],
