103b3c044acb51152a052946eee9bc48f6a6a39e
Dariusz Jarosz <djarosz@anl.gov>
2024-01-22 10:36:53 -0600

    Resolve clean-db

--- db/sql/clean/populate_item_category.sql
+++ db/sql/clean/populate_item_category.sql
@@ -6,6 +6,6 @@
 LOCK TABLES `item_category` WRITE;
 /*!40000 ALTER TABLE `item_category` DISABLE KEYS */;
 INSERT INTO `item_category` VALUES
-(1,'Other',NULL,2);
+(1,'Other',NULL,2, 0.0);
 /*!40000 ALTER TABLE `item_category` ENABLE KEYS */;
 UNLOCK TABLES;
--- db/sql/clean/populate_item_type.sql
+++ db/sql/clean/populate_item_type.sql
@@ -6,13 +6,13 @@
 LOCK TABLES `item_type` WRITE;
 /*!40000 ALTER TABLE `item_type` DISABLE KEYS */;
 INSERT INTO `item_type` VALUES
-(1,'Building',NULL,1),
-(2,'Area',NULL,1),
-(3,'Room',NULL,1),
-(4,'Table',NULL,1),
-(5,'Cabinet',NULL,1),
-(6,'Rack',NULL,1),
-(7,'Shelf',NULL,1),
-(8,'Other',NULL,2);
+(1,'Building',NULL,1, 0),
+(2,'Area',NULL,1, 0),
+(3,'Room',NULL,1, 0),
+(4,'Table',NULL,1, 0),
+(5,'Cabinet',NULL,1, 0),
+(6,'Rack',NULL,1, 0),
+(7,'Shelf',NULL,1, 0),
+(8,'Other',NULL,2, 0);
 /*!40000 ALTER TABLE `item_type` ENABLE KEYS */;
 UNLOCK TABLES;
