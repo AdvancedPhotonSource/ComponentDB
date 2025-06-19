1f8d1396cb6e9da952a3324e6070e2b35d449b8b
Dariusz Jarosz <djarosz@anl.gov>
2025-06-19 08:20:23 -0500

    Add functionality to specify a machine parent for a new IOC.
    #1670

--- src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/controllers/ItemDomainMachineDesignIOCController.java
+++ src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/controllers/ItemDomainMachineDesignIOCController.java
@@ -12,7 +12,11 @@ import gov.anl.aps.cdb.portal.controllers.utilities.ItemDomainMachineDesignIOCCo
 import gov.anl.aps.cdb.portal.model.ItemDomainMachineDesignIOCLazyDataModel;
 import gov.anl.aps.cdb.portal.model.ItemDomainMachineDesignTreeNode;
 import gov.anl.aps.cdb.portal.model.db.entities.EntityType;
+import gov.anl.aps.cdb.portal.model.db.entities.Item;
 import gov.anl.aps.cdb.portal.model.db.entities.ItemDomainMachineDesign;
+import gov.anl.aps.cdb.portal.model.db.entities.ItemElement;
+import gov.anl.aps.cdb.portal.model.db.entities.UserInfo;
+import gov.anl.aps.cdb.portal.utilities.SessionUtility;
 import java.util.ArrayList;
 import java.util.List;
 import javax.enterprise.context.SessionScoped;
@@ -29,8 +33,11 @@ import org.apache.logging.log4j.Logger;
 public class ItemDomainMachineDesignIOCController extends ItemDomainMachineDesignBaseController<ItemDomainMachineDesignTreeNode, ItemDomainMachineDesignIOCControllerUtility, ItemDomainMachineDesignIOCLazyDataModel> {
 
     public final static String CONTROLLER_NAMED = "itemDomainMachineDesignIOCController";
-    
+
     private static final Logger LOGGER = LogManager.getLogger(ItemDomainMachineDesignIOCController.class.getName());
+
+    private ItemDomainMachineDesignTreeNode parentMachineSelectionTreeNode;
+    private ItemDomainMachineDesignTreeNode parentMachineSelectedTreeNode = null;
 
     @Override
     public ItemDomainMachineDesignTreeNode loadMachineDesignRootTreeNode(List<ItemDomainMachineDesign> itemsWithoutParents) {
@@ -55,7 +62,7 @@ public class ItemDomainMachineDesignIOCController extends ItemDomainMachineDesig
     @Override
     public String getItemListPageTitle() {
         return "IOC Item List";
-    }           
+    }
 
     @Override
     public ItemDomainMachineDesignIOCLazyDataModel createItemLazyDataModel() {
@@ -69,18 +76,22 @@ public class ItemDomainMachineDesignIOCController extends ItemDomainMachineDesig
 
     @Override
     public boolean getEntityDisplayImportButton() {
-        return false; 
+        return false;
     }
 
     @Override
     public boolean getEntityDisplayExportButton() {
-        return false; 
-    }    
+        return false;
+    }
 
     @Override
     public String prepareCreate() {
+        // Reset variables
+        parentMachineSelectionTreeNode = null;
+
         String createRedirect = super.prepareCreate();
 
+        // Assign IOC type
         ItemDomainMachineDesign current = getCurrent();
         String iocEntityTypeName = EntityTypeName.ioc.getValue();
         EntityType iocEntityType = entityTypeFacade.findByName(iocEntityTypeName);
@@ -90,9 +101,69 @@ public class ItemDomainMachineDesignIOCController extends ItemDomainMachineDesig
             LOGGER.error(ex);
         }
         current.getEntityTypeList().add(iocEntityType);
-        
-        
-        return createRedirect;                 
+
+        // Create placeholder for machine parent element.
+        UserInfo user = SessionUtility.getUser();
+        ItemElement parentItemElement = getControllerUtility().createItemElement(current, user);
+        parentItemElement.setParentItem(null);
+        parentItemElement.setContainedItem(current);
+        try {
+            current.setParentMachineElement(parentItemElement);
+        } catch (CdbException ex) {
+            LOGGER.error(ex);
+        }
+
+        return createRedirect;
+    }
+
+    @Override
+    public String create() {
+        ItemDomainMachineDesign current = getCurrent();
+        ItemDomainMachineDesign parentMachineDesign = current.getParentMachineDesign();
+
+        if (parentMachineDesign == null) {
+            SessionUtility.addWarningMessage("Machine Parent", "Machine Parent must be specified");
+            return null;
+        }
+
+        String createRedirect = super.create();
+
+        return createRedirect;
+    }
+
+    public ItemDomainMachineDesignTreeNode getParentMachineSelectionTreeNode() {
+        if (parentMachineSelectionTreeNode == null) {
+            ItemDomainMachineDesignController instance = ItemDomainMachineDesignController.getInstance();
+            parentMachineSelectionTreeNode = instance.loadMachineDesignRootTreeNode();
+
+        }
+        return parentMachineSelectionTreeNode;
+    }
+
+    public ItemDomainMachineDesignTreeNode getParentMachineSelectedTreeNode() {
+        return parentMachineSelectedTreeNode;
+    }
+
+    public void setParentMachineSelectedTreeNode(ItemDomainMachineDesignTreeNode parentMachineSelectedTreeNode) {
+        this.parentMachineSelectedTreeNode = parentMachineSelectedTreeNode;
+
+        if (parentMachineSelectedTreeNode != null) {
+            ItemElement machineElement = (ItemElement) parentMachineSelectedTreeNode.getData();
+
+            if (machineElement.getId() == null) {
+                return;
+            }
+
+            ItemDomainMachineDesign current = getCurrent();
+            Item parentItem = machineElement.getContainedItem();
+
+            String elementName = getControllerUtility().generateUniqueElementNameForItem(parentItem);
+
+            ItemElement parentMachineElement = current.getParentMachineElement();
+            parentMachineElement.setName(elementName);
+
+            parentMachineElement.setParentItem(parentItem);
+        }
     }
 
 }
