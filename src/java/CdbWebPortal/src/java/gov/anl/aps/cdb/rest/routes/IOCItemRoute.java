51dad0927dfcfb08a1fc0b8c654d9dc65f4f5720
Dariusz Jarosz <djarosz@anl.gov>
2025-06-26 11:51:43 -0500

    Allow setting of the metadata fields using the API.
    #1670

--- src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/rest/routes/IOCItemRoute.java
+++ src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/rest/routes/IOCItemRoute.java
@@ -11,9 +11,11 @@ import gov.anl.aps.cdb.portal.controllers.utilities.ItemControllerUtility;
 import gov.anl.aps.cdb.portal.controllers.utilities.ItemDomainMachineDesignBaseControllerUtility;
 import gov.anl.aps.cdb.portal.model.db.beans.ItemDomainMachineDesignFacade;
 import gov.anl.aps.cdb.portal.model.db.entities.ItemDomainMachineDesign;
+import gov.anl.aps.cdb.portal.model.db.entities.ItemMetadataIOC;
 import gov.anl.aps.cdb.portal.model.db.entities.UserInfo;
 import gov.anl.aps.cdb.rest.authentication.Secured;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import javax.ejb.EJB;
@@ -21,6 +23,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -40,14 +43,29 @@ public class IOCItemRoute extends ItemBaseRoute {
     @Operation(summary = "Move machine to a new parent.")
     @SecurityRequirement(name = "cdbAuth")
     @Secured
-    public ItemDomainMachineDesign convertMachineToIOC(@PathParam("mdId") int machineId) throws AuthorizationError, CdbException {
+    public ItemDomainMachineDesign convertMachineToIOC(@PathParam("mdId") int machineId,
+            @Parameter(description = "Machine Tag") @QueryParam("machineTag") String machineTag,
+            @Parameter(description = "Function Tag") @QueryParam("functionTag") String functionTag) throws AuthorizationError, CdbException {
         ItemDomainMachineDesign machine = machineFacade.find(machineId);
 
         UserInfo currentUser = verifyCurrentUserPermissionForItem(machine);
 
         machine.addEntityType(EntityTypeName.ioc.getValue());
 
+        if (ItemDomainMachineDesign.isItemDeleted(machine)) {
+            throw new CdbException("The machine id " + machine.getId() + " is a deleted item.");
+        }
+
         ItemDomainMachineDesignBaseControllerUtility utility = machine.getItemControllerUtility();
+
+        ItemMetadataIOC iocInfo = machine.getIocInfo();
+        if (machineTag != null) {
+            iocInfo.setMachineTag(machineTag);
+        }
+        if (functionTag != null) {
+            iocInfo.setFunctionTag(functionTag);
+        }
+
         machine = utility.update(machine, currentUser);
 
         return machine;
