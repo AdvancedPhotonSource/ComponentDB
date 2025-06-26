1662eee9a3b1db58965c474b36d17e41fde1bb41
Dariusz Jarosz <djarosz@anl.gov>
2025-06-26 11:49:32 -0500

    Add core metadata for ioc type machines.
    #1670

--- src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/controllers/utilities/ItemDomainMachineDesignIOCControllerUtility.java
+++ src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/controllers/utilities/ItemDomainMachineDesignIOCControllerUtility.java
@@ -4,7 +4,11 @@
  */
 package gov.anl.aps.cdb.portal.controllers.utilities;
 
+import gov.anl.aps.cdb.common.constants.ItemMetadataFieldType;
 import gov.anl.aps.cdb.portal.model.db.entities.ItemDomainMachineDesign;
+import gov.anl.aps.cdb.portal.model.db.entities.ItemMetadataIOC;
+import static gov.anl.aps.cdb.portal.model.db.entities.ItemMetadataIOC.IOC_ITEM_INTERNAL_PROPERTY_TYPE;
+import gov.anl.aps.cdb.portal.view.objects.ItemMetadataPropertyInfo;
 import java.util.List;
 
 /**
@@ -20,7 +24,34 @@ public class ItemDomainMachineDesignIOCControllerUtility extends ItemDomainMachi
 
     @Override
     public String getDisplayEntityTypeName() {
-        return "IOC Item"; 
+        return "IOC Item";
+    }
+
+    @Override
+    public ItemMetadataPropertyInfo createCoreMetadataPropertyInfo() {
+
+        ItemMetadataPropertyInfo info
+                = new ItemMetadataPropertyInfo("IOC Metadata", IOC_ITEM_INTERNAL_PROPERTY_TYPE);
+
+        info.addField(
+                ItemMetadataIOC.IOC_ITEM_MACHINE_TAG_KEY,
+                "Machine Tag",
+                "Machine Tag.",
+                ItemMetadataFieldType.STRING,
+                "",
+                null,
+                null);
+
+        info.addField(
+                ItemMetadataIOC.IOC_ITEM_FUNCTION_TAG_KEY,
+                "Function Tag",
+                "Function Tag.",
+                ItemMetadataFieldType.STRING,
+                "",
+                null,
+                null);
+
+        return info;
     }
 
 }
